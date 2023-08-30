c9548ef5306eeb6c33b534683c0581f2d11def7f
ValberJunior <valberjr1992@gmail.com>
2023-08-30 11:47:51 -0300

    🔵 other: update component

--- plugins/gitlab-pipelines/src/components/ExampleComponent/ExampleComponent.tsx
+++ plugins/gitlab-pipelines/src/components/ExampleComponent/ExampleComponent.tsx
@@ -12,6 +12,8 @@ import {
 import { ExampleFetchComponent } from '../ExampleFetchComponent';
 import { useApi } from '@backstage/core-plugin-api';
 import { gitlabPipelinesApiRef } from '../../api';
+import { SelectBranch } from '../SelectBranch';
+import { GitlabPipelinesProvider } from '../context/GitlabPipelinesProvider';
 
 export const ExampleComponent = () => {
 
@@ -29,7 +31,8 @@ export const ExampleComponent = () => {
   }, []);
   
   return (
-  <Page themeId="tool">
+  <GitlabPipelinesProvider>
+    <Page themeId="tool">
     <Header title="Welcome to gitlab-pipelines!" subtitle="Optional subtitle">
       <HeaderLabel label="Owner" value="Team X" />
       <HeaderLabel label="Lifecycle" value="Alpha" />
@@ -40,6 +43,9 @@ export const ExampleComponent = () => {
       </ContentHeader>
       <Grid container spacing={3} direction="column">
         <Grid item>
+          <div>
+            <SelectBranch/>
+          </div>
           <InfoCard title="Information card">
             <Typography variant="body1">
               All content should be wrapped in a card like this.
@@ -52,4 +58,5 @@ export const ExampleComponent = () => {
       </Grid>
     </Content>
   </Page>
+  </GitlabPipelinesProvider>
 );}
