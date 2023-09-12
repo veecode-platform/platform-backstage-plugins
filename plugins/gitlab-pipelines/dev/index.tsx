e60bea3a9b1f9c6426f8ba5bc63571acd60ca014
ValberJunior <valberjr1992@gmail.com>
2023-09-12 09:05:45 -0300

    fix: change import

--- plugins/gitlab-pipelines/dev/index.tsx
+++ plugins/gitlab-pipelines/dev/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createDevApp } from '@backstage/dev-utils';
-import { gitlabPipelinesPlugin, GitlabPipelinesPage } from '../src/plugin';
+import { gitlabPipelinesPlugin, GitlabPipelinesOverview } from '../src/plugin';
 
 createDevApp()
   .registerPlugin(gitlabPipelinesPlugin)
   .addPage({
-    element: <GitlabPipelinesPage />,
+    element: <GitlabPipelinesOverview />,
     title: 'Root Page',
     path: '/gitlab-pipelines'
   })
