cddcb32181ae6a9d23b8939edf50c61372c79267
ValberJunior <valberjr1992@gmail.com>
2024-01-10 16:54:44 -0300

    update application plugin

--- plugins/application/src/components/shared/styles/Styles.tsx
+++ plugins/application/src/components/shared/styles/Styles.tsx
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core';
 export const Styles = makeStyles(theme => ({
     button: {
         background: theme.palette.primary.main,
-        margin: '.2rem'
+        margin: '.2rem',
+        color: theme.palette.background.default
     }
   
   }));
