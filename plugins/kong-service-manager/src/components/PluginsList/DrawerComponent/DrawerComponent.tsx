01163bc886054c470e26287333235cd0aa047735
ValberJunior <valberjr1992@gmail.com>
2024-03-01 18:19:14 -0300

    update

--- plugins/kong-service-manager/src/components/PluginsList/DrawerComponent/DrawerComponent.tsx
+++ plugins/kong-service-manager/src/components/PluginsList/DrawerComponent/DrawerComponent.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @backstage/no-undeclared-imports */
 import React, { useContext, useEffect, useState } from 'react';
-import { Box, Button, Drawer, IconButton, Typography, makeStyles } from '@material-ui/core';
+import { Box, Button, Checkbox, Drawer, FormControl, FormControlLabel, IconButton, TextField, Typography, makeStyles } from '@material-ui/core';
 import Close from '@material-ui/icons/Close';
 import { KongServiceManagerContext } from '../../context';
 import { CreatePlugin } from '../../../utils/types';
 import { useEntity } from '@backstage/plugin-catalog-react';
 import { useEntityAnnotation } from '../../../hooks';
 import { EmptyStateComponent } from '../../shared';
+import { Progress } from '@backstage/core-components';
 
 
 const useStyles = makeStyles(theme => ({
@@ -35,9 +37,24 @@ const useStyles = makeStyles(theme => ({
     },
     content: {
       height: '85%',
-      backgroundColor: theme.palette.background.default,
+      backgroundColor: theme.palette.background.paper,
       overflow: 'auto',
       padding: theme.spacing(2),
+    },
+    form:{
+      display: 'flex',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column'
+    },
+    input:{
+      width: '100%',
+      margin: '.5rem 0'
+    },
+    checkbox:{
+      width: '100%',
+      margin: '.5rem'
     },
     secondaryAction: {
       marginLeft: theme.spacing(2.5),
@@ -47,11 +64,12 @@ const useStyles = makeStyles(theme => ({
 
 export const DrawerComponent = () => {
 
-  const {paper, header,titleBar,pluginIcon, icon, content, secondaryAction} = useStyles();
+  const {paper, header,titleBar,pluginIcon, icon, content,form, input,checkbox, secondaryAction} = useStyles();
   const { entity } = useEntity();
   const { serviceName, kongInstance } = useEntityAnnotation(entity);
   const { handleToggleDrawer, openDrawer, enablePlugin, disablePlugin, getPluginFields ,selectedPlugin} = useContext(KongServiceManagerContext);
-  const [fieldsComponents, setFieldsComponents ] = useState<any[]|[]>([])
+  const [fieldsComponents, setFieldsComponents ] = useState<any[]|[]>([]);
+  const [ isLoading, setLoading] = useState<boolean>(false);
 
   const handleEnablePlugin = async ( config: CreatePlugin ) => {  // to do
     await enablePlugin(serviceName as string, config, kongInstance as string);
@@ -63,15 +81,19 @@ export const DrawerComponent = () => {
 
   const handlePluginFields = async (pluginName: string, proxyPath: string) => {
     const fields = await getPluginFields(pluginName, proxyPath);
+    // eslint-disable-next-line no-console
+    console.log(fields)
     if(fields) setFieldsComponents(fields!);
   };
 
   useEffect(()=>{
-   if(selectedPlugin) handlePluginFields(selectedPlugin.slug as string, kongInstance as string)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+   if(selectedPlugin) handlePluginFields(selectedPlugin.slug as string, kongInstance as string);
+   setLoading(true);
+   setTimeout(()=>{
+    setLoading(false)
+   },1500)
   },[selectedPlugin])
     
-
   return (
     <Drawer
       classes={{
@@ -98,7 +120,35 @@ export const DrawerComponent = () => {
         </IconButton>
       </div>
       <Box className={content}>
-        {fieldsComponents.length >=1 ? <h1>Hello world</h1>:<EmptyStateComponent/>}
+        {
+          isLoading ? <Progress/> : (
+            <>
+            {
+              fieldsComponents.length >=1 ? (  
+                <FormControl component="form" noValidate autoComplete="off" className={form}>
+                  <>
+                    {fieldsComponents.map(field => {
+                      switch (field.type) {
+                        case 'string':
+                          return (<TextField id={field.name} required={field.required} key={field.name} label={field.name} variant="outlined" className={input} defaultValue={field.defaultValue ?? ''}/>)
+                        case 'number':
+                            return (<TextField id={field.name} required={field.required} key={field.name} label={field.name} variant="outlined" className={input} defaultValue={field.defaultValue ?? ''}/>)
+                        case 'boolean':
+                          return (<FormControlLabel value="end" key={field.name} labelPlacement="end" label={field.name} control={<Checkbox color="primary" required={field.required} />} className={checkbox}/>)
+                        default:
+                          return <></>;
+                      }
+                    })}
+                  </>
+                </FormControl>
+            
+                )
+                :
+                <EmptyStateComponent/>
+            }
+            </>
+          )
+        }
       </Box>
       <div>
         <>
