4f008f72049b647df0a447e6a03c67a23d01da10
ValberJunior <valberjr1992@gmail.com>
2024-03-12 13:11:01 -0300

    update selectInstance

--- plugins/kong-service-manager/src/components/SelectInstance/SelectInstance.tsx
+++ plugins/kong-service-manager/src/components/SelectInstance/SelectInstance.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @backstage/no-undeclared-imports */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useContext, useEffect, useState } from 'react';
+import React, { Suspense, useContext, useEffect, useState } from 'react';
 import { Select, SelectedItems } from '@backstage/core-components';
 import { useEntity } from '@backstage/plugin-catalog-react';
 import { Entity } from '@backstage/catalog-model';
@@ -8,6 +8,7 @@ import { KongServiceManagerContext } from '../context';
 import { useEntityAnnotation } from '../../hooks';
 import { transformToSelectOptions } from '../../utils/common/transformToSelectOptions';
 import { Tooltip } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
 
 
 type OptionsProps = {
@@ -23,7 +24,8 @@ export const SelectInstance = () => {
   const [options, setOptions] = useState<OptionsProps[]>([]);
   const [instanceDefault, setInstanceDefault ] = useState<string>(kongInstances ? kongInstances[0] : '');
   const { instance, setInstanceState } = useContext(KongServiceManagerContext);
-   
+  const optionDetault = {label: 'Select the Kong Instance', value: 'Select the Kong Instance'}
+
   useEffect(() => {
        if(kongInstances){
         setInstances(kongInstances);
@@ -37,7 +39,7 @@ export const SelectInstance = () => {
 
   useEffect(() => {
     if (instances) {
-      const newOptions = transformToSelectOptions(instances)
+      const newOptions = transformToSelectOptions(instances);
       setOptions(newOptions);
     }
   }, [instances]);
@@ -48,15 +50,17 @@ export const SelectInstance = () => {
   };
 
   return (
-    <Tooltip title="Select the Kong Instance" placement="left">
-      <div>
-        <Select
-          onChange={handleSelectChange}
-          label=""
-          selected={(instance ? instance as string : instanceDefault as string) ?? ""}
-          items={options}
-        />
-      </div>
-    </Tooltip>
+    <Suspense fallback={<Skeleton variant="rect" width={210} height={118} animation="wave" />}>
+      <Tooltip title="Select the Kong Instance" placement="left">
+        <div>
+          <Select
+            onChange={handleSelectChange}
+            label=""
+            selected={instance ?? "Select the Kong Instance"}
+            items={options ?? optionDetault}
+          />
+        </div>
+      </Tooltip>
+    </Suspense>
   );
 };
