12ac2b113aebb19cea1e54b361425f701f87eb23
ValberJunior <valberjr1992@gmail.com>
2024-04-04 14:26:10 -0300

    update function

--- plugins/gitlab-pipelines/src/utils/commons/truncateString.ts
+++ plugins/gitlab-pipelines/src/utils/commons/truncateString.ts
@@ -1,6 +1,14 @@
 export const truncateString = (str: string, maxLength: number) => {
-    if (str.length > maxLength) {
-      return `${str.slice(0, maxLength)}   ...`;
-    }
-    return str;
-  };
+
+  // If it is a string made up of more than one word, return only the first word
+  if (str.includes(' ')) {
+    const words: string[] = str.split(' ');
+    const word: string = words[0];
+    return word;
+  }
+  if (str.length > maxLength) {
+    return `${str.slice(0, maxLength)}  ...`  // If it is a string that exceeds the character limit entered in the function parameter
+  }
+
+  return str;
+};
