8f697aa8dc6869ba85c122723f33e8fa61b527ea
ValberJunior <valberjr1992@gmail.com>
2024-04-11 15:03:41 -0300

    update hook useScaffolder

--- plugins/veecode-scaffolder-extensions/src/hooks/useScaffolder.ts
+++ plugins/veecode-scaffolder-extensions/src/hooks/useScaffolder.ts
@@ -4,25 +4,15 @@ export function useScaffolder() {
     const config = useApi(configApiRef);
     const scaffolder = config.getOptionalConfig('scaffolder');
     if(!scaffolder) return {};
-
-    // gitlab
-    const gitlabScaffolderExists = scaffolder.has('gitlab');
     const gitlabScaffolder = (scaffolder.getOptional('gitlab') || []) as any[];
-    const gitlabHostScaffolder = gitlabScaffolder[0]?.host;
-    const gitlabTokenScaffolder = gitlabScaffolder[0]?.token;
-    
-    // github
-    const githubScaffolderExists = scaffolder.has('github');
     const githubScaffolder = (scaffolder.getOptional('github') || []) as any[];
-    const githubHostScaffolder = githubScaffolder[0]?.host;
-    const githubTokenScaffolder = githubScaffolder[0]?.token;
 
     return {
-        gitlabScaffolderExists,
-        gitlabHostScaffolder,
-        gitlabTokenScaffolder,
-        githubScaffolderExists,
-        githubHostScaffolder,
-        githubTokenScaffolder,
+        gitlabScaffolderExists : scaffolder.has('gitlab'),
+        gitlabHostScaffolder: gitlabScaffolder[0]?.host,
+        gitlabTokenScaffolder: gitlabScaffolder[0]?.token,
+        githubScaffolderExists: scaffolder.has('github'),
+        githubHostScaffolder: githubScaffolder[0]?.host,
+        githubTokenScaffolder:  githubScaffolder[0]?.token,
     };
 }
