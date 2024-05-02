35f089f733b7d83702f5cc5fd71b9ed3bda588ca
ValberJunior <valberjr1992@gmail.com>
2024-05-02 14:17:31 -0300

    update component

--- plugins/github-workflows/src/components/GithubWorkflowsDetails/JobsComponent/JobsComponent.tsx
+++ plugins/github-workflows/src/components/GithubWorkflowsDetails/JobsComponent/JobsComponent.tsx
@@ -1,9 +1,10 @@
 import { Box, Paper, Tooltip, Typography } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import { Job } from '../../../utils/types'
 import { calculateDuration, getFileNameToPath } from '../../../utils/common'
 import { useStyles } from '../styles'
 import { WorkFlowStatus } from '../../WorkFlowStatus'
+import { JobModal } from '../JobModal'
 
 export type JobsComponentProps = {
     path:string,
@@ -15,37 +16,59 @@ export const JobsComponent : React.FC<JobsComponentProps> = (props) => {
 
   const {path,event,jobs} = props
   const title = getFileNameToPath(path);
+  const [ showModalSteps,setShowModalSteps ] = useState<boolean>(false);
+  const [ jobSelected, setJobSelected] = useState<Job|null>(null);
   const {jobsSection, jobsContent,jobItem,jobDuration} = useStyles();
 
+  const handleToggleModal = () => setShowModalSteps(!showModalSteps);
+
+  const handleClickAction = (job:Job) => {
+    if(job){
+      setJobSelected(job);
+      handleToggleModal();
+    }
+  }
+
   return (
-    <Paper variant="outlined" className={jobsSection}>
-      <Typography variant="h5">{title}</Typography>
-      <Typography variant="subtitle1" color="textSecondary">
-        on: {event}
-      </Typography>
-      <div className={jobsContent}>
-        {jobs.map(j => (
-          <Tooltip 
-            key={j.id} 
-            title="Click to see the steps..."
-            arrow
+    <>
+      <Paper variant="outlined" className={jobsSection}>
+        <Typography variant="h5">{title}</Typography>
+        <Typography variant="subtitle1" color="textSecondary">
+          on: {event}
+        </Typography>
+        <div className={jobsContent}>
+          {jobs.map(j => (
+            <Tooltip
+              key={j.id}
+              title="Click to see the steps..."
+              arrow
+              onClick={() => handleClickAction(j)}
             >
-            <Box className={jobItem}>
-              <WorkFlowStatus
-                status={j.status}
-                conclusion={j.conclusion}
-                icon
-              />
-              <Typography variant="body1">{j.name}</Typography>
-              <div className={jobDuration}>
-                <Typography variant="body1" align="right">
-                  {calculateDuration(j.started_at, j.completed_at!)}
-                </Typography>
-              </div>
-            </Box>
-          </Tooltip>
-        ))}
-      </div>
-    </Paper>
+              <Box className={jobItem}>
+                <WorkFlowStatus
+                  status={j.status}
+                  conclusion={j.conclusion}
+                  icon
+                />
+                <Typography variant="body1">{j.name}</Typography>
+                <div className={jobDuration}>
+                  <Typography variant="body1" align="right">
+                    {calculateDuration(j.started_at, j.completed_at!)}
+                  </Typography>
+                </div>
+              </Box>
+            </Tooltip>
+          ))}
+        </div>
+      </Paper>
+
+      {jobSelected && (
+        <JobModal
+          job={jobSelected!}
+          show={showModalSteps}
+          handleCloseModal={handleToggleModal}
+        />
+      )}
+    </>
   );
 }
