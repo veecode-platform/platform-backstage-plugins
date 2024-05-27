6b08d477e445a94b929b4a0a50ccc8820edd646f
ValberJunior <valberjr1992@gmail.com>
2024-05-27 10:51:15 -0300

    update config.d.ts

--- plugins/kong-service-manager/config.d.ts
+++ plugins/kong-service-manager/config.d.ts
@@ -27,37 +27,6 @@ export interface Config {
                     'x-api-key'?: string;
                     [key: string]: string | undefined;
                     };
-               /**
-               * @visibility frontend
-               */
-                allowedMethods?: string[];
-               /**
-               * The credentials policy to apply.
-               *
-               * @remarks
-               *
-               * The values are as follows:
-               *
-               * - 'require': Callers must provide Backstage user or service
-               *   credentials with each request. The credentials are not
-               *   forwarded to the proxy target.
-               * - 'forward': Callers must provide Backstage user or service
-               *   credentials with each request, and those credentials are
-               *   forwarded to the proxy target.
-               * - 'dangerously-allow-unauthenticated': No Backstage credentials
-               *   are required to access this proxy target. The target can still
-               *   apply its own credentials checks, but the proxy will not help
-               *   block non-Backstage-blessed callers.
-               *
-               * Note that if you have
-               * `backend.auth.dangerouslyDisableDefaultAuthPolicy` set to `true`,
-               * the `credentials` value does not apply; the proxy will behave as
-               * if all endpoints were set to `dangerously-allow-unauthenticated`.
-               */
-              credentials?:
-                | 'require'
-                | 'forward'
-                | 'dangerously-allow-unauthenticated';
             };
       };
     } 
