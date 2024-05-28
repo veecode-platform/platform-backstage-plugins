602b5ad11af60b5ebbd18c1683650391b285c72a
ValberJunior <valberjr1992@gmail.com>
2024-05-28 13:28:58 -0300

    create new types

--- plugins/kong-service-manager/src/utils/types/index.ts
+++ plugins/kong-service-manager/src/utils/types/index.ts
@@ -1,3 +1,32 @@
+import { DiscoveryApi } from "@backstage/core-plugin-api";
+
+export interface KongServiceManagerApi {
+
+    getEnabledPlugins(proxyPath: string): Promise<string[]>;
+
+    getPluginFields(workspace:string,pluginName: string, proxyPath: string): Promise<PluginFieldsResponse[]>;
+
+    getServiceAssociatedPlugins(workspace:string,serviceIdOrName: string, proxyPath: string): Promise<AssociatedPluginsResponse[]>;
+
+    createServicePlugin(workspace:string, serviceIdOrName: string, config: CreatePlugin, proxyPath: string): Promise<any>;
+
+    editServicePlugin(workspace:string,serviceIdOrName: string, pluginId: string, config: CreatePlugin, proxyPath: string): Promise<any>;
+
+    removeServicePlugin(workspace:string,serviceIdOrName: string, pluginId: string, proxyPath: string): Promise<any>;
+
+    getRoutesFromService(workspace:string,serviceIdOrName: string, proxyPath: string): Promise<RoutesResponse[]>;
+
+    getServiceInfo(workspace:string,serviceIdOrName: string, proxyPath: string): Promise<ServiceInfoResponse>;
+
+    getAllEnabledPlugins(workspace:string,serviceIdOrName: string, proxyPath: string, searchFilter?: string): Promise<PluginPerCategory[]>;
+
+
+}
+
+export type Options = {
+    discoveryApi: DiscoveryApi;
+    proxyPath: string;
+};
 export interface SchemaFields {
     [key: string]: any;
 }
