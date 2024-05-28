27619cf9eafd9dd6209c54bf071e725c96090567
ValberJunior <valberjr1992@gmail.com>
2024-05-28 13:28:46 -0300

    add kongInstance default value

--- plugins/kong-service-manager/src/hooks/useEntityAnnotation.ts
+++ plugins/kong-service-manager/src/hooks/useEntityAnnotation.ts
@@ -10,7 +10,7 @@ export const useEntityAnnotation = (entity: Entity) => {
     if (!kongInstance) {
         return {
           serviceName,
-          kongInstance: null,
+          kongInstance: '/kong-manager/api',
           workspace
         };
       }
