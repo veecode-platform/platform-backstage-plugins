d4f32b1bb77fa38b52f37a79bba1ce1483eeedda
ValberJunior <valberjr1992@gmail.com>
2024-06-05 15:52:21 -0300

    :tada: create files -- table and types

---
+++ plugins/infracost-backend/src/database/tables.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @backstage/no-undeclared-imports */
+import { JsonObject } from "@backstage/types"
+
+export type DbInfracostEstimateRow ={
+    id: string,
+    name: string,
+    currency: string,
+    projects: JsonObject[],
+    total_hourly_cost: string,
+    total_monthly_cost: string,
+    total_monthly_usage_const: string,
+    past_total_hourly_cost: string,
+    past_total_monthly_cost: string,
+    past_tota_monthly_usage_cost: string,
+    diff_total_hourly_cost: string,
+    diff_total_monthly_cost: string,
+    diff_total_monthly_usage_cost: string,
+    sumary: JsonObject,
+    time_generated: string,
+    created_at: string,
+    updated_at: string
+}
