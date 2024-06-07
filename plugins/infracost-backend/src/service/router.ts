32a450d51a292f3f20e6f886a0e5f53a462cc58a
ValberJunior <valberjr1992@gmail.com>
2024-06-07 15:02:00 -0300

    create route file

--- plugins/infracost-backend/src/service/router.ts
+++ plugins/infracost-backend/src/service/router.ts
@@ -2,17 +2,19 @@
 import { PluginDatabaseManager, UrlReader, errorHandler } from '@backstage/backend-common';
 import { AuthService, DiscoveryService, HttpAuthService, LoggerService, PermissionsService } from '@backstage/backend-plugin-api';
 import { Config } from '@backstage/config';
-import express from 'express';
+import express, { RequestHandler } from 'express';
 import Router from 'express-promise-router';
 import { CatalogApi } from '@backstage/catalog-client';
 import { IdentityApi } from '@backstage/plugin-auth-node';
+import { InfracosteStore } from '../database';
+import { InputError } from '@backstage/errors';
 
 export interface RouterOptions {
   logger: LoggerService;
   config: Config;
-  reader: UrlReader;
-  database: PluginDatabaseManager;
-  catalogClient: CatalogApi;
+  reader?: UrlReader;
+  database: InfracosteStore;
+  catalogClient?: CatalogApi;
   permissions?: PermissionsService;
   auth?: AuthService;
   httpAuth?: HttpAuthService;
@@ -23,17 +25,101 @@ export interface RouterOptions {
 export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
-  const { logger } = options;
 
+  const {logger,database} = options;
   const router = Router();
+
   router.use(express.json());
+  router.use(
+    express.urlencoded({
+      extended: true,
+    }),
+  );
+
+   /**
+   * health check
+   */
 
   router.get('/health', (_, response) => {
     logger.info('PONG!');
     response.json({ status: 'ok' });
   });
+  
+   /**
+   * List ALL Infracost Projects Estimate on the database
+   */
+
+  router.get('/infracost-estimate',( async (_, response) => {
+    const data = await database.listInfracostProjectsEstimate();
+    if(!data){
+      throw new InputError('There was an error trying to get Infracost Projects Estimate')
+    }
+    response.status(200);
+    response.json(data)
+  }) as RequestHandler);
+
+   /**
+   * Get by name
+   */
+   router.get('/infracost-estimate/id/:name',( async (request, response) => {
+    const estimateName = request.params.name;
+    const data = await database.getInfracostProjectsEstimatebyName(estimateName);
+
+    response.status(200);
+    response.json(data ?? [])
+  }) as RequestHandler);
+
+   /**
+   * Insert / Create a new Infracost Projects Estimate
+   */
+
+   router.post('/infracost-estimate',(async(request, response)=>{
+      const newEstimate = request.body;
+      const data = await database.createInfracostProjectsEstimate(newEstimate);
+
+      if(!data){
+        throw new InputError("There was an error trying persist Infracost Projects Estimate on database")
+      }
+
+      response.status(201);
+      response.json(data)
+
+   }) as RequestHandler);
+
+   /**
+   * Delete a Infracost Projects Estimate
+   */
+
+   router.delete('/infracost-estimate/:name', ( async (request,response)=> {
+    const estimateName = request.params.name;
+    const data = await database.deleteInfracostProjectsEstimate(estimateName) ;
+
+    if(!data){
+      throw new InputError("There was an error trying to delete the estimate...")
+    }
+
+    response.status(204)
+    response.json({status: 204, message: `Infracost Projects Estimate for ${estimateName} was successfully deleted`})
+   }) as RequestHandler);
+
+   /**
+   * Update a Infracost Projects Estimate
+   */
+
+   router.put('/infracost-estimate', ( async (request,response)=> {
+    const updatedEstimate = request.body;
+    const data = await database.updateInfracostProjectsEstimate(updatedEstimate);
+
+    if(!data){
+      throw new InputError("There was an error trying to update the estimate...")
+    }
+
+    response.status(200)
+    response.json(data)
+   }) as RequestHandler);
 
   router.use(errorHandler());
+
   return router;
 }
 
