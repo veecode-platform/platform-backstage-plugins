bff591fd1af866197e62d0b3fd4f6564305102fd
ValberJunior <valberjr1992@gmail.com>
2024-06-07 09:34:05 -0300

    create utils folder

---
+++ plugins/infracost-backend/src/utils/constants/index.ts
@@ -0,0 +1,2 @@
+export const ANNOTATION_INFRACOST_PROJECT = 'infracost/project';
+export const INFRACOST_TABLE = 'infracost_projects_estimate'
---
+++ plugins/infracost-backend/src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './types/infracost'
+export * from './constants'
