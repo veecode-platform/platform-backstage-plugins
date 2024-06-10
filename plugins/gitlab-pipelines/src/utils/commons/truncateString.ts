aafd2108b44d0ada0850127bab11d8ce521080d0
ValberJunior <valberjr1992@gmail.com>
2024-06-10 10:35:32 -0300

    fix button label

--- plugins/gitlab-pipelines/src/utils/commons/truncateString.ts
+++ plugins/gitlab-pipelines/src/utils/commons/truncateString.ts
@@ -1,14 +1,17 @@
+/* eslint-disable no-console */
 export const truncateString = (str: string, maxLength: number) => {
 
+  const treatedString = str.trim();
+
   // If it is a string made up of more than one word, return only the first word
-  if (str.includes(' ')) {
-    const words: string[] = str.split(' ');
+  if (treatedString.includes(' ')) {
+    const words: string[] = treatedString.split(' ');
     const word: string = words[0];
     return word;
   }
-  if (str.length > maxLength) {
-    return `${str.slice(0, maxLength)}  ...`  // If it is a string that exceeds the character limit entered in the function parameter
+  if (treatedString.length > maxLength) {
+    return `${treatedString.slice(0, maxLength)}  ...`  // If it is a string that exceeds the character limit entered in the function parameter
   }
 
-  return str;
+  return treatedString;
 };
