95c62e073b404847e9a786ca57e19e84727e7bf2
ValberJunior <valberjr1992@gmail.com>
2024-06-11 20:39:23 -0300

    refactor: 📦 delete finOps kind

--- plugins/veecode-platform-common/src/entitiesProcessor/index.ts
+++ plugins/veecode-platform-common/src/entitiesProcessor/index.ts
@@ -2,4 +2,3 @@ export * from './Cluster';
 export * from './Environment';
 export * from './Database';
 export * from './Vault';
-export * from './FinOps';
--- plugins/veecode-platform-common/src/kinds/index.ts
+++ plugins/veecode-platform-common/src/kinds/index.ts
@@ -2,4 +2,3 @@ export * from './ClusterEntityV1alpha1';
 export * from './EnvironmentEntityV1alpha1';
 export * from './DatabaseEntityV1alpha1';
 export * from './VaultEntityV1alpha1';
-export * from './FinOpsEntityV1alpha1';
