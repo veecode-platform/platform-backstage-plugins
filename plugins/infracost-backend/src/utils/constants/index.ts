4d40acaae8bd53850ae375b23c134d22e980d378
ValberJunior <valberjr1992@gmail.com>
2024-06-11 17:29:16 -0300

    refactor: 📦 change INFRACOST_TABLE

--- plugins/infracost-backend/src/utils/constants/index.ts
+++ plugins/infracost-backend/src/utils/constants/index.ts
@@ -1,4 +1,4 @@
 export const ANNOTATION_INFRACOST_PROJECT = 'infracost/project';
-export const INFRACOST_TABLE = 'infracost_projects_estimate'
+export const INFRACOST_TABLE = 'projects_estimate'
 export const INFRACOST_SPEC_TYPE = 'FinOps'
 export const CATALOG_REF = "catalog/ref"
