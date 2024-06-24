1f14ff9f2a1f689bd74e2bb915be7c3ef67008fd
ValberJunior <valberjr1992@gmail.com>
2024-06-24 14:57:57 -0300

    refactor: 📦 remove import

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -43,7 +43,6 @@ import { KongServiceManagerPage } from '@veecode-platform/plugin-kong-service-ma
 import { RepoUrlSelectorExtension, ResourcePickerExtension, UploadFilePickerExtension,OptionsPickerExtension } from '@veecode-platform/veecode-scaffolder-extensions';
 import { apiDocsPlugin } from '@backstage/plugin-api-docs';
 import { KubernetesGptAnalyzerPage } from '@veecode-platform/backstage-plugin-kubernetes-gpt-analyzer';
-import { InfracostPage } from '@veecode-platform/backstage-plugin-infracost';
 
 const app = createApp({
   apis,
@@ -129,7 +128,6 @@ const routes = (
     <Route path="/catalog-graph" element={<CatalogGraphPage />} />
     <Route path="/kong-service-manager" element={<KongServiceManagerPage />} />
     <Route path="/kubernetes-gpt-analyzer" element={<KubernetesGptAnalyzerPage />} />
-    <Route path="/infracost" element={<InfracostPage />} />
   </FlatRoutes>
 );
 
