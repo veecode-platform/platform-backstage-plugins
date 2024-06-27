eb34c8f6119ab6e54fbe33d0f94179b3b3fc4588
ValberJunior <valberjr1992@gmail.com>
2024-06-27 15:26:54 -0300

    feat: ✨ add Modal Component and code review

--- plugins/infracost/src/components/infracostOverviewPage/infracostDashboard/resourcesComponent/ResourcesComponent.tsx
+++ plugins/infracost/src/components/infracostOverviewPage/infracostDashboard/resourcesComponent/ResourcesComponent.tsx
@@ -8,13 +8,17 @@ import { Chart } from './chart';
 import { Table } from '../../../shared/table';
 import { initialResourcesTableState, resourcesTableReducer } from './state/table/reducer';
 import { addRow } from './state/table/actions';
+import { BiExpand } from "react-icons/bi";
+import { Tooltip } from '@material-ui/core';
+import { ModalComponent } from './modalComponent';
 
 export const ResourcesComponent : React.FC<ResourcesComponentProps> = (props) => {
 
+  const [showModal, setShowModal] = React.useState<boolean>(false);
   const [chartState, chartDispatch] = React.useReducer(chartReducer,initialChartState);
   const [tableState, tableDispatch] = React.useReducer(resourcesTableReducer,initialResourcesTableState);
   const { resources } = props;
-  const { root, chartStyles, tableStyles } = useResourcesComponentStyles();
+  const { root, chartStyles, tableStyles,expandDetails } = useResourcesComponentStyles();
   const labelProps = ['Name','monthly_cost']
   
   const generateResourceData = (resourcesValue: Resource[]) => {
@@ -24,27 +28,31 @@ export const ResourcesComponent : React.FC<ResourcesComponentProps> = (props) =>
           const monthlyCost = Number(resource.monthlyCost)
           chartDispatch(addItem({label: resource.name, value: Number(monthlyCost.toFixed(2))}))
         }
-        // if(!resource.monthlyCost || resource.monthlyCost === '0'){
-        //   chartDispatch(addItem({label: resource.name, value: 0}))
-        // }
-     })
-      
+     })   
   };
+
+  const handleToggleModal = ()=> setShowModal(!showModal);
   
   React.useEffect(()=>{
     generateResourceData(resources)
   },[resources]);
 
   return (
-    <div className={root}>
-      <div className={chartStyles}>
-        <Chart items={chartState}/>
+    <>
+      <div className={root}>
+        <div className={chartStyles}>
+          <Chart items={chartState} />
+        </div>
+        <div className={tableStyles}>
+          <Table labels={labelProps} data={tableState} />
+        </div>
+        <div className={expandDetails}>
+          <Tooltip title="Expand Details">
+            <BiExpand size="22" color="#B1B1B190" onClick={handleToggleModal}/>
+          </Tooltip>
+        </div>
       </div>
-      <div className={tableStyles}>
-        <Table 
-         labels={labelProps} 
-         data={tableState}/>
-      </div>
-    </div>
-  )
+      {showModal && <ModalComponent show={showModal} handleCloseModal={handleToggleModal} resources={resources}/>}
+    </>
+  );
 }
