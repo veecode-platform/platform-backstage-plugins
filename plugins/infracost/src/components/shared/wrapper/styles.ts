0be6d7d66d4e9e7f7f67927aa4426681a5e0e740
ValberJunior <valberjr1992@gmail.com>
2024-06-28 10:28:42 -0300

    refactor: 📦 changes in prop

--- plugins/infracost/src/components/shared/wrapper/styles.ts
+++ plugins/infracost/src/components/shared/wrapper/styles.ts
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core";
 
 export const useWrapperStyles = makeStyles(theme=>({
     root: {
-        background: theme.palette.background.default,
+        backgroundColor: theme.palette.background.default,
         padding: '1.5rem',
         borderRadius: '5px',
         border: `1px solid ${theme.palette.grey[700]}`
