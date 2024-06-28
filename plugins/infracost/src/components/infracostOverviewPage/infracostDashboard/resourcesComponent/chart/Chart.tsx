184e95886c5d60116142eb55e88dec140f98e932
ValberJunior <valberjr1992@gmail.com>
2024-06-27 21:59:44 -0300

    update chart component

--- plugins/infracost/src/components/infracostOverviewPage/infracostDashboard/resourcesComponent/chart/Chart.tsx
+++ plugins/infracost/src/components/infracostOverviewPage/infracostDashboard/resourcesComponent/chart/Chart.tsx
@@ -1,28 +1,39 @@
 import React from 'react'
 import { ChartProps } from './types'
 import { PieChart } from '@mui/x-charts/PieChart';
+import { Box } from '@material-ui/core';
 
 export const Chart : React.FC<ChartProps> = (props) => {
 
  const { items } = props;
  const chartItems = (items.length === 1 && items[0].value === 0) ? [{id: items[0].id, label: items[0].label, value: 0.001}] : items;
+ const size = {height: 450}
 
   return (
+    <Box sx={{ width: '100%' }}>
       <PieChart
         series={[
           {
             data: chartItems,
-            innerRadius: 50,
+            innerRadius: '50%',
             highlightScope: { faded: 'global', highlighted: 'item' },
-          }
+          },
         ]}
-        height={250}
-        // slotProps={{
-        //   legend: { 
-        //       hidden: false
-        //    },
-        // }}
-        margin={{ top: 0, bottom: 0, left: 0, right: 200 }}
+        slotProps={{
+          legend: {
+            direction: 'row',
+            position: { vertical: 'bottom', horizontal: 'middle' },
+            padding: 0,
+            labelStyle:{
+              fontSize: 14
+            },
+            itemMarkWidth: 11,
+            itemMarkHeight: 10,
+          },
+        }}
+        margin={{ top: 100, bottom: 100, left: 100, right: 100 }}
+        {...size}
       />
+    </Box>
   );
 }
