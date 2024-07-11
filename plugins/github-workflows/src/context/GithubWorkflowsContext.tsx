49a60453b91bfb357278cfd0c70008d79552663c
ValberJunior <valberjr1992@gmail.com>
2024-07-11 09:13:49 -0300

    refactor: 📦 changes in context

--- plugins/github-workflows/src/context/GithubWorkflowsContext.tsx
+++ plugins/github-workflows/src/context/GithubWorkflowsContext.tsx
@@ -4,21 +4,21 @@ import { RestEndpointMethodTypes } from "@octokit/rest";
 
 
 export type GithubWorkflowsContextType = {
-  listAllWorkflows: (projectName: string, filter?: string[]) => Promise<WorkflowResultsProps[] | null | void>,
-  listJobsForWorkflowRun: (projectName: string, id: number) => Promise<Job[]>,
+  listAllWorkflows: (hostname:string, projectName: string, filter?: string[]) => Promise<WorkflowResultsProps[] | null | void>,
+  listJobsForWorkflowRun: (hostname:string, projectName: string, id: number) => Promise<Job[]>,
   branch: string | null,
   setBranchState: (branch: string) => void,
   setInputs: (inputs: object) => void,
   inputsWorkflowsParams: object | null,
-  getWorkflowById:(id: number, projectName: string) => Promise<WorkflowRun|null>,
+  getWorkflowById:(hostname:string, projectName: string,id: number) => Promise<WorkflowRun|null>,
   workflowsState: WorkflowResultsProps[] | null,
   setWorkflowsState: React.Dispatch<React.SetStateAction<WorkflowResultsProps[] | null>>,
   workflowsByAnnotationsState: WorkflowResultsProps[] | null,
   setWorkflowsByAnnotationsState:  React.Dispatch<React.SetStateAction<WorkflowResultsProps[] | null>>,
-  handleStartWorkflowRun: (workFlowId: number, projectSlug: string) => Promise<WorkflowRun | null>,
-  handleStopWorkflowRun: (runId: number, projectSlug: string) => Promise<void>,
-  downloadJobLogs: (projectSlug: string, jobId: number) => Promise<RestEndpointMethodTypes['actions']['downloadJobLogsForWorkflowRun']['response']['data']|null>,
-  listAllEnvironments: (projectSlug: string) => Promise<RestEndpointMethodTypes['repos']['getAllEnvironments']['response']['data']|null>
+  handleStartWorkflowRun: (hostname:string, projectSlug: string, workFlowId: number) => Promise<WorkflowRun | null>,
+  handleStopWorkflowRun: (hostname:string, projectSlug: string, runId: number) => Promise<void>,
+  downloadJobLogs: (hostname:string, projectSlug: string, jobId: number) => Promise<RestEndpointMethodTypes['actions']['downloadJobLogsForWorkflowRun']['response']['data']|null>,
+  listAllEnvironments: (hostname:string, projectSlug: string) => Promise<RestEndpointMethodTypes['repos']['getAllEnvironments']['response']['data']|null>
 };
 
 export const GithubWorkflowsContext = createContext<GithubWorkflowsContextType>(null!);
--- plugins/github-workflows/src/context/GithubWorkflowsProvider.tsx
+++ plugins/github-workflows/src/context/GithubWorkflowsProvider.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { GithubWorkflowsContext } from './GithubWorkflowsContext';
 import { errorApiRef, useApi } from '@backstage/core-plugin-api';
 import { githubWorkflowsApiRef } from '../api';
-import { WorkflowResultsProps } from '../utils/types';
+import { Job, WorkflowResultsProps } from '../utils/types';
 import { sortWorflowsByName } from '../utils/helpers';
 
 interface GithubWorkflowsProviderProps {
@@ -27,9 +27,9 @@ export const GithubWorkflowsProvider: React.FC<GithubWorkflowsProviderProps> = (
     setInputsWorkflowsParams(inputs);
   }
 
-  const listAllWorkflows = async (projectName: string, filter: string[] = []) => {
+  const listAllWorkflows = async (hostname:string, projectName: string, filter: string[] = []) => {
     try {
-      const workflows = await api.listWorkflows(projectName, branch!, filter);
+      const workflows = await api.listWorkflows(hostname,projectName, branch!, filter);
       if(workflows){
         const newWorkflowsState = await Promise.all(workflows.map(async (w) => {
           return {
@@ -54,20 +54,36 @@ export const GithubWorkflowsProvider: React.FC<GithubWorkflowsProviderProps> = (
     }
   }
 
-  const getWorkflowById = async (id: number,projectName:string) => {
+  const getWorkflowById = async (hostname:string,projectName:string,id:number) => {
     try{
-      const workflow = await api.getWorkflowRunById(id.toString(),projectName);
-      return workflow
+      const data = await api.getWorkflowRunById(hostname,projectName,id);
+      const workflow = {
+        id: data.id as number,
+        name: data.name as string,
+        head_branch: data.head_branch as string,
+        event: data.event as string,
+        status: data.status as string,
+        conclusion: data.conclusion as string,
+        run_started_at: data.run_started_at as string,
+        created_at: data.created_at as string,
+        updated_at: data.updated_at as string,
+        actor: data.actor,
+        path: data.path as string,
+        artifacts_url: data.artifacts_url as string,
+        head_sha: data.head_sha as string,
+        repository: data.repository
+      };
+      return workflow;
     }catch(e:any){
       errorApi.post(e);
       return null
     }
   }
 
-  const listJobsForWorkflowRun = async (projectName:string, id: number) => {
+  const listJobsForWorkflowRun = async (hostname:string, projectName:string, id: number) => {
     try{
-      const data = await api.listJobsForWorkflowRun(projectName,id);
-      return data.jobs;
+      const data = await api.listJobsForWorkflowRun(hostname,projectName,id);
+      return data.jobs as Job[];
     }catch(e:any){
       errorApi.post(e);
       return []
@@ -75,9 +91,9 @@ export const GithubWorkflowsProvider: React.FC<GithubWorkflowsProviderProps> = (
     }
   }
 
-  const handleStartWorkflowRun = async (workFlowId: number, projectSlug: string) => { 
+  const handleStartWorkflowRun = async (hostname:string,projectSlug: string, workFlowId: number, ) => { 
     try {
-      const response = await api.startWorkflowRun(workFlowId.toString(), projectSlug, branch!, inputsWorkflowsParams ?? {});
+      const response = await api.startWorkflowRun(hostname, projectSlug, workFlowId,branch!, inputsWorkflowsParams as any);
       if(response) return response;
       return null
     }
@@ -87,18 +103,18 @@ export const GithubWorkflowsProvider: React.FC<GithubWorkflowsProviderProps> = (
      }
   };
 
-  const handleStopWorkflowRun = async (runId: number, projectSlug: string) => {
+  const handleStopWorkflowRun = async (hostname:string,projectSlug: string,runId: number) => {
     try {
-      await api.stopWorkflowRun(runId.toString(), projectSlug);
+      await api.stopWorkflowRun(hostname,projectSlug,runId);
     }
     catch (e:any) {
       errorApi.post(e)
      }
   }
 
-  const downloadJobLogs = async (projectSlug: string,jobId:number) => {
+  const downloadJobLogs = async (hostname:string,projectSlug: string,jobId:number) => {
     try{
-      const response = await api.downloadJobLogsForWorkflowRun(projectSlug,jobId);
+      const response = await api.downloadJobLogsForWorkflowRun(hostname,projectSlug,jobId);
       if(response) return response;
       return null
       } 
@@ -108,9 +124,9 @@ export const GithubWorkflowsProvider: React.FC<GithubWorkflowsProviderProps> = (
      }
   }
 
-  const listAllEnvironments = async(projectSlug:string)=>{
+  const listAllEnvironments = async(hostname:string,projectSlug:string)=>{
     try{
-      const response = await api.getEnvironmentsList(projectSlug);
+      const response = await api.getEnvironmentsList(hostname,projectSlug);
       if(response) return response;
       return null
     }catch(e:any){
