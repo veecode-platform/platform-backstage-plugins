340dd573e14a630abe7d5c7532b550b1b0e98134
ValberJunior <valberjr1992@gmail.com>
2024-07-12 18:56:38 -0300

    feat: ✨ create queued component

---
+++ plugins/github-workflows/src/components/WorkFlowStatus/queued.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core";
+
+const useQueuedStyles = makeStyles({
+    root: {
+        width: '28px',
+        height: '28px', 
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    circle: {
+        backgroundColor: '#eab308',
+        borderRadius: '50%',
+        width: '18px',
+        height: '18px',
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%) scale(1)', 
+        animation: '$growUp 1s linear infinite',
+    },
+    '@keyframes growUp': {
+        '0%': {
+            transform: 'translate(-50%, -50%) scale(.9)' 
+        },
+        '100%': {
+            transform: 'translate(-50%, -50%) scale(1.05)' 
+        }
+    }
+});
+
+export const Queued = () => {
+    const { root, circle } = useQueuedStyles();
+
+    return (
+        <div className={root}>
+            <span className={circle}> </span>
+        </div>
+    );
+};
