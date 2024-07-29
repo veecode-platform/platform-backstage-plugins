4ddd286b4757fc995ec0f273ea314a05d9e1c607
ValberJunior <valberjr1992@gmail.com>
2024-07-29 13:57:58 -0300

    feat: ✨ create InfracostPlugin (new backend)

---
+++ plugins/infracost-backend/src/InfracostPlugin.ts
@@ -0,0 +1,37 @@
+import { coreServices, createBackendPlugin } from "@backstage/backend-plugin-api";
+import { createRouter } from "./service/router";
+import { DatabaseInfracostStore } from "./database";
+
+
+/**
+ *  Infracost Plugin
+ *  @alpha
+ */
+
+
+export const infracostPlugin = createBackendPlugin({
+    pluginId: 'infracost',
+    register(env) {
+      env.registerInit({
+        deps: {
+          httpRouter: coreServices.httpRouter,
+          logger: coreServices.logger,
+          database: coreServices.database,
+          config: coreServices.rootConfig
+        },
+        async init({ httpRouter, logger,database,config }) {
+            const db = await DatabaseInfracostStore.create({
+                database: database,
+                logger
+              });
+            httpRouter.use(
+                await createRouter({
+                  logger,
+                  database: db,
+                  config,
+                }),
+              );
+        },
+      });
+    },
+  });
