7bd03628210bde1506a33bf1ef74feb5773c8159
ValberJunior <valberjr1992@gmail.com>
2024-08-01 16:09:02 -0300

    refactor: 📦 review component

--- plugins/github-workflows/src/components/GitubWorkflowsContent/WorkflowsDetails/JobsComponent/JobsComponent.tsx
+++ plugins/github-workflows/src/components/GitubWorkflowsContent/WorkflowsDetails/JobsComponent/JobsComponent.tsx
@@ -1,25 +1,29 @@
-import { Box, Paper, Tooltip, Typography } from '@material-ui/core'
-import React, { useState } from 'react'
-import { useWorkflowDetailsStyles } from '../styles'
-import { WorkFlowStatus } from '../../WorkFlowStatus'
-import { JobModal } from '../JobModal'
-import { JobsComponentProps } from '../types'
-import { calculateDuration, getFileNameToPath } from '../../../../utils/helpers'
-import { Job } from '../../../../utils/types'
+import React from 'react';
+import { Box, Paper, Tooltip, Typography } from '@material-ui/core';
+import { useWorkflowDetailsStyles } from '../styles';
+import { WorkFlowStatus } from '../../WorkFlowStatus';
+import { JobModal } from '../JobModal';
+import { JobsComponentProps } from '../types';
+import { calculateDuration, getFileNameToPath } from '../../../../utils/helpers';
+import { Job } from '../../../../utils/types';
+import { initialJobState, JobSelectedReducer } from '../state/jobSelected/reducer';
+import { addJob } from '../state/jobSelected/actions';
+
 
 export const JobsComponent : React.FC<JobsComponentProps> = (props) => {
 
-  const {path,event,jobs} = props
+  const [ showModalSteps,setShowModalSteps ] = React.useState<boolean>(false);
+  const [ jobSelectedState, dispatchJobSelected] = React.useReducer(JobSelectedReducer,initialJobState);
+  const {path,event,jobs} = props;
   const title = getFileNameToPath(path);
-  const [ showModalSteps,setShowModalSteps ] = useState<boolean>(false);
-  const [ jobSelected, setJobSelected] = useState<Job|null>(null);
   const {jobsSection, jobsContent,jobItem,jobDuration} = useWorkflowDetailsStyles();
+
 
   const handleToggleModal = () => setShowModalSteps(!showModalSteps);
 
   const handleClickAction = (job:Job) => {
     if(job){
-      setJobSelected(job);
+      dispatchJobSelected(addJob(job));
       handleToggleModal();
     }
   }
@@ -57,9 +61,9 @@ export const JobsComponent : React.FC<JobsComponentProps> = (props) => {
         </div>
       </Paper>
 
-      {jobSelected && (
+      {jobSelectedState && (
         <JobModal
-          job={jobSelected!}
+          job={jobSelectedState!}
           show={showModalSteps}
           handleCloseModal={handleToggleModal}
         />
