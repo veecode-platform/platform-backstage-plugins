e054bef208844bbcdff43c5f9cfa835372d10f0f
ValberJunior <valberjr1992@gmail.com>
2024-08-01 14:30:11 -0300

    refactor: 📦 review types

--- plugins/github-workflows/src/context/GithubWorkflowsContext.tsx
+++ plugins/github-workflows/src/context/GithubWorkflowsContext.tsx
@@ -1,18 +1,23 @@
 import { createContext} from "react";
 import { Job, WorkflowResultsProps, WorkflowRun } from "../utils/types";
 import { RestEndpointMethodTypes } from "@octokit/rest";
+import { Entity } from "@backstage/catalog-model";
 
 
 export type GithubWorkflowsContextType = {
-  listAllWorkflows: (filter?: string[]) => Promise<WorkflowResultsProps[] | null | void>,
-  listJobsForWorkflowRun: (id: number) => Promise<Job[]>,
+  entity: Entity,
+  projectName: string,
+  hostname: string,
+  workflowsByAnnotation: string[] | null,
   branch: string | null,
   setBranchState: (branch: string) => void,
   inputsParamsState: object,
   setInputParams: (inputsParams: object) => void,
-  getWorkflowById:(id: number) => Promise<WorkflowRun|null>,
   allWorkflowsState: WorkflowResultsProps[],
   setWorkflowsState: (workflowsParams: WorkflowResultsProps[]) => void,
+  listAllWorkflows: (filter?: string[]) => Promise<WorkflowResultsProps[] | null | void>,
+  listJobsForWorkflowRun: (id: number) => Promise<Job[]>,
+  getWorkflowById:(id: number) => Promise<WorkflowRun|null>,
   handleStartWorkflowRun: (workFlowId: number) => Promise<boolean>,
   handleStopWorkflowRun: (runId: number) => Promise<void>,
   downloadJobLogs: (jobId: number) => Promise<RestEndpointMethodTypes['actions']['downloadJobLogsForWorkflowRun']['response']['data']|null>,
