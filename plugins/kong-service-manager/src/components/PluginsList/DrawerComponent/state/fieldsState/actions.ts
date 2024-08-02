6e867ddd03b35a638453a16b71d944b4cc7c6b7e
ValberJunior <valberjr1992@gmail.com>
2024-08-02 13:36:03 -0300

    feat: ✨ create State folder

---
+++ plugins/kong-service-manager/src/components/PluginsList/DrawerComponent/state/fieldsState/actions.ts
@@ -0,0 +1,18 @@
+import { PluginFieldsResponse } from "../../../../../utils/types";
+
+export const addFields = (fields:PluginFieldsResponse[])=>({
+    type: 'ADD_FIELDS',
+    payload: fields
+} as const);
+
+export const removeFields = (fieldName : string)=>({
+    type: 'REMOVE_FIELD',
+    payload: fieldName
+}as const);
+
+export const updateFields = (field: PluginFieldsResponse)=>({
+    type: 'UPDATE_FIELD',
+    payload: field
+}as const);
+
+export type FieldsActionType = ReturnType<typeof addFields | typeof removeFields | typeof updateFields>;
