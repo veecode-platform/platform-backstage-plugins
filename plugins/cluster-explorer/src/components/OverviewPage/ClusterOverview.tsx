17b75c35aaee31e203af4ae44ba86df7a08691d0
luccas <luccasmtxr@gmail.com>
2024-08-08 15:17:12 -0300

    add logger to cluster explorer proxy api

--- plugins/cluster-explorer/src/components/OverviewPage/ClusterOverview.tsx
+++ plugins/cluster-explorer/src/components/OverviewPage/ClusterOverview.tsx
@@ -6,7 +6,7 @@ import { Progress } from '@backstage/core-components';
 import { useEntity, MissingAnnotationEmptyState } from '@backstage/plugin-catalog-react';
 import { Grid } from '@material-ui/core';
 import OpenInNewIcon from "@material-ui/icons/OpenInNew"
-import { InfoCard, Content, Link, StructuredMetadataTable, Table, StatusOK, StatusError} from '@backstage/core-components';
+import { InfoCard, Content, Link, StructuredMetadataTable, Table, StatusOK, StatusError } from '@backstage/core-components';
 import { ClusterCapacity, ClusterInformation, ClusterLinks, ClusterNamespace, ClusterNodes, ClusterResponse, NamespacesResponse, NodeResponse } from '../../utils/types';
 import { useEntityAnnotations } from '../../hooks';
 import { Entity } from '@backstage/catalog-model';
@@ -24,7 +24,7 @@ export const ClusterOverview = () => {
     const [isOpen, toggleDrawer] = React.useState(false);
     const [nodeInfoState, nodeInfoDispatch] = React.useReducer(NodeInfoReducer, initialNodeInfoState);
     const { entity } = useEntity();
-    const { clusterName,clusterMode } = useEntityAnnotations(entity as Entity);
+    const { clusterName, clusterMode } = useEntityAnnotations(entity as Entity);
     const kubernetesApi = useApi(kubernetesApiRef);
 
     const { loading, error, value } = useAsync(async (): Promise<ClusterResponse> => {
@@ -32,10 +32,13 @@ export const ClusterOverview = () => {
         const namespaces: any = await (await kubernetesApi.proxy({
             clusterName: clusterName,
             path: '/api/v1/namespaces',
-
+        
         })).json();
-
-        if (!namespaces.items) throw new Error(namespaces.message);
+        
+        if (!namespaces.items) {
+            console.log("RESPONSE FROM PROXY NAMESPACES: ", namespaces)
+            throw new Error(namespaces.message);
+        }
 
         const nodes: any = await (await kubernetesApi.proxy({
             clusterName: clusterName,
@@ -67,7 +70,7 @@ export const ClusterOverview = () => {
         if (!services.items) throw new Error(services.message);
 
         const namespacesList: ClusterNamespace[] = namespaces.items?.map((namespace: NamespacesResponse) => {
-            return <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>{<SwitchStatuses status={namespace.status.phase as string}/>}{namespace.metadata?.name}</div>
+            return <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>{<SwitchStatuses status={namespace.status.phase as string} />}{namespace.metadata?.name}</div>
         })
 
         const nodesList: ClusterNodes[] = nodes.items?.map((node: NodeResponse) => {
@@ -91,7 +94,7 @@ export const ClusterOverview = () => {
             }
             return {
                 ...fullInfo,
-                info: <InfoButton info={fullInfo} toggleDrawer={toggleDrawer}  nodeInfoDispatch={nodeInfoDispatch}/>
+                info: <InfoButton info={fullInfo} toggleDrawer={toggleDrawer} nodeInfoDispatch={nodeInfoDispatch} />
             }
         })
 
@@ -120,19 +123,19 @@ export const ClusterOverview = () => {
         const mapedIngressClasses = servicesLoadBalancerList.map(
             (serviceLoadBalancer: { metadata: { labels: { [x: string]: any; }; }; status: { loadBalancer: { ingress: any[]; }; }; }) => {
 
-            const filteredIngressClass = ingressClasses.items?.find(
-                (ingressClass: { metadata?: { labels: { [x: string]: any; }; }; }) =>
-                    ingressClass.metadata?.labels["app.kubernetes.io/instance"] === serviceLoadBalancer.metadata?.labels["app.kubernetes.io/instance"]
-            )
-            if(!filteredIngressClass) return null
-            const ipList = serviceLoadBalancer.status?.loadBalancer?.ingress?.length > 0 ? serviceLoadBalancer.status?.loadBalancer?.ingress.map((ingress: { hostname: any; ip: any; }) => ingress.hostname ? ingress.hostname : ingress.ip).join(",") : "Pending"
-            return {
-                name: filteredIngressClass.metadata.name,
-                version: filteredIngressClass.metadata.labels["app.kubernetes.io/version"],
-                createdAt: filteredIngressClass.metadata.creationTimestamp,
-                ip: ipList
-            }
-        })
+                const filteredIngressClass = ingressClasses.items?.find(
+                    (ingressClass: { metadata?: { labels: { [x: string]: any; }; }; }) =>
+                        ingressClass.metadata?.labels["app.kubernetes.io/instance"] === serviceLoadBalancer.metadata?.labels["app.kubernetes.io/instance"]
+                )
+                if (!filteredIngressClass) return null
+                const ipList = serviceLoadBalancer.status?.loadBalancer?.ingress?.length > 0 ? serviceLoadBalancer.status?.loadBalancer?.ingress.map((ingress: { hostname: any; ip: any; }) => ingress.hostname ? ingress.hostname : ingress.ip).join(",") : "Pending"
+                return {
+                    name: filteredIngressClass.metadata.name,
+                    version: filteredIngressClass.metadata.labels["app.kubernetes.io/version"],
+                    createdAt: filteredIngressClass.metadata.creationTimestamp,
+                    ip: ipList
+                }
+            })
 
         const response: ClusterResponse = {
             nodes: nodesList,
@@ -159,86 +162,86 @@ export const ClusterOverview = () => {
         return response
 
     }, []);
-    
+
     if (!clusterName)
-      return (
-        <Grid item lg={12}>
-          <MissingAnnotationEmptyState
-            annotation={['veecode/cluster-name']}
-          />
-        </Grid>
-      );
+        return (
+            <Grid item lg={12}>
+                <MissingAnnotationEmptyState
+                    annotation={['veecode/cluster-name']}
+                />
+            </Grid>
+        );
 
     if (loading) return <Progress />;
 
-    if (error) return <ClusterNotConfigured error={error}/>
+    if (error) return <ClusterNotConfigured error={error} />
 
-    if(!value) return null;
-    
-        return (
-            <Content>
-                <Grid container spacing={4} direction="row">
-                   <DrawerComponent
-                     isOpen={isOpen}
-                     toggleDrawer={toggleDrawer}
-                     nodeInfoState={nodeInfoState as ClusterNodes}
+    if (!value) return null;
 
-                    />
-                    <Grid item md={3} sm={12} >{/* left-side div: cluster info + capacity*/}
-                        <Grid container spacing={1}>
-                            <Grid item md={12} sm={12} style={{maxHeight: "600px", overflow: "auto"}}>
-                                <InfoCard title="Cluster information">
-                                    <StructuredMetadataTable dense={false} metadata={value.info} />
-                                </InfoCard>
-                            </Grid>
-                            {
-                                value.links ?
-                                    <Grid item md={12}>
-                                        <InfoCard title="Links">
-                                            <div>{value.links.admin}</div>
-                                        </InfoCard>
-                                    </Grid>
-                                    : null
-                            }
-                            <Grid item md={12}>
-                                <InfoCard title="Capacity">
-                                    <StructuredMetadataTable metadata={value.capacity} />
-                                </InfoCard>
-                            </Grid>
+    return (
+        <Content>
+            <Grid container spacing={4} direction="row">
+                <DrawerComponent
+                    isOpen={isOpen}
+                    toggleDrawer={toggleDrawer}
+                    nodeInfoState={nodeInfoState as ClusterNodes}
+
+                />
+                <Grid item md={3} sm={12} >{/* left-side div: cluster info + capacity*/}
+                    <Grid container spacing={1}>
+                        <Grid item md={12} sm={12} style={{ maxHeight: "600px", overflow: "auto" }}>
+                            <InfoCard title="Cluster information">
+                                <StructuredMetadataTable dense={false} metadata={value.info} />
+                            </InfoCard>
                         </Grid>
-                    </Grid>
-
-                   <Grid item md={9} sm={12}> {/* right-side div: node table + ingress classes*/}
-                        <Grid container spacing={2}>
-                            <Grid item md={12}>
-                                <Table
-                                    title="Nodes"
-                                    columns={[
-                                        { title: 'Name', field: 'name' },
-                                        { title: 'Os', field: 'os' },
-                                        { title: 'Arch', field: 'arch' },
-                                        { title: "Info", field: "info" },
-                                        { title: 'Creation', field: 'createdAt' }
-                                    ]}
-                                    data={value.nodes}
-                                    options={{ search: true, paging: true }} />
-                            </Grid>
-                            { clusterMode !== "demo" && ( <Grid item md={12}>
-                                <Table
-                                    title="Ingress classes"
-                                    columns={[
-                                        { title: 'Name', field: 'name' },
-                                        { title: 'Ip', field: 'ip' },
-                                        { title: 'Version', field: 'version' },
-                                        { title: 'Creation', field: 'createdAt' }
-                                    ]}
-                                    data={value.ingressClasses}
-                                    options={{ search: true, paging: true }} />
-                            </Grid>)}
+                        {
+                            value.links ?
+                                <Grid item md={12}>
+                                    <InfoCard title="Links">
+                                        <div>{value.links.admin}</div>
+                                    </InfoCard>
+                                </Grid>
+                                : null
+                        }
+                        <Grid item md={12}>
+                            <InfoCard title="Capacity">
+                                <StructuredMetadataTable metadata={value.capacity} />
+                            </InfoCard>
                         </Grid>
                     </Grid>
                 </Grid>
-            </Content>
-        )
-    
+
+                <Grid item md={9} sm={12}> {/* right-side div: node table + ingress classes*/}
+                    <Grid container spacing={2}>
+                        <Grid item md={12}>
+                            <Table
+                                title="Nodes"
+                                columns={[
+                                    { title: 'Name', field: 'name' },
+                                    { title: 'Os', field: 'os' },
+                                    { title: 'Arch', field: 'arch' },
+                                    { title: "Info", field: "info" },
+                                    { title: 'Creation', field: 'createdAt' }
+                                ]}
+                                data={value.nodes}
+                                options={{ search: true, paging: true }} />
+                        </Grid>
+                        {clusterMode !== "demo" && (<Grid item md={12}>
+                            <Table
+                                title="Ingress classes"
+                                columns={[
+                                    { title: 'Name', field: 'name' },
+                                    { title: 'Ip', field: 'ip' },
+                                    { title: 'Version', field: 'version' },
+                                    { title: 'Creation', field: 'createdAt' }
+                                ]}
+                                data={value.ingressClasses}
+                                options={{ search: true, paging: true }} />
+                        </Grid>)}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Content>
+    )
+
 }
