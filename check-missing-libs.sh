fb8e7ec05639b7e1f0b7676c793efb3051eae87e
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2024-08-12 14:05:21 -0300

    depcheck testing

---
+++ check-missing-libs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+plugins_dir="./plugins"
+
+for dir in "$plugins_dir"/*; do
+  if [ -d "$dir" ]; then
+    package_json="$dir/package.json"
+    if [ -f "$package_json" ]; then
+      echo "--------------------------------------------------------------------------------------------------------------------------"
+      echo "Checking dependencies for $(basename "$dir")..."
+      output=$(npx --yes depcheck ./plugins/kong-service-manager)
+      echo "$output"
+    else
+      echo "No package.json found in $(basename "$dir"), skipping..."
+    fi
+  fi
+done
