ec0cd93a38e6308dcb4b84c4d9bebc5f16baa615
ValberJunior <valberjr1992@gmail.com>
2024-08-27 17:29:25 -0300

    feat: ✨ add hostname props

--- plugins/gitlab-pipelines/src/hooks/useEntityAnnotations.ts
+++ plugins/gitlab-pipelines/src/hooks/useEntityAnnotations.ts
@@ -1,18 +1,26 @@
-import { Entity } from '@backstage/catalog-model';
+import { ANNOTATION_LOCATION, ANNOTATION_SOURCE_LOCATION, Entity } from '@backstage/catalog-model';
 import { JobAnnotationProps } from '../utils/types';
 import { GitlabPipelinesStatus } from '../utils/enums/GitlabPipelinesStatus';
 import { GITLAB_ANNOTATION, GITLAB_JOBS_ANNOTATION } from '../utils/constants';
+import gitUrlParse from 'git-url-parse';
 
 export const useEntityAnnotations = (entity: Entity) => {
   const projectName =
     entity?.metadata.annotations?.[GITLAB_ANNOTATION] ?? '';
   const JobsList =
     entity?.metadata.annotations?.[GITLAB_JOBS_ANNOTATION] ?? '';  
+  const location = 
+    entity?.metadata.annotations?.[ANNOTATION_SOURCE_LOCATION] ??
+    entity?.metadata.annotations?.[ANNOTATION_LOCATION];
+  const hostname = location?.startsWith('url:')
+  ? gitUrlParse(location.slice(4)).resource
+  : 'gitlab.com';
 
     if (!JobsList) {
       return {
         projectName,
         jobsAnnotations: null,
+        hostname
       };
     }
 
@@ -25,7 +33,8 @@ export const useEntityAnnotations = (entity: Entity) => {
   
   return {
     projectName,
-    jobsAnnotations
+    jobsAnnotations,
+    hostname
   };
 
 };
