5a413dace1a38737decb7f5e6ace91f04f27abfd
ValberJunior <valberjr1992@gmail.com>
2024-09-04 09:07:42 -0300

    refactor: 📦 move to folder types

---
+++ plugins/kong-service-manager-common/src/types/index.ts
@@ -0,0 +1,135 @@
+export interface SchemaFields {
+    [key: string]: any;
+}
+
+export interface PluginPerCategory {
+  category: string,
+  plugins: PluginCard[] | []
+}
+
+export interface PluginFieldsResponse {
+    name: string,
+    type: string
+    required: boolean,
+    defaultValue: string | number | boolean | any ,
+    defaultValues?: string | string[] | number | boolean | any ,
+    arrayType: string | number | boolean | any | undefined,
+    arrayOptions: string[] | number[] | boolean[] | any[] | undefined,
+    isMultipleArray: boolean,
+    recordFields?: any
+}
+
+export interface AssociatedPluginsResponse {
+    name: string,
+    id: string,
+    tags: string[],
+    enabled: boolean,
+    createdAt: number,
+    config: any
+}
+
+export interface RoutesResponse{
+    name: string, 
+    protocols: string[],
+    methods: string[],
+    tags: string[],
+    hosts: string[],
+    paths: string[] 
+}
+
+export interface CreatePlugin{
+    config: any,
+    name: string, 
+}
+
+export interface ServiceInfoResponse {
+    tags: string[];
+    ca_certificates: string[];
+    updated_at: number;
+    read_timeout: number;
+    host: string;
+    id: string;
+    retries: number;
+    write_timeout: number;
+    connect_timeout: number;
+    name: string;
+    path: string;
+    tls_verify: boolean;
+    port: number;
+    tls_verify_depth: any;
+    created_at: number;
+    enabled: boolean;
+    client_certificate: any;
+    protocol: string;
+}
+  
+  export interface PluginCard {
+    id?: string | null,
+    name: string,
+    slug: string,
+    associated?: boolean,
+    image: string,
+    tags: string[],
+    description: string,
+  }
+
+  export interface CreateRoute {
+    name?: string;
+    protocols?: string[];
+    methods?: string[];
+    hosts?: string[];
+    paths?: string[];
+    snis?: string[];
+    headers?: {[key: string]: string[]};
+    sources?: {[key: string]: string[]};
+    destinations?: {[key: string]: string[]};
+    https_redirect_status_code?: number;
+    regex_priority?: number;
+    strip_path?: boolean;
+    path_handling?: "v0" | "v1";
+    preserve_host?: boolean;
+    request_buffering?: boolean;
+    response_buffering?: boolean;
+    tags?: string[];
+  }
+
+  export interface RouteResponse{
+    name: string,
+    protocols: string[],
+    methods: string[],
+    tags: string[],
+    hosts: string[],
+    paths: string[] 
+    snis: string[],
+    headers: {[key: string]: string[]},
+    sources: {[key: string]: string[]},
+    destinations: {[key: string]: string[]},
+    https_redirect_status_code: number,
+    regex_priority: number,
+    strip_path: boolean,
+    preserve_host: boolean,
+    request_buffering: boolean,
+    response_buffering: boolean,
+  }
+
+  export interface HeaderObj {
+    id: string;
+    value: string;
+  }
+
+  export interface Headers {
+    headers: {
+        [key: string]: string[];
+    };
+  }
+
+  export interface Destination {
+    ip: string;
+    port: number;
+  }
+
+  export interface Sources {
+    ip: string;
+    port: number;
+  }
+
