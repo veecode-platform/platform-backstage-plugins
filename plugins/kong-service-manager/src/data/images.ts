89bedea85d37452b1b8f250a23a42c7597d5f56e
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2024-09-05 15:50:41 -0300

    update images

--- plugins/kong-service-manager/src/data/images.ts
+++ plugins/kong-service-manager/src/data/images.ts
@@ -1,178 +1,202 @@
-// AI
-import AiProxyImg from "../assets/ai/kong-inc_ai-proxy.png";
-import AiPromptDecoratorImg from "../assets/ai/kong-inc_ai-prompt-decorator.png";
-import AiPromptGuardImg from "../assets/ai/kong-inc_ai-prompt-guard.png";
-import AiPromptTemplateImg from "../assets/ai/kong-inc_ai-prompt-template.png";
-import AiRequestTransformerImg from "../assets/ai/kong-inc_ai-request-transformer.png";
-import AiResponseTransformerImg from "../assets/ai/kong-inc_ai-response-transformer.png";
-// Auth
-import BasicAuthImg from "../assets/auth/kong-inc_basic-auth.png";
-import HmacAuthImg from "../assets/auth/kong-inc_hmac-auth.png";
-import JweDecryptImg from "../assets/auth/kong-inc_jwe-decrypt.png";
-import JwtImg from "../assets/auth/kong-inc_jwt.png";
-import KeyAuthImg from "../assets/auth/kong-inc_key-auth.png";
-import KeyAuthEncImg from "../assets/auth/kong-inc_key-auth-enc.png";
-import JwtSignerImg from "../assets/auth/kong-inc_jwt-signer.png";
-import LdapAuthImg from "../assets/auth/kong-inc_ldap-auth.png";
-import LdapAuthAdvancedImg from "../assets/auth/kong-inc_ldap-auth-advanced.png";
-import MtlsAuthImg from "../assets/auth/kong-inc_mtls-auth.png";
-import Oauth2Img from "../assets/auth/kong-inc_oauth2.png";
-import Oauth2IntrospectionImg from "../assets/auth/kong-inc_oauth2-introspection.png";
-import OpenidConnectImg from "../assets/auth/kong-inc_openid-connect.png";
-import AppRegistrationImg from "../assets/auth/kong-inc_application-registration.png";
-import SamlImg from "../assets/auth/kong-inc_saml.png";
-import SessionImg from "../assets/auth/kong-inc_session.png";
-import VaultAuthImg from "../assets/auth/kong-inc_vault-auth.png";
-import OktaImg from "../assets/auth/okta_okta.png";
-// Security
-import AcmeImg from "../assets/security/kong-inc_acme.png";
-import BotDetectionImg from "../assets/security/kong-inc_bot-detection.png";
-import CorsImg from "../assets/security/kong-inc_cors.png";
-import IpRestrictionImg from "../assets/security/kong-inc_ip-restriction.png";
-import OpaImg from "../assets/security/kong-inc_opa.png";
-import TlsHandshakeModifierImg from "../assets/security/kong-inc_tls-handshake-modifier.png";
-import TlsMetadataHeadersImg from "../assets/security/kong-inc_tls-metadata-headers.png";
-import ImpervaApiSecurityImg from "../assets/security/imperva_imp-appsec-connector.png";
-// Traffic Control
-import AclImg from "../assets/traffic-control/kong-inc_acl.png";
-import CanaryImg from "../assets/traffic-control/kong-inc_canary.png";
-import ForwardProxyImg from "../assets/traffic-control/kong-inc_forward-proxy.png";
-import GraphQLProxyCacheAdvancedImg from "../assets/traffic-control/kong-inc_graphql-proxy-cache-advanced.png";
-import GraphQLRateLimitingAdvancedImg from "../assets/traffic-control/kong-inc_graphql-rate-limiting-advanced.png";
-import MockingImg from "../assets/traffic-control/kong-inc_mocking.png";
-import OasValidationImg from "../assets/traffic-control/kong-inc_oas-validation.png";
-import ProxyCacheImg from "../assets/traffic-control/kong-inc_proxy-cache.png";
-import ProxyCacheAdvancedImg from "../assets/traffic-control/kong-inc_proxy-cache-advanced.png";
-import RateLimitingImg from "../assets/traffic-control/kong-inc_rate-limiting.png";
-import RateLimitingAdvancedImg from "../assets/traffic-control/kong-inc_rate-limiting-advanced.png";
-import RequestSizeLimitingImg from "../assets/traffic-control/kong-inc_request-size-limiting.png";
-import RequestTerminationImg from "../assets/traffic-control/kong-inc_request-termination.png";
-import RequestValidatorImg from "../assets/traffic-control/kong-inc_request-validator.png";
-import ResponseRateLimitingImg from "../assets/traffic-control/kong-inc_response-ratelimiting.png";
-import RouteByHeaderImg from "../assets/traffic-control/kong-inc_route-by-header.png";
-import UpstreamTimeoutImg from "../assets/traffic-control/kong-inc_upstream-timeout.png";
-import WebSocketSizeLimitImg from "../assets/traffic-control/kong-inc_websocket-size-limit.png";
-import WebSocketValidatorImg from "../assets/traffic-control/kong-inc_websocket-validator.png";
-import XmlThreatProtectionImg from "../assets/traffic-control/kong-inc_xml-threat-protection.png";
-// Serverless
-import AwsLambdaImg from "../assets/serverless/kong-inc_aws-lambda.png";
-import OpenWhiskImg from "../assets/serverless/kong-inc_openwhisk.png";
-import AzureFunctionsImg from "../assets/serverless/kong-inc_azure-functions.png";
-import PosFunctionImg from "../assets/serverless/kong-inc_post-function.png";
-import PreFunctionImg from "../assets/serverless/kong-inc_pre-function.png";
-// Analitics & Monitoring
-import AppDynamicsImg from "../assets/analytics-monitoring/kong-inc_app-dynamics.png";
-import DatadogImg from "../assets/analytics-monitoring/kong-inc_datadog.png";
-import OpenTelemetryImg from "../assets/analytics-monitoring/kong-inc_opentelemetry.png";
-import PrometheusImg from "../assets/analytics-monitoring/kong-inc_prometheus.png";
-import StatsDImg from "../assets/analytics-monitoring/kong-inc_statsd.png";
-import StatsDAdvancedImg from "../assets/analytics-monitoring/kong-inc_statsd-advanced.png";
-import ZipkinImg from "../assets/analytics-monitoring/kong-inc_zipkin.png";
-// Transformations
-import CorrelationIdImg from "../assets/transformations/kong-inc_correlation-id.png";
-import DeGraphQLImg from "../assets/transformations/kong-inc_degraphql.png";
-import ExitTransformerImg from "../assets/transformations/kong-inc_exit-transformer.png";
-import KafkaUpstreamImg from "../assets/transformations/kong-inc_kafka-upstream.png";
-import RequestTransformerImg from "../assets/transformations/kong-inc_request-transformer.png";
-import RequestTransformerAdvancedImg from "../assets/transformations/kong-inc_request-transformer-advanced.png";
-import ResponseTransformerImg from "../assets/transformations/kong-inc_response-transformer.png";
-import ResponseTransformerAdvancedImg from "../assets/transformations/kong-inc_response-transformer-advanced.png";
-import RouteTransformerAdvancedImg from "../assets/transformations/kong-inc_route-transformer-advanced.png";
-import GrpcWebImg from "../assets/transformations/kong-inc_grpc-web.png";
-import GrpcGatewayImg from "../assets/transformations/kong-inc_grpc-gateway.png";
-import JqImg from "../assets/transformations/kong-inc_jq.png";
-// Logging
-import FileLogImg from "../assets/logging/kong-inc_file-log.png";
-import HttpLogImg from "../assets/logging/kong-inc_http-log.png";
-import KafkaLogImg from "../assets/logging/kong-inc_kafka-log.png";
-import LogglyImg from "../assets/logging/kong-inc_loggly.png";
-import SyslogImg from "../assets/logging/kong-inc_syslog.png";
-import TcpLogImg from "../assets/logging/kong-inc_tcp-log.png";
-import UdpLogImg from "../assets/logging/kong-inc_udp-log.png";
-
+// Warning: This file is generated by the `plugins-updater` script.
+import KongPluginMoesifImg from "../assets/plugins/moesif_kong-plugin-moesif.png";
+import ResponseTransformerAdvancedImg from "../assets/plugins/kong-inc_response-transformer-advanced.png";
+import ImpAppsecConnectorImg from "../assets/plugins/imperva_imp-appsec-connector.png";
+import ProxyCacheAdvancedImg from "../assets/plugins/kong-inc_proxy-cache-advanced.png";
+import KeyAuthImg from "../assets/plugins/kong-inc_key-auth.png";
+import KongPluginAmberfloImg from "../assets/plugins/amberflo_kong-plugin-amberflo.png";
+import JwtImg from "../assets/plugins/kong-inc_jwt.png";
+import TraceableaiImg from "../assets/plugins/traceableai_traceableai.png";
+import FileLogImg from "../assets/plugins/kong-inc_file-log.png";
+import SessionImg from "../assets/plugins/kong-inc_session.png";
+import LogglyImg from "../assets/plugins/kong-inc_loggly.png";
+import RequestTransformerImg from "../assets/plugins/kong-inc_request-transformer.png";
+import Oauth2IntrospectionImg from "../assets/plugins/kong-inc_oauth2-introspection.png";
+import AcmeImg from "../assets/plugins/kong-inc_acme.png";
+import WallarmImg from "../assets/plugins/wallarm_wallarm.png";
+import JqImg from "../assets/plugins/kong-inc_jq.png";
+import LdapAuthAdvancedImg from "../assets/plugins/kong-inc_ldap-auth-advanced.png";
+import RequestTerminationImg from "../assets/plugins/kong-inc_request-termination.png";
+import TlsHandshakeModifierImg from "../assets/plugins/kong-inc_tls-handshake-modifier.png";
+import IpRestrictionImg from "../assets/plugins/kong-inc_ip-restriction.png";
+import OpenidConnectImg from "../assets/plugins/kong-inc_openid-connect.png";
+import MockingImg from "../assets/plugins/kong-inc_mocking.png";
+import NonamesecurityKongtrafficsourceImg from "../assets/plugins/nonamesecurity_nonamesecurity-kongtrafficsource.png";
+import KongUpstreamJwtImg from "../assets/plugins/optum_kong-upstream-jwt.png";
+import XmlThreatProtectionImg from "../assets/plugins/kong-inc_xml-threat-protection.png";
+import MtlsAuthImg from "../assets/plugins/kong-inc_mtls-auth.png";
+import SyslogImg from "../assets/plugins/kong-inc_syslog.png";
+import OktaImg from "../assets/plugins/okta_okta.png";
+import RequestValidatorImg from "../assets/plugins/kong-inc_request-validator.png";
+import StatsdAdvancedImg from "../assets/plugins/kong-inc_statsd-advanced.png";
+import UpstreamTimeoutImg from "../assets/plugins/kong-inc_upstream-timeout.png";
+import CanaryImg from "../assets/plugins/kong-inc_canary.png";
+import ResponseRatelimitingImg from "../assets/plugins/kong-inc_response-ratelimiting.png";
+import BotDetectionImg from "../assets/plugins/kong-inc_bot-detection.png";
+import DatadogImg from "../assets/plugins/kong-inc_datadog.png";
+import OpaImg from "../assets/plugins/kong-inc_opa.png";
+import ResponseTransformerImg from "../assets/plugins/kong-inc_response-transformer.png";
+import KongResponseSizeLimitingImg from "../assets/plugins/optum_kong-response-size-limiting.png";
+import RequestSizeLimitingImg from "../assets/plugins/kong-inc_request-size-limiting.png";
+import LdapAuthImg from "../assets/plugins/kong-inc_ldap-auth.png";
+import AppsentinelsImg from "../assets/plugins/appsentinels_appsentinels.png";
+import AiRequestTransformerImg from "../assets/plugins/kong-inc_ai-request-transformer.png";
+import OasValidationImg from "../assets/plugins/kong-inc_oas-validation.png";
+import RateLimitingAdvancedImg from "../assets/plugins/kong-inc_rate-limiting-advanced.png";
+import KafkaLogImg from "../assets/plugins/kong-inc_kafka-log.png";
+import ZipkinImg from "../assets/plugins/kong-inc_zipkin.png";
+import KeyAuthEncImg from "../assets/plugins/kong-inc_key-auth-enc.png";
+import VaultAuthImg from "../assets/plugins/kong-inc_vault-auth.png";
+import OpentelemetryImg from "../assets/plugins/kong-inc_opentelemetry.png";
+import KongSplunkLogImg from "../assets/plugins/optum_kong-splunk-log.png";
+import SaltImg from "../assets/plugins/salt_salt.png";
+import HmacAuthImg from "../assets/plugins/kong-inc_hmac-auth.png";
+import OpenwhiskImg from "../assets/plugins/kong-inc_openwhisk.png";
+import UdpLogImg from "../assets/plugins/kong-inc_udp-log.png";
+import ForwardProxyImg from "../assets/plugins/kong-inc_forward-proxy.png";
+import TlsMetadataHeadersImg from "../assets/plugins/kong-inc_tls-metadata-headers.png";
+import GrpcGatewayImg from "../assets/plugins/kong-inc_grpc-gateway.png";
+import AiPromptDecoratorImg from "../assets/plugins/kong-inc_ai-prompt-decorator.png";
+import PostFunctionImg from "../assets/plugins/kong-inc_post-function.png";
+import RateLimitingImg from "../assets/plugins/kong-inc_rate-limiting.png";
+import WebsocketSizeLimitImg from "../assets/plugins/kong-inc_websocket-size-limit.png";
+import KongSpecExposeImg from "../assets/plugins/optum_kong-spec-expose.png";
+import DegraphqlImg from "../assets/plugins/kong-inc_degraphql.png";
+import RouteTransformerAdvancedImg from "../assets/plugins/kong-inc_route-transformer-advanced.png";
+import ProxyCacheImg from "../assets/plugins/kong-inc_proxy-cache.png";
+import AiProxyImg from "../assets/plugins/kong-inc_ai-proxy.png";
+import CorrelationIdImg from "../assets/plugins/kong-inc_correlation-id.png";
+import SamlImg from "../assets/plugins/kong-inc_saml.png";
+import CorsImg from "../assets/plugins/kong-inc_cors.png";
+import AiPromptGuardImg from "../assets/plugins/kong-inc_ai-prompt-guard.png";
+import AiAzureContentSafetyImg from "../assets/plugins/kong-inc_ai-azure-content-safety.png";
+import ApplicationRegistrationImg from "../assets/plugins/kong-inc_application-registration.png";
+import ExitTransformerImg from "../assets/plugins/kong-inc_exit-transformer.png";
+import AwsLambdaImg from "../assets/plugins/kong-inc_aws-lambda.png";
+import AiRateLimitingAdvancedImg from "../assets/plugins/kong-inc_ai-rate-limiting-advanced.png";
+import GrpcWebImg from "../assets/plugins/kong-inc_grpc-web.png";
+import GraphqlProxyCacheAdvancedImg from "../assets/plugins/kong-inc_graphql-proxy-cache-advanced.png";
+import StatsdImg from "../assets/plugins/kong-inc_statsd.png";
+import GraphqlRateLimitingAdvancedImg from "../assets/plugins/kong-inc_graphql-rate-limiting-advanced.png";
+import Oauth2Img from "../assets/plugins/kong-inc_oauth2.png";
+import KongServiceVirtualizationImg from "../assets/plugins/optum_kong-service-virtualization.png";
+import PrometheusImg from "../assets/plugins/kong-inc_prometheus.png";
+import JweDecryptImg from "../assets/plugins/kong-inc_jwe-decrypt.png";
+import AzureFunctionsImg from "../assets/plugins/kong-inc_azure-functions.png";
+import JwtSignerImg from "../assets/plugins/kong-inc_jwt-signer.png";
+import PreFunctionImg from "../assets/plugins/kong-inc_pre-function.png";
+import RequestTransformerAdvancedImg from "../assets/plugins/kong-inc_request-transformer-advanced.png";
+import WebsocketValidatorImg from "../assets/plugins/kong-inc_websocket-validator.png";
+import BasicAuthImg from "../assets/plugins/kong-inc_basic-auth.png";
+import KafkaUpstreamImg from "../assets/plugins/kong-inc_kafka-upstream.png";
+import AppDynamicsImg from "../assets/plugins/kong-inc_app-dynamics.png";
+import AiPromptTemplateImg from "../assets/plugins/kong-inc_ai-prompt-template.png";
+import AwsRequestSigningImg from "../assets/plugins/TheLEGOGroup_aws-request-signing.png";
+import KongPluginDatadomeImg from "../assets/plugins/datadome_kong-plugin-datadome.png";
+import AclImg from "../assets/plugins/kong-inc_acl.png";
+import AiResponseTransformerImg from "../assets/plugins/kong-inc_ai-response-transformer.png";
+import HttpLogImg from "../assets/plugins/kong-inc_http-log.png";
+import RouteByHeaderImg from "../assets/plugins/kong-inc_route-by-header.png";
+import TcpLogImg from "../assets/plugins/kong-inc_tcp-log.png";
 
 export {
-    AiProxyImg,
-    AiPromptDecoratorImg,
-    AiPromptGuardImg,
-    AiPromptTemplateImg,
-    AiRequestTransformerImg,
-    AiResponseTransformerImg,
-    BasicAuthImg,
-    HmacAuthImg,
-    JweDecryptImg,
-    JwtImg,
-    KeyAuthImg,
-    KeyAuthEncImg,
-    JwtSignerImg,
-    LdapAuthImg,
-    LdapAuthAdvancedImg,
-    MtlsAuthImg,
-    Oauth2Img,
-    Oauth2IntrospectionImg,
-    OpenidConnectImg,
-    AppRegistrationImg,
-    SamlImg,
-    SessionImg,
-    VaultAuthImg,
-    OktaImg,
-    AcmeImg,
-    BotDetectionImg,
-    CorsImg,
-    IpRestrictionImg,
-    OpaImg,
-    TlsHandshakeModifierImg,
-    TlsMetadataHeadersImg,
-    ImpervaApiSecurityImg,
-    AclImg,
-    CanaryImg,
-    ForwardProxyImg,
-    GraphQLProxyCacheAdvancedImg,
-    GraphQLRateLimitingAdvancedImg,
-    MockingImg,
-    OasValidationImg,
-    ProxyCacheImg,
-    ProxyCacheAdvancedImg,
-    RateLimitingImg,
-    RateLimitingAdvancedImg,
-    RequestSizeLimitingImg,
-    RequestTerminationImg,
-    RequestValidatorImg,
-    ResponseRateLimitingImg,
-    RouteByHeaderImg,
-    UpstreamTimeoutImg,
-    WebSocketSizeLimitImg,
-    WebSocketValidatorImg,
-    XmlThreatProtectionImg,
-    AwsLambdaImg,
-    OpenWhiskImg,
-    AzureFunctionsImg,
-    PosFunctionImg,
-    PreFunctionImg,
-    AppDynamicsImg,
-    DatadogImg,
-    OpenTelemetryImg,
-    PrometheusImg,
-    StatsDImg,
-    StatsDAdvancedImg,
-    ZipkinImg,
-    CorrelationIdImg,
-    DeGraphQLImg,
-    ExitTransformerImg,
-    KafkaUpstreamImg,
-    RequestTransformerImg,
-    RequestTransformerAdvancedImg,
-    ResponseTransformerImg,
-    ResponseTransformerAdvancedImg,
-    RouteTransformerAdvancedImg,
-    GrpcWebImg,
-    GrpcGatewayImg,
-    JqImg,
-    FileLogImg,
-    HttpLogImg,
-    KafkaLogImg,
-    LogglyImg,
-    SyslogImg,
-    TcpLogImg,
-    UdpLogImg,
+PostFunctionImg,
+Oauth2IntrospectionImg,
+ForwardProxyImg,
+RouteByHeaderImg,
+WebsocketSizeLimitImg,
+KongSpecExposeImg,
+KongUpstreamJwtImg,
+KongResponseSizeLimitingImg,
+StatsdImg,
+OktaImg,
+AclImg,
+NonamesecurityKongtrafficsourceImg,
+OpenidConnectImg,
+KongPluginMoesifImg,
+ApplicationRegistrationImg,
+UdpLogImg,
+MtlsAuthImg,
+GraphqlProxyCacheAdvancedImg,
+VaultAuthImg,
+AiRequestTransformerImg,
+AwsRequestSigningImg,
+TraceableaiImg,
+GraphqlRateLimitingAdvancedImg,
+GrpcWebImg,
+WallarmImg,
+RequestSizeLimitingImg,
+KongPluginAmberfloImg,
+OpenwhiskImg,
+TlsHandshakeModifierImg,
+RateLimitingAdvancedImg,
+DatadogImg,
+KeyAuthImg,
+ZipkinImg,
+AiResponseTransformerImg,
+RequestTransformerAdvancedImg,
+ResponseTransformerAdvancedImg,
+ProxyCacheImg,
+KeyAuthEncImg,
+ExitTransformerImg,
+JwtImg,
+JweDecryptImg,
+KongSplunkLogImg,
+SyslogImg,
+Oauth2Img,
+ResponseTransformerImg,
+AiAzureContentSafetyImg,
+AiPromptDecoratorImg,
+OasValidationImg,
+PrometheusImg,
+AcmeImg,
+MockingImg,
+AiRateLimitingAdvancedImg,
+BotDetectionImg,
+AppDynamicsImg,
+KongPluginDatadomeImg,
+OpaImg,
+HttpLogImg,
+BasicAuthImg,
+CanaryImg,
+RateLimitingImg,
+RequestTerminationImg,
+ImpAppsecConnectorImg,
+AppsentinelsImg,
+WebsocketValidatorImg,
+GrpcGatewayImg,
+OpentelemetryImg,
+TcpLogImg,
+ProxyCacheAdvancedImg,
+CorrelationIdImg,
+ResponseRatelimitingImg,
+UpstreamTimeoutImg,
+DegraphqlImg,
+AzureFunctionsImg,
+LdapAuthImg,
+TlsMetadataHeadersImg,
+JqImg,
+SamlImg,
+IpRestrictionImg,
+PreFunctionImg,
+CorsImg,
+KongServiceVirtualizationImg,
+AwsLambdaImg,
+StatsdAdvancedImg,
+RequestValidatorImg,
+XmlThreatProtectionImg,
+AiPromptGuardImg,
+SaltImg,
+AiPromptTemplateImg,
+LogglyImg,
+RequestTransformerImg,
+KafkaLogImg,
+HmacAuthImg,
+SessionImg,
+FileLogImg,
+AiProxyImg,
+LdapAuthAdvancedImg,
+KafkaUpstreamImg,
+RouteTransformerAdvancedImg,
+JwtSignerImg
 };
