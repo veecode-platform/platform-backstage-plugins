0153c1ddb937155922daa028022915a1ee68d852
ValberJunior <valberjr1992@gmail.com>
2024-09-05 12:33:07 -0300

    refactor: 📦 remove workspace

--- plugins/kong-service-manager/src/hooks/useEntityAnnotation.ts
+++ plugins/kong-service-manager/src/hooks/useEntityAnnotation.ts
@@ -1,17 +1,16 @@
 import { Entity } from '@backstage/catalog-model';
-import { KONG_SERVICE_INSTANCE_ANNOTATION, KONG_SERVICE_NAME_ANNOTATION, KONG_SERVICE_WORKSPACE } from '../utils/constants/kongAnnotations';
+import { KONG_SERVICE_INSTANCE_ANNOTATION, KONG_SERVICE_NAME_ANNOTATION } from '../utils/constants/kongAnnotations';
 
 
 export const useEntityAnnotation = (entity: Entity) => {
     const serviceName = entity.metadata.annotations?.[KONG_SERVICE_NAME_ANNOTATION] ?? null;
     const kongInstance = entity.metadata.annotations?.[KONG_SERVICE_INSTANCE_ANNOTATION] ?? null;
-    const workspace = entity.metadata.annotations?.[KONG_SERVICE_WORKSPACE] ?? 'default';
+
 
     if (!kongInstance) {
         return {
           serviceName,
-          kongInstance: '/kong-manager/api',
-          workspace
+          kongInstance: null,
         };
       }
   
@@ -19,7 +18,6 @@ export const useEntityAnnotation = (entity: Entity) => {
 
     return {
         serviceName,
-        kongInstances,
-        workspace
+        kongInstances
     }
 }
