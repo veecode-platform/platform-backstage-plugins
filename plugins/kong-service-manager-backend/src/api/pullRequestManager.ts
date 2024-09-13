8293506f8f2bb0d1f800558ce1470c4aeb62f6e9
ValberJunior <valberjr1992@gmail.com>
2024-09-13 11:43:38 -0300

    feat: ✨ create aclass

---
+++ plugins/kong-service-manager-backend/src/api/pullRequestManager.ts
@@ -0,0 +1,13 @@
+import gitUrlParse from 'git-url-parse';
+
+export class PullRequestManager {
+
+    constructor(
+        private location : string
+    ){}
+
+    getRepoInfo(location:string){
+
+    }
+
+}
