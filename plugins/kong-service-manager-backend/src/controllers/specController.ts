5a2aa5be1ae2693464af9956091dbab78b298681
ValberJunior <valberjr1992@gmail.com>
2024-09-13 10:52:46 -0300

    feat: ✨ update getSpecsByEntity in  SpecController and create updateSpec method

--- plugins/kong-service-manager-backend/src/controllers/specController.ts
+++ plugins/kong-service-manager-backend/src/controllers/specController.ts
@@ -6,12 +6,13 @@ import { InputError, stringifyError } from "@backstage/errors";
 export class SpecController extends KongController implements ISpecController{
 
     getSpecsByEntity= async (req:Request, res:Response) => {
-        const { kind, name } = req.params;
+        const { kind, entityName } = req.params;
+
         try{    
           const specs = await this.kongServiceManagerApi
           .getPluginsFromSpec(
             kind,
-            name
+            entityName
           );
 
           res
@@ -29,6 +30,34 @@ export class SpecController extends KongController implements ISpecController{
               }
               throw err;  
         }
+    }
+
+    updateSpec = async (req:Request, res:Response) => {
+      const { specName } = req.params;
+      const { plugins } = req.body;
+      
+      try{ 
+        const response = await this.kongServiceManagerApi
+        .applyPluginsToSpec(
+          specName,
+           plugins
+          );
+
+        res
+        .status(200)
+        .json({
+          spec: response
+        })
+      }catch(err:any){
+        if (err.errors) {
+          throw new InputError(
+            `Unable to update in spec [${specName}]:
+             ${stringifyError(err.errors)}`,
+          );
+        }
+        throw err;  
+      }
+
     }
 
 }
