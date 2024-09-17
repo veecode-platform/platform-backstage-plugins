72ad8bd836556bc0ae0cf8772477987807a90bb9
ValberJunior <valberjr1992@gmail.com>
2024-09-17 19:42:10 -0300

    feat: ✨ add props

--- plugins/kong-service-manager/src/components/SpecList/SpecPluginsList/SpecPluginsList.tsx
+++ plugins/kong-service-manager/src/components/SpecList/SpecPluginsList/SpecPluginsList.tsx
@@ -6,8 +6,8 @@ import { Box, Typography } from '@material-ui/core';
 import { PluginsTable } from './pluginsTable';
 
 
-export const SpecPluginsList = () => {
- 
+export const SpecPluginsList  = () => {
+
   const { specName } = useParams();
   const { root, content,titleBar } = useSpecPluginsListStyles();
 
@@ -18,7 +18,7 @@ export const SpecPluginsList = () => {
            <div className={titleBar}>
             <Typography variant="h6">Plugins associated to service</Typography>
            </div>
-            <PluginsTable />
+            <PluginsTable specName={specName!} />
          </Box>
        </div>
     </BoxComponent>
