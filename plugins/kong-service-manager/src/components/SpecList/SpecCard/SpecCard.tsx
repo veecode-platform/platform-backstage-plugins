72684006c44033a286952d488e9f68c645aeb744
ValberJunior <valberjr1992@gmail.com>
2024-09-17 12:48:21 -0300

    refactor: 📦 update SpecCard Component

--- plugins/kong-service-manager/src/components/SpecList/SpecCard/SpecCard.tsx
+++ plugins/kong-service-manager/src/components/SpecList/SpecCard/SpecCard.tsx
@@ -4,6 +4,7 @@ import { useSpecCardStyles } from './styles';
 import { Box, Chip, Typography } from '@material-ui/core';
 import { MdOpenInNew } from 'react-icons/md';
 import { FaUser } from 'react-icons/fa6';
+import { Link } from '@backstage/core-components';
 
 export const SpecCard : React.FC<SpecCardProps> = (props) => {
   
@@ -14,7 +15,7 @@ export const SpecCard : React.FC<SpecCardProps> = (props) => {
     <Box className={root}>
         <div className={cardHeader}>
             <Typography variant="h6">{title}</Typography>
-            <MdOpenInNew size={26} color="#CDCDCD" style={{ cursor: 'pointer'}}/>
+            <Link to={`${title}`}><MdOpenInNew size={26} color="#CDCDCD" style={{ cursor: 'pointer'}}/></Link>
         </div>
         <div className={cardBody}>
             <Typography variant="body1">{description.length <= 3 ? 'No description...' : description}</Typography>
