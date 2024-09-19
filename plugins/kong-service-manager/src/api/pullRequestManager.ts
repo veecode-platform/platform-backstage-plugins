04be60464b9a5c5171e556a2ef5560dafcbf4fba
ValberJunior <valberjr1992@gmail.com>
2024-09-19 14:45:50 -0300

    feat: ✨ add gitlabManager prop

--- plugins/kong-service-manager/src/api/pullRequestManager.ts
+++ plugins/kong-service-manager/src/api/pullRequestManager.ts
@@ -2,13 +2,15 @@ import { ScmAuthApi } from "@backstage/integration-react";
 import { parseGitUrl } from "../utils/helpers/parseGitUrl";
 import { GithubManager } from "./git-providers/Github";
 import { ConfigApi } from "@backstage/core-plugin-api";
+import { GitLabManager } from "./git-providers/Gitlab";
 
 export class PullRequestManager {
 
     constructor(
         private readonly scmAuthApi: ScmAuthApi,
         private readonly configApi: ConfigApi,
-        private readonly githubManager = new GithubManager(this.scmAuthApi, this.configApi)
+        private readonly githubManager = new GithubManager(this.scmAuthApi, this.configApi),
+        private readonly gitlabManager = new GitLabManager(this.scmAuthApi, this.configApi)
     ){}
 
     async createPullRequest(
@@ -23,6 +25,9 @@ export class PullRequestManager {
             case url.includes('github'): {
                return this.githubManager.createPullRequest(url,fileContent,title,message);
             }
+            case url.includes('gitlab'): {
+                return this.gitlabManager.createPullRequest(url,fileContent,title,message);
+             }
             default:
               throw new Error('unimplemented!');
         }
