933d58889728323e871e67017a829ad55644308e
ValberJunior <valberjr1992@gmail.com>
2024-09-19 13:12:53 -0300

    refactor: 📦 component review

--- plugins/kong-service-manager/src/components/SpecList/SpecList.tsx
+++ plugins/kong-service-manager/src/components/SpecList/SpecList.tsx
@@ -6,10 +6,12 @@ import { ISpec } from '@veecode-platform/backstage-plugin-kong-service-manager-c
 import useAsync from 'react-use/esm/useAsync'
 import { SpecCard } from './SpecCard'
 import { useSpecListStyles } from './styles'
+import { addSelectedSpec } from '../../context/state'
 
 export const SpecList = () => {
   
-  const { getSpecs } = useKongServiceManagerContext();
+  const [ selectedSpec, setSelectedSpec ] = React.useState<string>('');
+  const { getSpecs, selectedSpecDispatch } = useKongServiceManagerContext();
   const { content } = useSpecListStyles();
 
   const fetchSpecs = async (): Promise<ISpec[]> => {
@@ -17,8 +19,16 @@ export const SpecList = () => {
     return data;
   };
 
-  const {loading, error, value: allspecs } = useAsync(fetchSpecs,[])
+  const {loading, error, value: allspecs } = useAsync(fetchSpecs,[]);
 
+  React.useEffect(()=>{
+    if(selectedSpec !== '' && allspecs){
+      const specData = allspecs.filter(spec => spec.metadata.name === selectedSpec)[0];
+      selectedSpecDispatch(addSelectedSpec(specData));
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[selectedSpec])
+  
   if(loading) <h1>Carregando</h1>  // TO DO
 
   if(error) <h1>Error</h1>  // TO DO
@@ -36,7 +46,8 @@ export const SpecList = () => {
                 title={spec.metadata.name}
                 description={spec.metadata.description}
                 owner={spec.spec.owner}
-                tags={spec.metadata.tags}            
+                tags={spec.metadata.tags} 
+                setSpec={setSelectedSpec}           
             />
             )
           )}
