134b3ad71752b488126367ea734520720586d89d
ValberJunior <valberjr1992@gmail.com>
2024-09-20 11:47:43 -0300

    feat: ✨ create state

---
+++ plugins/kong-service-manager/src/components/SpecList/PullRequesModal/state/pullRequestResponseState/actions.ts
@@ -0,0 +1,13 @@
+import { PullRequestResponse } from "../../types";
+
+export const addPullRequestResponse = (response: PullRequestResponse) => ({
+    type: 'ADD_PULL_REQUEST_RESPONSE',
+    payload: response
+}as const);
+
+export const removePullRequestResponse = () => ({
+    type: 'REMOVE_PULL_REQUEST_RESPONSE',
+    payload: null
+} as const);
+
+export type PullRequestResponseActionType = ReturnType<typeof addPullRequestResponse | typeof removePullRequestResponse>;
---
+++ plugins/kong-service-manager/src/components/SpecList/PullRequesModal/state/pullRequestResponseState/reducer.ts
@@ -0,0 +1,15 @@
+import { PullRequestResponse } from "../../types";
+import { PullRequestResponseActionType } from "./actions";
+
+export const initialPullRequestResponseState : PullRequestResponse | null = null;
+
+export const PullRequestResponseReducer = (state: PullRequestResponse | null, action: PullRequestResponseActionType):PullRequestResponse | null => {
+    switch (action.type) {
+        case 'ADD_PULL_REQUEST_RESPONSE':
+            return action.payload;
+        case 'REMOVE_PULL_REQUEST_RESPONSE':
+            return null;
+        default:
+            return state;
+    }
+}
