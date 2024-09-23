9e15be70923a0a6855373514f690cd909716d888
ValberJunior <valberjr1992@gmail.com>
2024-09-23 17:37:01 -0300

    feat: ✨ create KONG_SERVICE_SPEC_ANNOTATION const

--- plugins/kong-service-manager/src/utils/constants/kongAnnotations.ts
+++ plugins/kong-service-manager/src/utils/constants/kongAnnotations.ts
@@ -1,2 +1,3 @@
 export const KONG_SERVICE_NAME_ANNOTATION = 'kong-manager/service-name';
 export const KONG_SERVICE_INSTANCE_ANNOTATION = 'kong-manager/instance';
+export const KONG_SERVICE_SPEC_ANNOTATION = 'kong-manager/spec';
