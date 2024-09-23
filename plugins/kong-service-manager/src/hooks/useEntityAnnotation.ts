d121c4d4faa4bbfee97e5a31d38d8417341e8322
ValberJunior <valberjr1992@gmail.com>
2024-09-23 17:36:44 -0300

    refactor: 📦 update return kongSpecs

--- plugins/kong-service-manager/src/hooks/useEntityAnnotation.ts
+++ plugins/kong-service-manager/src/hooks/useEntityAnnotation.ts
@@ -1,11 +1,11 @@
 import { Entity } from '@backstage/catalog-model';
-import { KONG_SERVICE_INSTANCE_ANNOTATION, KONG_SERVICE_NAME_ANNOTATION } from '../utils/constants/kongAnnotations';
+import { KONG_SERVICE_INSTANCE_ANNOTATION, KONG_SERVICE_NAME_ANNOTATION, KONG_SERVICE_SPEC_ANNOTATION } from '../utils/constants/kongAnnotations';
 
 
 export const useEntityAnnotation = (entity: Entity) => {
     const serviceName = entity.metadata.annotations?.[KONG_SERVICE_NAME_ANNOTATION] ?? null;
     const kongInstance = entity.metadata.annotations?.[KONG_SERVICE_INSTANCE_ANNOTATION] ?? null;
-
+    const kongSpec = entity.metadata.annotations?.[KONG_SERVICE_SPEC_ANNOTATION];
 
     if (!kongInstance) {
         return {
@@ -13,11 +13,21 @@ export const useEntityAnnotation = (entity: Entity) => {
           kongInstance: null,
         };
       }
-  
-    const kongInstances = kongInstance.includes(',') ? kongInstance.split(/\s*,\s*/g) : [kongInstance];
+
+    if (!kongSpec) {
+     return {
+         serviceName,
+         kongInstance,
+         kongSpecs: null
+    };
+    }
+
+    const kongInstances = kongInstance?.includes(',') ? kongInstance.split(/\s*,\s*/g) : [kongInstance];
+    const kongSpecs = kongSpec?.includes(',') ? kongSpec.split(/\s*,\s*/g) : [kongSpec];
 
     return {
         serviceName,
-        kongInstances
+        kongInstances,
+        kongSpecs
     }
 }
