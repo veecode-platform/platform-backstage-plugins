2a510858a30fb340a0d075dcd185a663ab4961e9
ValberJunior <valberjr1992@gmail.com>
2024-09-24 19:33:54 -0300

    feat: ✨ add isKongManagerSpecAvailable

--- plugins/kong-service-manager/src/hooks/useKongServiceManager.ts
+++ plugins/kong-service-manager/src/hooks/useKongServiceManager.ts
@@ -1,4 +1,5 @@
 import { Entity } from "@backstage/catalog-model";
-import { KONG_SERVICE_NAME_ANNOTATION } from "../utils/constants/kongAnnotations";
+import { KONG_SERVICE_NAME_ANNOTATION, KONG_SERVICE_SPEC_ANNOTATION } from "../utils/constants/kongAnnotations";
 
 export const isKongServiceManagerAvailable = (entity:Entity) : boolean => !!entity.metadata.annotations?.[KONG_SERVICE_NAME_ANNOTATION];
+export const isKongManagerSpecAvailable = (entity:Entity) : boolean => !!entity.metadata.annotations?.[KONG_SERVICE_SPEC_ANNOTATION];
