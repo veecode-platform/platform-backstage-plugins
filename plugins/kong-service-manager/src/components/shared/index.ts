7b1ff4f8836acadac53667a89c6f5dfaad4424be
ValberJunior <valberjr1992@gmail.com>
2024-09-24 19:32:38 -0300

    refactor: 📦 add new components

--- plugins/kong-service-manager/src/components/shared/index.ts
+++ plugins/kong-service-manager/src/components/shared/index.ts
@@ -2,4 +2,6 @@ export * from './BoxComponent';
 export * from './SelectComponent';
 export * from './EmptyStateComponent';
 export * from './HtmlTooltip';
-export * from './ButtonComponent'
+export * from './ButtonComponent';
+export * from './LoadingComponent';
+export * from './MissingAnnotation';
