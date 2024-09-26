a35eb441837c6ad3ffb076ffd7d1273a807b8725
ValberJunior <valberjr1992@gmail.com>
2024-09-26 15:19:35 -0300

    refactor: 📦 changes in component -- analyze workflowsByAnnotation and add the label and tooltip to workflowItem

--- plugins/github-workflows/src/components/GitubWorkflowsContent/WorkFlowsCards/WorkFlowItem/WorkFlowItem.tsx
+++ plugins/github-workflows/src/components/GitubWorkflowsContent/WorkFlowsCards/WorkFlowItem/WorkFlowItem.tsx
@@ -9,79 +9,71 @@ import { WorkFlowItemProps } from './types';
 import { useWorkflowItemStyles } from './styles';
 import { WorkFlowStatus } from '../../WorkFlowStatus';
 import { useGithuWorkflowsContext } from '../../../../context';
+import { WorkflowAnnotation } from '../../../../utils/types';
 
-
-export const WorkFlowItem : React.FC<WorkFlowItemProps> = (props) => {
-
+export const WorkFlowItem: React.FC<WorkFlowItemProps> = (props) => {
   const [showModal, setShowModal] = React.useState<boolean>(false);
-  const { entity } = useGithuWorkflowsContext();
+  const { entity, workflowsByAnnotation } = useGithuWorkflowsContext();
   const classes = useWorkflowItemStyles();
-  const { id, status, conclusion, workflowName, parameters, lastRunId } = props;
+  const { id, status, conclusion, workflowName, parameters, lastRunId, path } = props;
+
+  const workflow = (path && workflowsByAnnotation)
+    ? workflowsByAnnotation.find((w: WorkflowAnnotation) => w.workflow && path.includes(w.workflow as string))
+    : undefined;
 
   const handleShowModal = () => {
-    setShowModal(!showModal)
-  }
+    setShowModal(!showModal);
+  };
 
-  const handleCICDLogs = (paramsId:string) => {
+  const handleCICDLogs = (paramsId: string) => {
     const baseUrl = window.location.origin;
     const newUrl = `${baseUrl}/catalog/${entity.metadata.namespace}/${entity.kind.toLowerCase()}/${entity.metadata.name}/ci-cd/${paramsId}`;
     window.location.href = newUrl;
-  }
+  };
 
   return (
     <>
-      <Box
-        className={classes.workflow}
-      >
+      <Box className={classes.workflow}>
         <WorkFlowStatus
           status={status}
           conclusion={conclusion ? conclusion : ''}
           icon
         />
 
-        <Tooltip title={workflowName} placement="top">
-            <Typography 
-              onClick={()=>handleCICDLogs(lastRunId as string)}
-              className={classes.name}
-              >
-              {truncateString(workflowName, 12)}
-            </Typography>
+        <Tooltip title={workflow?.tooltip ?? workflowName} placement="top">
+          <Typography
+            onClick={() => handleCICDLogs(lastRunId as string)}
+            className={classes.name}
+          >
+            {truncateString(workflow?.label ?? workflowName, 12)}
+          </Typography>
         </Tooltip>
 
-        <Box
-          role="button"
-          className={classes.clickable}>
+        <Box role="button" className={classes.clickable}>
+          {parameters && parameters.length > 0 && status !== StatusWorkflowEnum.queued && (
+            <Tooltip title="Add Parameters" placement="top">
+              <Box role="button" className={classes.clickable} onClick={handleShowModal}>
+                <SettingsIcon />
+              </Box>
+            </Tooltip>
+          )}
 
-          {(parameters && parameters?.length > 0 && status !== StatusWorkflowEnum.queued) && (
-              <Tooltip title="Add Parameters" placement="top">
-                  <Box
-                    role="button"
-                    className={classes.clickable}
-                    onClick={handleShowModal}
-                  >
-                    <SettingsIcon />
-                  </Box>
-                </Tooltip>) }
-
-            <WorkFlowActions
-              status={status}
-              conclusion={conclusion}
-              workflowId={id} 
-              parameters={parameters}
-              />
+          <WorkFlowActions
+            status={status}
+            conclusion={conclusion}
+            workflowId={id}
+            parameters={parameters}
+          />
         </Box>
-
       </Box>
 
-      {
-        showModal && (
-          <ModalComponent
-            open={showModal}
-            handleModal={handleShowModal}
-            parameters={parameters ? parameters : []}
-          />
-        )
-      }
+      {showModal && (
+        <ModalComponent
+          open={showModal}
+          handleModal={handleShowModal}
+          parameters={parameters ? parameters : []}
+        />
+      )}
     </>
-  )
-}
+  );
+};
