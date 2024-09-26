1b86c1502b9d5d3765d8415c4e9ccc52a2e853c7
ValberJunior <valberjr1992@gmail.com>
2024-09-26 16:25:08 -0300

    refactor: 📦 changes in useEntityAnnotations hook -- add transformJobsAnnotations function

--- plugins/gitlab-pipelines/src/hooks/useEntityAnnotations.ts
+++ plugins/gitlab-pipelines/src/hooks/useEntityAnnotations.ts
@@ -1,17 +1,20 @@
 import { ANNOTATION_LOCATION, ANNOTATION_SOURCE_LOCATION, Entity } from '@backstage/catalog-model';
-import { JobAnnotationProps } from '../utils/types';
-import { GitlabPipelinesStatus } from '../utils/enums/GitlabPipelinesStatus';
 import { GITLAB_ANNOTATION, GITLAB_JOBS_ANNOTATION } from '../utils/constants';
 import gitUrlParse from 'git-url-parse';
+import { transformJobsAnnotations } from '../utils/helpers/transformJobsAnnotations';
 
 export const useEntityAnnotations = (entity: Entity) => {
+
   const projectName =
     entity?.metadata.annotations?.[GITLAB_ANNOTATION] ?? '';
+
   const JobsList =
     entity?.metadata.annotations?.[GITLAB_JOBS_ANNOTATION] ?? '';  
+
   const location = 
     entity?.metadata.annotations?.[ANNOTATION_SOURCE_LOCATION] ??
     entity?.metadata.annotations?.[ANNOTATION_LOCATION];
+
   const hostname = location?.startsWith('url:')
   ? gitUrlParse(location.slice(4)).resource
   : 'gitlab.com';
@@ -23,13 +26,7 @@ export const useEntityAnnotations = (entity: Entity) => {
         hostname
       };
     }
-
-  const jobsAnnotationList = JobsList.includes(',') ? JobsList.split(',') : [JobsList];
-
-  const jobsAnnotations: JobAnnotationProps[] = jobsAnnotationList.map(item => {
-    const [label, varValue] = item.split(':');
-    return { id: label, label, var: varValue, status: GitlabPipelinesStatus.success };
-  });
+  const jobsAnnotations = transformJobsAnnotations(JobsList)
   
   return {
     projectName,
