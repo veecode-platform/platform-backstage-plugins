71dcf4c15269cd4d6e5cf9d86ef9e79ff67aa450
ValberJunior <valberjr1992@gmail.com>
2024-11-01 18:14:53 -0300

    fix: 🐛 githubExtractUrl fix

--- plugins/kong-service-manager/src/utils/helpers/extractGithubInfo.ts
+++ plugins/kong-service-manager/src/utils/helpers/extractGithubInfo.ts
@@ -1,19 +1,19 @@
-export function extractGitHubInfo(url:string) {
-    const regex = /https:\/\/([^\/]+)\/([^\/]+)\/([^\/]+)\/tree\/([^\/]+)\/(.*)\/([^\/]+)$/;
-    const match = url.match(regex);
-  
-    if (!match) {
-      throw new Error("URL inválida");
-    }
-  
-    const [_, host, owner, repo, branch, path, file] = match;
-  
-    return {
-      host,  
-      owner,         
-      repo, 
-      branch,      
-      path,
-      file        
-    };
+export function extractGitHubInfo(url: string) {
+  const regex = /https:\/\/([^\/]+)\/([^\/]+)\/([^\/]+)\/tree\/([^\/]+)(?:\/(.*))?\/([^\/]+)$/;
+  const match = url.match(regex);
+
+  if (!match) {
+    throw new Error("URL inválida");
   }
+
+  const [_, host, owner, repo, branch, path, file] = match;
+
+  return {
+    host,
+    owner,
+    repo,
+    branch,
+    path: path || "",
+    file,
+  };
+}
