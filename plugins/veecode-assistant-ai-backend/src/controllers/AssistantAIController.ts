ee92467815f83f878cb65e527fa080bc3e5f5c39
ValberJunior <valberjr1992@gmail.com>
2024-11-22 19:01:51 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai-backend/src/controllers/AssistantAIController.ts
+++ plugins/veecode-assistant-ai-backend/src/controllers/AssistantAIController.ts
@@ -2,6 +2,10 @@ import { AuthorizeResult, BasicPermission } from "@backstage/plugin-permission-c
 import { OpenAIApi } from "../api";
 import type { HttpAuthService, PermissionsService } from "@backstage/backend-plugin-api";
 import type { Request } from "express";
+import { extractToken } from "../utils/helpers/extractToken";
+import type { LoggerService } from "@backstage/backend-plugin-api";
+import type { Config } from "@backstage/config";
+import { GitManager } from "../api/git/gitManager";
 
 
 export abstract class AssistantAIController {
@@ -9,7 +13,22 @@ export abstract class AssistantAIController {
         protected openAIApi: OpenAIApi,
         protected httpAuth: HttpAuthService,
         protected permissions: PermissionsService,
+        protected config: Config,
+        protected logger: LoggerService
     ){}
+
+    protected getToken(req:Request){
+        const header = req.headers.authorization;
+        if(!header){
+            throw new Error("Error: Not Found Authorization token")
+        }
+        const token = extractToken(header);
+        return token          
+    } 
+
+    protected gitProviderManager(token: string){
+        return new GitManager(this.config, this.logger, token);
+    }
 
     async isRequestAuthorized(req:Request, permission: BasicPermission): Promise<boolean>{
         const credentials = await this.httpAuth.credentials(req);
