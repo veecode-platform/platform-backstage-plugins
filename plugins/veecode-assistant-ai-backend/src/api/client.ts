49a4f36f22384502a2d34873bd702318310376be
ValberJunior <valberjr1992@gmail.com>
2024-12-10 15:52:10 -0300

    refactor: 📦 api review and add methods

--- plugins/veecode-assistant-ai-backend/src/api/client.ts
+++ plugins/veecode-assistant-ai-backend/src/api/client.ts
@@ -1,17 +1,52 @@
 import type { LoggerService } from "@backstage/backend-plugin-api";
 import type { Config } from "@backstage/config";
-import { VeecodeAIConfig } from "../lib/types";
-import { VeecodeAssistantAIConfig } from "../lib";
+import { OpenAIApi } from "./openAI/openAIApi/OpenAIApi";
+import { EngineEnum } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
+import { IVeeCodeAssistantAIClient } from "./types";
 
 
-export abstract class VeeCodeAssistantAIClient {
-    protected config: Config;
-    protected logger: LoggerService;
-    protected AIConfig: VeecodeAIConfig;
-
-    constructor(config:Config, logger:LoggerService){
+export class VeeCodeAssistantAIClient implements IVeeCodeAssistantAIClient {
+    private config: Config;
+    private logger: LoggerService;
+    private engine : string;
+    private openAIApi: OpenAIApi;
+   
+    constructor(config:Config, logger:LoggerService, engine: string){
         this.config = config;
         this.logger = logger;
-        this.AIConfig = new VeecodeAssistantAIConfig(this.config, this.logger);
+        this.engine = engine;
+        this.openAIApi = new OpenAIApi(this.config, this.logger)
+    }
+
+    async submitDataToVectorStore (repoName:string, files:File[]){
+      switch(this.engine){
+        case EngineEnum.openAI:
+            return await this.openAIApi.submitDataToVectorStore(repoName,files);
+        default:
+            throw new Error("No engine found");
+      }
+    }
+
+    async chat (vectorStoreId: string, prompt: string,template?:string, useDataset?:boolean){
+        switch(this.engine){
+            case EngineEnum.openAI:
+                {
+                    const { threadId, assistantId } = await this.openAIApi.startChat(vectorStoreId,useDataset);
+                    const response = await this.openAIApi.getChat(assistantId, threadId, prompt,template);
+                    return {
+                        threadId,
+                        assistantId,
+                        messages: response.messages,
+                        generatedFiles: response.generatedFiles
+                    };
+                }
+            default:
+                throw new Error("No engine found");
+        }
+    }
+
+    async clearHistory (vectorStoreId: string, assistantId: string, threadId: string){
+        const response = await this.openAIApi.clearHistory(vectorStoreId, assistantId, threadId);
+        return response
     }
 }
