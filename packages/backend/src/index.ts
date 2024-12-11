e3c5da22ea75bba87920a823e57afc0e0b2c869e
ValberJunior <valberjr1992@gmail.com>
2024-12-11 19:43:28 -0300

    refactor: 📦 comment

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -62,6 +62,6 @@ backend.add(catalogPluginGitlabFillerProcessorModule);
 
 backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 
-backend.add(import('@veecode-platform/backstage-plugin-veecode-assistant-ai-backend'));
+// backend.add(import('@veecode-platform/backstage-plugin-veecode-assistant-ai-backend'));
 backend.start();
 
