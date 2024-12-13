170a35f160acf2759bdcbae1d6bfd43168bf93c1
ValberJunior <valberjr1992@gmail.com>
2024-12-13 10:55:00 -0300

    feat: ✨

--- plugins/veecode-assistant-ai/src/routes.ts
+++ plugins/veecode-assistant-ai/src/routes.ts
@@ -1,5 +1,17 @@
-import { createRouteRef } from '@backstage/core-plugin-api';
+import { createRouteRef, createSubRouteRef } from '@backstage/core-plugin-api';
 
 export const rootRouteRef = createRouteRef({
   id: 'veecode-assistant-ai',
 });
+
+export const aiOptionsRef = createSubRouteRef({
+  id: 'ai-options',
+  parent: rootRouteRef,
+  path: '/ai-options'
+})
+
+export const aiChatRef = createSubRouteRef({
+  id: 'ai-chat',
+  parent: rootRouteRef,
+  path: '/ai-chat'
+})
