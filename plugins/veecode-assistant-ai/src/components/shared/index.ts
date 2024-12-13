db2c422a498417ceb8e93a65c892cf9488d892f6
ValberJunior <valberjr1992@gmail.com>
2024-12-13 10:57:08 -0300

    feat: ✨ export ChatBubble

--- plugins/veecode-assistant-ai/src/components/shared/index.ts
+++ plugins/veecode-assistant-ai/src/components/shared/index.ts
@@ -2,3 +2,4 @@ export { InfoBox } from './InfoBox/InfoBox';
 export { LoadingProgress } from './LoadingProgress/LoadingProgress';
 export { PluginNotConfigured } from './pluginNotConfigured/PluginNotConfigured';
 export { ContentLayout } from './contentLayout/ContentLayout';
+export { ChatBubble } from './chatBubble';
