15e99c4335a47d313827ea0625e0bab53ae12a5e
ValberJunior <valberjr1992@gmail.com>
2024-12-13 10:56:20 -0300

    refactor: 📦 update AssistantAIContent

--- plugins/veecode-assistant-ai/src/components/assistantAIContent/AssistantAIContent.tsx
+++ plugins/veecode-assistant-ai/src/components/assistantAIContent/AssistantAIContent.tsx
@@ -3,6 +3,7 @@ import { Box, Typography } from "@material-ui/core";
 import { useAssistantAIStyles } from "./styles";
 import StarsIcon from "../../assets/stars.png"
 import { AIModalComponent } from "./aImodalComponent";
+import { Link } from 'react-router-dom'
 
 
 export const AssistantAIContent = () => {
@@ -20,10 +21,12 @@ export const AssistantAIContent = () => {
             toggleDialog={handleDialog}
            />
           )}
-          <Box className={assistant} onClick={handleDialog}>
-            <span><Typography variant="body1">Analyze your code with AI</Typography></span>
-            <img src={StarsIcon} alt="" className={icon}/>
-          </Box>
+          <Link to="ai-options" onClick={handleDialog}>
+            <Box className={assistant}>
+              <span><Typography variant="body1">Analyze your code with AI</Typography></span>
+              <img src={StarsIcon} alt="" className={icon}/>
+            </Box>
+          </Link>
         </>
     )
 }
