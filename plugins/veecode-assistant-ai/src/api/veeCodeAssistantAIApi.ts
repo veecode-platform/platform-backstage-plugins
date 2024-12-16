505ea87ba3f50bbde0318b233ba360f34cff976b
ValberJunior <valberjr1992@gmail.com>
2024-12-16 13:02:25 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai/src/api/veeCodeAssistantAIApi.ts
+++ plugins/veecode-assistant-ai/src/api/veeCodeAssistantAIApi.ts
@@ -9,5 +9,5 @@ export interface VeeCodeAssistantAIApi {
     submitRepo(engine: string | undefined, repoName: string, location: string): Promise<SubmitRepoResponse>,
     getChat(engine: string | undefined, vectorStoreId: string, prompt: string): Promise<InitializeAssistantAIResponse>,
     clearHistory(engine: string | undefined, vectorStoreId: string, assistantId: string, threadId: string): Promise<clearHistoryResponse>,
-    createPullRequest(files: FileContent[], location: string): Promise<SubmitRepoResponse>
+    createPullRequest(files: FileContent[], engine: string, vectorStoreId: string, location: string): Promise<SubmitRepoResponse>
 }
