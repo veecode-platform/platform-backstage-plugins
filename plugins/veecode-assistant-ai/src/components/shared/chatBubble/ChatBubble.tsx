115803e0808ce9f02104b8c0dd2ec35392b0c2f2
ValberJunior <valberjr1992@gmail.com>
2024-12-16 10:02:37 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai/src/components/shared/chatBubble/ChatBubble.tsx
+++ plugins/veecode-assistant-ai/src/components/shared/chatBubble/ChatBubble.tsx
@@ -2,22 +2,27 @@ import React from "react";
 import type { ChatBubbleProps } from "./types";
 import { Box } from "@material-ui/core";
 import { useChatBubbleStyles } from "./styles";
-import VeeCodeIcon from "../../../assets/veecodeIcon.png";
+import VeeCodeIcon from "../../../assets/logo-veecode.png";
 import PersonAvatar from "../../../assets/person.png";
+import { LoadingAnswer } from "../loadingAnswer/LoadingAnswer";
 
 export const ChatBubble : React.FC<ChatBubbleProps> = (props) => {
-    const { robot, children } = props;
-    const { root, aiBubble, avatar, avatarImg, content} = useChatBubbleStyles();
+    const { robot, children, loading } = props;
+    const { root, loadingContent, bubble, aiBubble, avatar, avatarImg, content} = useChatBubbleStyles();
 
     return (
-        <Box
-         className={`${root} ${robot ? aiBubble : ''}`}>
-            <div className={avatar}>
-             <img alt="" src={`${robot ? VeeCodeIcon : PersonAvatar}`} className={avatarImg} />
-            </div>
-            <div className={content}>
-                {children}
-            </div>
-        </Box>
+        <Box className={root}>
+        <div className={avatar}>
+          <img alt="" src={`${robot ? VeeCodeIcon : PersonAvatar}`} className={avatarImg} />
+        </div>
+        {loading ? <Box className={`${loadingContent} ${robot ? aiBubble : ''}`}><LoadingAnswer/></Box> :(
+            <Box
+            className={`${bubble} ${robot ? aiBubble : ''}`}>
+             <div className={content}>
+               {children}
+             </div>
+           </Box>
+        )}
+       </Box>
     )
 }
