ad8e07a8be5b5724fb3e787daf4fdc775db6c422
ValberJunior <valberjr1992@gmail.com>
2024-12-16 10:02:03 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai/src/components/assistantAIContent/aImodalComponent/aIContent/aiOptions/AIOptions.tsx
+++ plugins/veecode-assistant-ai/src/components/assistantAIContent/aImodalComponent/aIContent/aiOptions/AIOptions.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { useAIOptionsStyles } from "./styles";
-import { ChatBubble } from "../../../../shared";
-import { Typography } from "@material-ui/core";
+import { ChatBubble, TypingAnimation } from "../../../../shared";
 import { MenuOptions } from "./menuOptions";
 import FixedOptions from "../../../../../lib/options.json";
 
 export const AIOptions = () => {
+   
+    const [ typingComplete, setTypingComplete ] = React.useState<boolean>(false);
     const { root } = useAIOptionsStyles();
+
+    const handleTypingComplete = () => {
+      setTypingComplete(true);
+    };
 
     return(
         <div className={root}>
           <ChatBubble robot>
-             <Typography variant="body1">
-              Perfect, I managed to take a look at your code, now I need you to tell me which instruction to make:
-             </Typography>
-             <MenuOptions 
+              <TypingAnimation 
+                text="Perfect, I managed to take a look at your code, now I need you to tell me which instruction to make:" 
+                speed={15} 
+                onComplete={handleTypingComplete}
+                />
+              { !typingComplete ? null : (<MenuOptions 
                options={FixedOptions} 
-               />
+               />)}
           </ChatBubble>
         </div>
     )
