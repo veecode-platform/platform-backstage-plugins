b5d07847f3313ef8e3b7be00590029182c2a668a
ValberJunior <valberjr1992@gmail.com>
2024-12-16 10:01:20 -0300

    style: 💎

--- plugins/veecode-assistant-ai/src/components/assistantAIContent/styles.ts
+++ plugins/veecode-assistant-ai/src/components/assistantAIContent/styles.ts
@@ -1,13 +1,13 @@
 import { makeStyles } from "@material-ui/core";
+import { themeVariables } from "../../utils/constants/theme";
 
 export const useAssistantAIStyles = makeStyles(theme=>({
     assistant:{
         width: '80px',
         height: '80px',
-        backgroundColor: '#091d292d',
-        backdropFilter: 'blur(13.5px)',
-        '-webkit-backdrop-filter': 'blur(13.5px)',
-        border: '1px solid rgba(255, 255, 255, 0.18)',
+        backgroundColor: theme.palette.background.paper,
+        border: `1px solid ${themeVariables.colors.main}`,
+        boxShadow: 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
         padding: '1.5rem',
         borderRadius: '50px',
         position: 'absolute',
