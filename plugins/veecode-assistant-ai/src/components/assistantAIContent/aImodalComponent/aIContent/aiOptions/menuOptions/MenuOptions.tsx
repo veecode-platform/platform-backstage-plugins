c4c5b4fa67dae0fc9d6f5a2139e2b3128e12534c
ValberJunior <valberjr1992@gmail.com>
2024-12-16 10:02:16 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai/src/components/assistantAIContent/aImodalComponent/aIContent/aiOptions/menuOptions/MenuOptions.tsx
+++ plugins/veecode-assistant-ai/src/components/assistantAIContent/aImodalComponent/aIContent/aiOptions/menuOptions/MenuOptions.tsx
@@ -5,30 +5,31 @@ import { Box } from "@material-ui/core";
 import { useVeecodeAssistantAIContext } from "../../../../../../context/veecodeAssistantAIProvider";
 
 export const MenuOptions : React.FC<MenuOptionsProps> = (props) => {
-
+  
     const { options } = props;
     const { list,listItem } = useMenuOptionsStyles();
     const { handleChat } = useVeecodeAssistantAIContext();
 
     return (
-     <Box className={list}>
-        { options.map
-          (option => 
-           (<div 
-              key={option.id} 
-              className={listItem} 
-              onClick={handleChat}
-              role="button"
-              tabIndex={0}
-              onKeyDown={(e) => {
-                if (e.key === "Enter" || e.key === " ") handleChat();
-              }}
-              aria-label={`Navigate to ${option.label}`}
-              >
-              {option.label} 
-            </div>)
-         )
-         }
-      </Box>
+     <Box className={list}>       
+      {
+         options.map
+            (option => 
+             (<div 
+                key={option.id} 
+                className={listItem} 
+                onClick={handleChat}
+                role="button"
+                tabIndex={0}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" || e.key === " ") handleChat();
+                }}
+                aria-label={`Navigate to ${option.label}`}
+                >
+                {option.label} 
+              </div>)
+           )
+       }
+    </Box>
     )
 }
