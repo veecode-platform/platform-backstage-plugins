f170a9b75a84d0b709afcbf86ce95c7c861da465
ValberJunior <valberjr1992@gmail.com>
2024-12-16 11:52:06 -0300

    refactor: 📦 update > add api implementation

--- plugins/veecode-assistant-ai/src/plugin.ts
+++ plugins/veecode-assistant-ai/src/plugin.ts
@@ -1,15 +1,36 @@
 import {
+  createApiFactory,
+  configApiRef,
+  fetchApiRef,
+  githubAuthApiRef,
+  gitlabAuthApiRef,
   createPlugin,
   createRoutableExtension,
 } from '@backstage/core-plugin-api';
 
 import { rootRouteRef } from './routes';
+import { veecodeAssistantAIApiRef } from './api/veeCodeAssistantAIApi';
+import { VeeCodeAssistantAIClient } from './api';
 
 export const veecodeAssistantAiPlugin = createPlugin({
   id: 'veecode-assistant-ai',
   routes: {
     root: rootRouteRef,
   },
+  apis:[
+    createApiFactory({
+      api: veecodeAssistantAIApiRef,
+      deps: { config: configApiRef, fetchApi: fetchApiRef, githubAuthApi: githubAuthApiRef, gitlabAuthApi: gitlabAuthApiRef },
+      factory: ({config, fetchApi, githubAuthApi, gitlabAuthApi}) => {
+        return new VeeCodeAssistantAIClient(
+          config,
+          fetchApi,
+          githubAuthApi,
+          gitlabAuthApi
+        )
+      }
+    })
+  ]
 });
 
 export const AssistantAIContent = veecodeAssistantAiPlugin.provide(
