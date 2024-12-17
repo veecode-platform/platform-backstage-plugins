cb08a6bd55f12aa90ea8710eca7dbf0e8afb57c9
ValberJunior <valberjr1992@gmail.com>
2024-12-16 21:06:04 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai/src/components/assistantAIContent/aImodalComponent/AIModalComponent.tsx
+++ plugins/veecode-assistant-ai/src/components/assistantAIContent/aImodalComponent/AIModalComponent.tsx
@@ -3,23 +3,16 @@ import { useAIModalComponentStyles } from "./styles";
 import { AIModalComponentProps } from "./types";
 import { Box, Fade, Modal, Tooltip } from '@material-ui/core';
 import { MdClose } from "react-icons/md";
-import { LoadingProgress } from "../../shared";
 import { AIContent } from "./aIContent/AIContent";
 import { useEntity } from "@backstage/plugin-catalog-react";
 import { useEntityAnnotation } from "../../../hooks/useEntityAnnotation";
 
 export const AIModalComponent : React.FC<AIModalComponentProps> = (props) => {
     
-    const [ loadingState, setLoadingState ] = React.useState<boolean>(true);
     const { show, toggleDialog } = props;
-    const { modalOnBlur,modalContent,modalHeader,closeModal,modalBody,loadingContainer } = useAIModalComponentStyles();
+    const { modalOnBlur,modalContent,modalHeader,closeModal,modalBody } = useAIModalComponentStyles();
     const { entity } = useEntity();
     const { location, projectName, engine } = useEntityAnnotation(entity);
-
-    React.useEffect(()=>{
-        setTimeout(()=>{setLoadingState(false)},1000); 
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      },[])
 
     return (
       <Modal
@@ -42,17 +35,12 @@ export const AIModalComponent : React.FC<AIModalComponentProps> = (props) => {
                 </Tooltip>
               </div>
             <div className={modalBody}>
-              { loadingState ? 
-                 <div className={loadingContainer}>
-                   <LoadingProgress/>
-                 </div>
-                : <AIContent 
-                    engine={engine!}
-                    location={location!}
-                    projectName={projectName!}
-                    toggleDialog={toggleDialog}
-                   />
-              }
+              <AIContent 
+                engine={engine!}
+                location={location!}
+                projectName={projectName!}
+                toggleDialog={toggleDialog}
+                />
             </div>
           </Box>
         </Fade>
