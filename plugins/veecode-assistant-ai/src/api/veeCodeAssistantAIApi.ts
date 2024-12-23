970b1ed05646175c00b149308b5481f41ad72961
ValberJunior <valberjr1992@gmail.com>
2024-12-23 15:12:11 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai/src/api/veeCodeAssistantAIApi.ts
+++ plugins/veecode-assistant-ai/src/api/veeCodeAssistantAIApi.ts
@@ -1,14 +1,14 @@
 import { createApiRef } from "@backstage/core-plugin-api";
-import { clearHistoryResponse, FileContent, InitializeAssistantAIResponse, SubmitRepoResponse } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
+import { ClearHistoryResponse, FileContent, InitializeAssistantAIResponse, SubmitRepoResponse } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
 
 export const veecodeAssistantAIApiRef = createApiRef<VeeCodeAssistantAIApi>({
     id: 'plugin.veecodeassistantai'
 });
 
 export interface VeeCodeAssistantAIApi {
-    downloadRepoFiles(location: string): Promise<File[]>,
+    downloadRepoFiles(location:string): Promise<File[]>,
     submitRepo(engine: string | undefined, files: File[], repoName: string): Promise<SubmitRepoResponse>,
     getChat(engine: string | undefined, vectorStoreId: string, prompt: string): Promise<InitializeAssistantAIResponse>,
-    clearHistory(engine: string | undefined, vectorStoreId: string, assistantId: string, threadId: string): Promise<clearHistoryResponse>,
+    clearHistory(engine: string | undefined, vectorStoreId: string, assistantId: string, threadId: string): Promise<ClearHistoryResponse>,
     createPullRequest(files: FileContent[], engine: string, vectorStoreId: string, location: string): Promise<void>
 }
