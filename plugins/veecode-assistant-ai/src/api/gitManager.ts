9127f73677f8236ce0d66e2adea0e5997f9e4804
ValberJunior <valberjr1992@gmail.com>
2024-12-23 15:12:05 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai/src/api/gitManager.ts
+++ plugins/veecode-assistant-ai/src/api/gitManager.ts
@@ -3,34 +3,47 @@ import { parseGitUrl } from "../utils/helpers/parseGitUrl";
 import { ConfigApi } from "@backstage/core-plugin-api";
 import { FileContent } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
 import { GithubManager } from "./git-providers/github";
+import { GitLabManager } from "./git-providers/gitlab";
 
 export class GitManager {
   
     private readonly githubManager : GithubManager;
-    // private readonly gitlabManager : GitLabManager;
+    private readonly gitlabManager : GitLabManager;
    
     constructor(
         private readonly scmAuthApi: ScmAuthApi,
         private readonly configApi: ConfigApi,
     ){
         this.githubManager = new GithubManager(this.scmAuthApi, this.configApi);
-       // this.gitlabManager = new GitLabManager(this.scmAuthApi, this.configApi)
+        this.gitlabManager = new GitLabManager(this.scmAuthApi, this.configApi)
     }
 
-    
-    async downloadRepoFiles(location:string){
+
+    async returnRepoInfo(location:string){
         const  url = parseGitUrl(location);
         switch(true){
-            case url.includes('github'): {
-               return this.githubManager.getFilesFromRepo(url);
-            }
-            // case url.includes('gitlab'): {
-            //     return this.gitlabManager.getFilesFromRepo(url);
-            //  }
+            case url.includes('gitlab'): {
+                return this.gitlabManager.returnRepoInfo(url);
+             }
             default:
               throw new Error('Git provider error: unimplemented!');
         }
     }
+
+    
+    // async downloadRepoFiles(location:string){
+    //     const  url = parseGitUrl(location);
+    //     switch(true){
+    //         case url.includes('github'): {
+    //            return this.githubManager.getFilesFromRepo(url);
+    //         }
+    //         case url.includes('gitlab'): {
+    //             return this.gitlabManager.getFilesFromRepo(url);
+    //          }
+    //         default:
+    //           throw new Error('Git provider error: unimplemented!');
+    //     }
+    // }
 
     async createPullRequest(
         files: FileContent[],
@@ -44,9 +57,9 @@ export class GitManager {
             case url.includes('github'): {
                return this.githubManager.createPullRequest(files,url,title,message);
             }
-            // case url.includes('gitlab'): {
-            //     return this.gitlabManager.createMergeRequest(files,url,title,message);
-            //  }
+            case url.includes('gitlab'): {
+                return this.gitlabManager.createMergeRequest(files,url,title,message);
+             }
             default:
               throw new Error('Git provider error: unimplemented!');
         }        
