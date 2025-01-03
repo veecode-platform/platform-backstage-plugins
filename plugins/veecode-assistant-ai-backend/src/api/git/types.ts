1303fd575f80db1e3710d743bbf8818d4a6cdf6d
ValberJunior <valberjr1992@gmail.com>
2025-01-03 08:38:52 -0300

    refactor: 📦 delete files

--- plugins/veecode-assistant-ai-backend/src/api/git/types.ts
+++
@@ -1,4 +0,0 @@
-export interface IGit {
-    cloneRepo(token:string,localPath: string, repoUrl: string, branch: string): Promise<string>,
-    returnFilesFromLocalPath(localPath: string): Promise<File[]>
-}
