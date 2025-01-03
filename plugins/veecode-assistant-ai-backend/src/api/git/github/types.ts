25f84b817ba34414dd8bddf0f400ec18f249bb46
ValberJunior <valberjr1992@gmail.com>
2025-01-03 08:43:36 -0300

    refactor: 📦 update IGithubManager

--- plugins/veecode-assistant-ai-backend/src/api/git/github/types.ts
+++ plugins/veecode-assistant-ai-backend/src/api/git/github/types.ts
@@ -1,7 +1,11 @@
-import { FileContent } from "../../../utils/types";
+import { FileContent } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
 import { PullRequestResponse } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
 
 export interface IGithubManager {
-    getFilesFromRepo(url: string): Promise<File[]>,
+    returnRepoInfo(url: string): Promise<{
+        localPath: string;
+        repoUrl: string;
+        branch: string;
+    }>;
     createPullRequest(files: FileContent[], location: string, title: string, message: string): Promise<PullRequestResponse>  
 }
