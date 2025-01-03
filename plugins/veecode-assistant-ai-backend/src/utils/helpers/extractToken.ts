7d8cfa630e125701e0f8b3fa8b771e66bb471b24
ValberJunior <valberjr1992@gmail.com>
2025-01-03 08:46:44 -0300

    refactor: 📦 changes in function

--- plugins/veecode-assistant-ai-backend/src/utils/helpers/extractToken.ts
+++ plugins/veecode-assistant-ai-backend/src/utils/helpers/extractToken.ts
@@ -1,13 +1,6 @@
 export function extractToken(header:string):string{
-    
-    const headerObj = JSON.parse(header);
-    const authorizationHeader = headerObj.headers?.Authorization;
   
-    if (!authorizationHeader) {
-      throw new TypeError('Missing Authorization header');
-    }
-  
-    const token = authorizationHeader.match(/^Bearer\s(\S+)$/i)?.[1]; 
+  const token = header.match(/^Bearer\s(\S+)$/i)?.[1];
   
     if (!token) {
       throw new TypeError('Expected Bearer token');
