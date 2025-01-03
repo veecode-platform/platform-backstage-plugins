505aefe8719bfe7ea16a063696025f8bf5f5a00f
ValberJunior <valberjr1992@gmail.com>
2025-01-03 08:46:07 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai-backend/src/controllers/GitController.ts
+++ plugins/veecode-assistant-ai-backend/src/controllers/GitController.ts
@@ -1,32 +1,29 @@
-import { LoggerService } from "@backstage/backend-plugin-api";
-import { Git } from "../api/git/git";
 import { Request, Response } from 'express';
-import { CloneRepositoryParams } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
+import { CloneRepositoryParams, SaveChangesInRepository } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
 import { InputError, stringifyError } from "@backstage/errors";
 import { IGitController } from "./types";
+import { AssistantAIController } from "./AssistantAIController";
 
-export class GitController implements IGitController {
+export class GitController extends AssistantAIController implements IGitController {
 
-    public git : Git;
-
-    constructor(
-        private readonly logger: LoggerService
-    ){
-        this.git = new Git(this.logger)
-    };
+    returnToken = (req:Request) => {
+      this.logger.info(`Dentro do RETURN TOKEN NO GITCONTROLLER >>>>>>> ${req.headers.authorization}`)
+      return this.getToken(req);
+    }
 
     clone = async (req: Request,res: Response) => {
 
-      const { localPath, repoUrl, branch } = req.body  as CloneRepositoryParams;
+    const { location } = req.body  as CloneRepositoryParams;
+
+    if(!location){
+      res.status(400).json({error: "Bad Request: Expect: { location } in the request body"})
+    }
 
     try {
-  
-        if(!localPath || !repoUrl || !branch){
-          res.status(400).json({error: "Bad Request"})
-        }
-  
-        const response = await this.git.cloneRepo(localPath,repoUrl,branch);
-  
+        const token = this.returnToken(req);
+        const gitManager = this.gitProviderManager(token);
+        const { localPath, repoUrl, branch } = await gitManager.returnRepoInfo(location);
+        const response = await gitManager.cloneRepo(token,localPath,repoUrl,branch);
         res.status(200).json({
           message: 'Repository successfully cloned',
           data: response
@@ -43,31 +40,41 @@ export class GitController implements IGitController {
       }
       
     }
+    
+    submitChangesToRepository = async(req:Request, res:Response) => {
+      const {
+        files,
+        location,
+        title,
+        message
+      } = req.body as SaveChangesInRepository;
 
-    getFiles = async (req: Request, res:Response) => {
-        const { localPath } = req.params;
+      if(!files || !location || !title || !message){
+        res.status(400).json({error: "Bad Request. Expect: {files, location, title, message} in the request body"})
+      }
 
-        try {
-      
-            if(!localPath){
-              res.status(400).json({error: "Bad Request"})
-            }
-      
-            const response = await this.git.returnFilesFromLocalPath(localPath);
-      
-              res.status(200).json({
-              data: response
-            })
-          } catch (err: any) {
-            if (err.errors) {
-              throw new InputError(
-                `Error during get files from localPath: [${localPath}] : ${stringifyError(
-                  err.errors,
-                )}`,
-              );
-            }
-            throw err;
-          }
+      try {    
+        const token = this.returnToken(req);
+        const gitManager = this.gitProviderManager(token);
+        const response = await gitManager.createPullRequest(files,location,title,message);
+  
+          res.status(200).json({
+          status: response.status,
+          message: response.message,
+          link: response.link
+        })
+      } catch (err: any) {
+        if (err.errors) {
+          throw new InputError(
+            `Error to save changes in repository : ${stringifyError(
+              err.errors,
+            )}`,
+          );
+        }
+        throw err;
+      }
+
+
     }
 
 }
