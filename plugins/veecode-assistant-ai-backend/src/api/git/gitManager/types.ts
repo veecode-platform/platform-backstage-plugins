88b380a629a55b8e61f674b5cf723c0ec6c94a9a
ValberJunior <valberjr1992@gmail.com>
2025-01-05 12:52:23 -0300

    refactor: 📦 update types

--- plugins/veecode-assistant-ai-backend/src/api/git/gitManager/types.ts
+++ plugins/veecode-assistant-ai-backend/src/api/git/gitManager/types.ts
@@ -1,4 +1,4 @@
-import { GitCloneResponse, PullRequestResponse } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
+import { PullRequestResponse } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
 import { FileContent } from "openai/resources";
 
 export interface IGitManager {
@@ -7,7 +7,7 @@ export interface IGitManager {
         repoUrl: string;
         branch: string;
     }>,
-    cloneRepo(token: string, localPath: string, repoUrl: string, branch: string): Promise<GitCloneResponse>;
+    cloneRepo(token: string, localPath: string, repoUrl: string, branch: string): Promise<FileContent[]>;
     // returnFilesFromLocalPath(localPath: string): Promise<File[]>;
     createPullRequest(files: FileContent[], location: string, title: string, message: string): Promise<PullRequestResponse> 
 }
