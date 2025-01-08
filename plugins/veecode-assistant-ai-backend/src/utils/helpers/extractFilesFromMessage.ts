4d2711bff6c0c13bf6ebd619e168e4c713f3dac1
ValberJunior <valberjr1992@gmail.com>
2025-01-08 09:41:48 -0300

    refactor: 📦 update extractFIlesMessage function

--- plugins/veecode-assistant-ai-backend/src/utils/helpers/extractFilesFromMessage.ts
+++ plugins/veecode-assistant-ai-backend/src/utils/helpers/extractFilesFromMessage.ts
@@ -1,26 +1,11 @@
-import mime from 'mime-types';
 import { FileContent } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
 
-export function extractFilesFromMessage(content: string): FileContent[] {
-    const fileRegex = /---START FILE---\nName: (.+?)\nContent:\n([\s\S]+?)\n---END FILE---/g;
-    const files: FileContent[] = [];
-    let match = fileRegex.exec(content);
-
-    while (match !== null) {
-        const fileName = match[1];
-        const fileContent = match[2];
-        
-        // Determina o tipo MIME com base na extensão do arquivo
-        const mimeType = mime.lookup(fileName) || "application/octet-stream"; // Define um tipo padrão caso o MIME não seja encontrado
-
-        files.push({
-            name: fileName,
-            content: fileContent,
-            type: mimeType,
-        });
-
-        match = fileRegex.exec(content);
-    }
-
-    return files;
+export function extractFilesFromMessage(content: any): FileContent[] {
+    return content
+        .filter((block: any) => block.type === "file")
+        .map((block: any) => ({
+            name: block.name,
+            content: block.content,
+            type: block.mimeType,
+        }));
 }
