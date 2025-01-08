60fd0315108e1f101efb72a8e1823af68b23105c
ValberJunior <valberjr1992@gmail.com>
2025-01-08 12:46:13 -0300

    refactor: 📦 update MarkdownRenderer component

--- plugins/veecode-assistant-ai/src/components/shared/markdownRenderer/MarkdownRenderer.tsx
+++ plugins/veecode-assistant-ai/src/components/shared/markdownRenderer/MarkdownRenderer.tsx
@@ -1,19 +1,39 @@
 import React from "react";
 import { MarkdownRendererProps } from "./type";
 import { useMarkdowRendererStyles } from "./styles";
-import Markdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { CodeBlockComponent } from "../codeBlock/CodeBlock";
 
 export const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ markdown }) => {
-
-   const { wrapper } = useMarkdowRendererStyles();
+  const { wrapper } = useMarkdowRendererStyles();
 
   return (
     <div className={wrapper}>
-     <Markdown remarkPlugins={[remarkGfm]}>
-      {markdown}
-    </Markdown>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            const language = match ? match[1] : "";
+
+            if (!className) {
+              return (
+                <code {...props} style={{ backgroundColor: "#313131", padding: "2px 4px", borderRadius: "4px", color: "#FAFAFA" }}>
+                  {children}
+                </code>
+              );
+            }
+            return (
+              <CodeBlockComponent language={language} className={className!}>
+                {String(children).trim()}
+              </CodeBlockComponent>
+            );
+          },
+        }}
+      >
+        {markdown}
+      </Markdown>
     </div>
   );
 };
