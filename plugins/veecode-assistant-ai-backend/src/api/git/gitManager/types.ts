3bc7bd9c160ea1d981d0fc607b31c6635a9b4d8b
ValberJunior <valberjr1992@gmail.com>
2025-01-09 17:24:39 -0300

    refactor: 📦 remove method

--- plugins/veecode-assistant-ai-backend/src/api/git/gitManager/types.ts
+++ plugins/veecode-assistant-ai-backend/src/api/git/gitManager/types.ts
@@ -1,5 +1,4 @@
-import { IRepository, PullRequestResponse } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
-import { FileContent } from "openai/resources";
+import { IRepository } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
 
 export interface IGitManager {
     returnRepoInfo(location: string): Promise<{
@@ -8,6 +7,4 @@ export interface IGitManager {
         branch: string;
     }>,
     cloneRepo(token: string, localPath: string, repoUrl: string, branch: string): Promise<IRepository>;
-    // returnFilesFromLocalPath(localPath: string): Promise<File[]>;
-    createPullRequest(files: FileContent[], location: string, title: string, message: string): Promise<PullRequestResponse> 
 }
