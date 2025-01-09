8337448b36b87a1202939d9489daa8d9e4e589fd
ValberJunior <valberjr1992@gmail.com>
2025-01-09 00:22:31 -0300

    refactor: 📦

--- plugins/veecode-assistant-ai/src/context/veecodeAssistantAIProvider.tsx
+++ plugins/veecode-assistant-ai/src/context/veecodeAssistantAIProvider.tsx
@@ -55,11 +55,9 @@ export const VeecodeAssistantAIProvider: React.FC<VeecodeAssistantAIProviderProp
               const response = await api.getChat(engine,vectorStoreId,prompt,projectName, projectStructure);
               setAssistantId(response.assistantId);
               setThreadId(response.threadId);
-              // eslint-disable-next-line no-console
-              console.log("AI RAPAZEADA O RESPONSE DO CHAT >>>>",response)
               return {
                 analysis: response.message,
-                files: response.data.generatedFiles
+                files: response.generatedFiles
               }
             }
             return null
