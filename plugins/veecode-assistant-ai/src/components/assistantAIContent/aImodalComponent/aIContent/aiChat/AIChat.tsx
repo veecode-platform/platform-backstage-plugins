3cc76a9bcfcf226a62055cc980c561a48784508d
ValberJunior <valberjr1992@gmail.com>
2025-01-09 00:20:46 -0300

    refactor: 📦 update component

--- plugins/veecode-assistant-ai/src/components/assistantAIContent/aImodalComponent/aIContent/aiChat/AIChat.tsx
+++ plugins/veecode-assistant-ai/src/components/assistantAIContent/aImodalComponent/aIContent/aiChat/AIChat.tsx
@@ -1,21 +1,44 @@
 import React from "react";
-import { Button, ChatBubble, MarkdownRenderer } from "../../../../shared";
-import { Typography } from "@material-ui/core";
+import { Button, ChatBubble, FeedbackComponent, MarkdownRenderer } from "../../../../shared";
+import { CircularProgress, Typography } from "@material-ui/core";
 import { useAIChatStyles } from "./styles";
 import type { AIChatProps } from "./types";
 import { useVeecodeAssistantAIContext } from "../../../../../context/veecodeAssistantAIProvider";
 import useAsync from "react-use/esm/useAsync";
+import { FileContent } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
+import { addPullRequestResponse, initialPullRequestResponseState, PullRequestResponseReducer } from "./state";
 
 export const AIChat : React.FC<AIChatProps> = (props) => {
-
-    const { handleChat, chat, promptValue } = useVeecodeAssistantAIContext();
+    
+    const [LoadingPullRequest, setLoadingPullRequest ] = React.useState<boolean>(false);
+    const [ showFeedback, setShowFeedback ] = React.useState<boolean>(false); 
+    const [ pullRequestResponseState, pullRequestResponseDispatch ] = React.useReducer(PullRequestResponseReducer,initialPullRequestResponseState);
+    const { handleChat, chat, promptValue, analyzeChangesAndSubmitToRepository } = useVeecodeAssistantAIContext();
     const { closeModal } = props;
-    const { root, chatContent, footer, buttonGroup } = useAIChatStyles();
+    const { root, chatContent, footer, buttonGroup, spinner } = useAIChatStyles();
 
     const clearHistoryAndExit = () => {
       closeModal();
       handleChat();
     };
+
+    const preparePullRequest = async (files: FileContent[]) => {
+      if(files.length > 0) {
+        setLoadingPullRequest(true)
+        const response = await analyzeChangesAndSubmitToRepository(files);
+        if (response) {
+          pullRequestResponseDispatch(addPullRequestResponse({
+            status: response.status === "ok" ? 'success':'error',
+            message: response.message,
+            link: response.link
+          }));
+          setShowFeedback(true);
+          setTimeout(()=>{
+            setShowFeedback(false);
+          },3000);
+        }
+      }
+    }
 
     const { value: chatResponse, loading, /* error */ } = useAsync(async()=>{  
         const response = await chat(promptValue!);
@@ -24,13 +47,10 @@ export const AIChat : React.FC<AIChatProps> = (props) => {
           files: response?.files ?? []
         } 
     },[promptValue]);
-
-    React.useEffect(()=>{
-      // eslint-disable-next-line no-console
-      console.log(chatResponse)
-    },[chatResponse])
+    
 
     return(
+       <>
         <div className={root}>
           <div className={chatContent}>
             <ChatBubble robot loading={loading}>
@@ -40,7 +60,15 @@ export const AIChat : React.FC<AIChatProps> = (props) => {
                     <Typography variant="body1">
                       Do you want to commit the changes to your repository?
                     </Typography>
-                    <Button variant="primary">Create Pull Request</Button>
+                    <Button  
+                      disabled={LoadingPullRequest} 
+                      variant="primary"
+                      onClick={() => preparePullRequest(chatResponse.files)}
+                      >
+                      {LoadingPullRequest ? 
+                        (<><CircularProgress className={spinner} size={20} /> Creating ...</>) 
+                        : (<>Create Pull Request</>)}  
+                    </Button>
                   </div>
                 )}
             </ChatBubble>
@@ -51,5 +79,12 @@ export const AIChat : React.FC<AIChatProps> = (props) => {
           </div>
          )}
         </div>
+
+        {showFeedback && 
+            (<FeedbackComponent 
+              variant={pullRequestResponseState!.status} 
+              message={pullRequestResponseState!.message} 
+              />)}
+       </>
     )
 }
