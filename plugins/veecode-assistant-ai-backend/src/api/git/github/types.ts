77e025a75b63ae88056541d26c3d5f82852821d4
ValberJunior <valberjr1992@gmail.com>
2025-01-09 17:23:11 -0300

    feat: ✨ create interface

--- plugins/veecode-assistant-ai-backend/src/api/git/github/types.ts
+++ plugins/veecode-assistant-ai-backend/src/api/git/github/types.ts
@@ -1,11 +1,11 @@
-import { FileContent } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
-import { PullRequestResponse } from "@veecode-platform/backstage-plugin-veecode-assistant-ai-common";
 
+/**
+ *  @public
+ */
 export interface IGithubManager {
     returnRepoInfo(url: string): Promise<{
         localPath: string;
         repoUrl: string;
         branch: string;
-    }>;
-    createPullRequest(files: FileContent[], location: string, title: string, message: string): Promise<PullRequestResponse>  
+    }>
 }
