fe6e045011b6522627ab80985bd891a6cb3b222a
ValberJunior <valberjr1992@gmail.com>
2025-01-11 16:07:01 -0300

    feat: ✨ add analysis props

--- plugins/veecode-assistant-ai/src/components/shared/loadingAnswer/styles.ts
+++ plugins/veecode-assistant-ai/src/components/shared/loadingAnswer/styles.ts
@@ -1,11 +1,33 @@
 import { makeStyles } from "@material-ui/core";
+import { themeVariables } from "../../../utils/constants/theme";
 
 export const useLoadingAnswerStyles = makeStyles({
-    loadingContent:{
+    loadingContent: {
         width: '100%',
         height: '100%',
         display: 'flex',
         alignItems: 'flex-start',
         justifyContent: 'center',
-    }
-})
+    },
+    loadingAnalysis: {
+        minWidth: '25vw',
+        marginLeft: '.8rem',
+        marginTop: '1.5rem',
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        position: 'relative',
+        gap: '2rem',
+        border: `1px solid ${themeVariables.border.main}`,
+        borderRadius: '0px 20px 20px 20px',
+        padding: '2.5rem 3rem',
+        backgroundColor: themeVariables.background.main,
+    },
+    analysisText: {
+        animation: `$fadeInOut 2s infinite`,
+    },
+    "@keyframes fadeInOut": {
+        "0%": { opacity: 0 },
+        "100%": { opacity: 1 },
+    },
+});
