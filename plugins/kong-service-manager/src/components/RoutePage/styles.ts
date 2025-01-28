fc84009e1f8ac879720cc80d4233de305beb5cd4
ValberJunior <valberjr1992@gmail.com>
2025-01-28 20:50:06 -0300

    style: 💎 add maxWidth

--- plugins/kong-service-manager/src/components/RoutePage/styles.ts
+++ plugins/kong-service-manager/src/components/RoutePage/styles.ts
@@ -13,6 +13,7 @@ export const useServicePageStyles = makeStyles(theme=>({
       color: theme.palette.background.default,
       padding: '.8rem 1rem',
       width: 'auto',
+      maxWidth: '180px',
       transition: 'all .5s ease-in-out both',
       '&:hover':{
         backgroundColor: theme.palette.background.default,
