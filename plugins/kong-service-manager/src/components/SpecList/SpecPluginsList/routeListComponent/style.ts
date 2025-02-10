36376512fcfc4af36ed7f6815f839eba0660f718
ValberJunior <valberjr1992@gmail.com>
2025-02-10 12:34:29 -0300

    feat: ✨ create useRouteListComponentStyles

---
+++ plugins/kong-service-manager/src/components/SpecList/SpecPluginsList/routeListComponent/style.ts
@@ -0,0 +1,8 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useRouteListComponentStyles = makeStyles({
+    root:{
+        minHeight: '45vh',
+        padding: '.3rem'
+    }
+})
