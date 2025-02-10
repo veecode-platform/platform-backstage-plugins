285d4550f3de4a381ea0c7126affe04aed2f8490
ValberJunior <valberjr1992@gmail.com>
2025-02-10 08:57:44 -0300

    feat: ✨ add permission : update route path in spec ... (comment)

--- plugins/kong-service-manager/src/components/SpecList/SpecPluginsList/SpecPluginsList.tsx
+++ plugins/kong-service-manager/src/components/SpecList/SpecPluginsList/SpecPluginsList.tsx
@@ -5,12 +5,17 @@ import { useSpecPluginsListStyles } from './styles';
 import { CardTab, TabbedCard } from '@backstage/core-components';
 import RouteListComponent from './routeListComponent/RouteListComponent';
 import ServiceComponent from './serviceComponent/ServiceComponent';
+// import { usePermission } from '@backstage/plugin-permission-react';
+// import { kongUpdateRoutePathInSpecPermission } from '@veecode-platform/backstage-plugin-kong-service-manager-common';
 
 
 export const SpecPluginsList  = () => {
 
   const { specName } = useParams();
   const { cardTabstyle } = useSpecPluginsListStyles();  
+  // const { loading: loadingUpdateRoutePathInSpecPermission, allowed: canAddRoute } = usePermission({
+  //   permission: kongUpdateRoutePathInSpecPermission,
+  // });
 
   return (
     <BoxComponent title={specName as string} noSelectInstance goBack>
