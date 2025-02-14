faa0c31db277def72fb0e2b85057d95f163ef92b
ValberJunior <valberjr1992@gmail.com>
2025-02-14 12:04:43 -0300

    feat: ✨ update logo

--- plugins/vee/src/components/shared/contentLayout/styles.ts
+++ plugins/vee/src/components/shared/contentLayout/styles.ts
@@ -24,7 +24,7 @@ export const useContentLayoutStyles = makeStyles({
       padding: "0 1rem",
     },
     logo: {
-      width: "220px",
+      width: "90px",
     },
     body: {
       padding: "2.5rem 2rem",
