4755857522d235fce1b5396459c3406640d739e1
ValberJunior <valberjr1992@gmail.com>
2025-02-27 17:08:29 -0300

    feat: ✨ add new permissions

--- plugins/vee-common/src/permissions.ts
+++ plugins/vee-common/src/permissions.ts
@@ -10,11 +10,45 @@ export const veeReadPermission = createPermission({
     attributes: { action: 'read'}
 });
 
+export const veeReadStacks = createPermission({
+    name: 'vee.ai.read.stacks',
+    attributes: { action: 'read' }
+})
 
-export const veeScaffolderReadPermission = createPermission({
-    name: 'vee.ai.scaffolder.read',
+export const veeCreateStack = createPermission({
+    name: 'vee.ai.create.stack',
+    attributes: { action: 'create' }
+})
+
+export const veeEditStack = createPermission({
+    name: 'vee.ai.edit.stack',
+    attributes: { action: 'update' }
+})
+
+export const veeRemoveStack = createPermission({
+    name: 'vee.ai.remove.stack',
+    attributes: { action: 'delete' }
+})
+
+export const veeReadPlugins = createPermission({
+    name: 'vee.ai.read.plugins',
     attributes: { action: 'read'}
-});
+})
+
+export const veeAddPlugin = createPermission({
+    name: 'vee.ai.add.plugin',
+    attributes: { action: 'create'}
+})
+
+export const veeEditPlugin = createPermission({
+    name: 'vee.ai.edit.plugin',
+    attributes: { action: 'update' }
+})
+
+export const veeRemovePlugin = createPermission({
+    name: 'vee.ai.remove.plugin',
+    attributes: { action: 'delete'}
+})
 
 export const veeAnalyzerReadPermission = createPermission({
     name: 'vee.ai.analyzer.read',
@@ -26,15 +60,22 @@ export const veeAnalyzerSaveChangesInRepo = createPermission({
     attributes: { action: 'update' }
 });
 
-export const veeScaffolderSaveTemplate = createPermission({
-    name: 'vee.ai.scaffolder.save.template',
+export const veeSaveTemplate = createPermission({
+    name: 'vee.ai.save.template',
     attributes: { action: 'create' }
 })
 
 export const veePermissions = {
     veeReadPermission,
-    veeScaffolderReadPermission,
+    veeReadStacks,
+    veeCreateStack,
+    veeEditStack,
+    veeRemoveStack,
+    veeReadPlugins,
+    veeAddPlugin,
+    veeEditPlugin,
+    veeRemovePlugin,
     veeAnalyzerReadPermission,
     veeAnalyzerSaveChangesInRepo,
-    veeScaffolderSaveTemplate
+    veeSaveTemplate
 }
