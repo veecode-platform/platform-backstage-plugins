91a4246494533536cf54a073dc54db316fba0100
ValberJunior <valberjr1992@gmail.com>
2025-02-28 17:18:41 -0300

    feat: ✨ create constants

---
+++ plugins/vee-backend/src/utils/constants/tables.ts
@@ -0,0 +1,6 @@
+export const FIXED_OPTIONS_TABLE = 'fixed_options';
+export const OPTIONS_TABLE = 'options';
+export const STACKS_TABLE = 'stacks';
+export const PLUGINS_TABLE = 'plugins';
+export const ANNOTATIONS_TABLE = 'annotations';
+export const STACK_PLUGINS_TABLE = 'stack_plugins'
