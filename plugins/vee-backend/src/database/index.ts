d1f1463469bc5edc8d2d7cc68aa6ff34584e14ee
ValberJunior <valberjr1992@gmail.com>
2025-02-28 17:17:43 -0300

    feat: ✨ export stores

---
+++ plugins/vee-backend/src/database/index.ts
@@ -0,0 +1,4 @@
+export { DatabaseVeeStore } from "./DatabaseVeeStore";
+export { FixedOptionsStore } from "./FixedOptionsStore";
+export { StackStore } from "./StackStore";
+export { PluginStore } from "./PluginStore";
