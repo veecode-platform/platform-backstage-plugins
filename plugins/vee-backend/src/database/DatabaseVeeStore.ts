3ea52f7346efe8b73bbf42ce9ca504c007febd36
ValberJunior <valberjr1992@gmail.com>
2025-02-28 17:28:32 -0300

    refactor: 📦

--- plugins/vee-backend/src/database/DatabaseVeeStore.ts
+++ plugins/vee-backend/src/database/DatabaseVeeStore.ts
@@ -13,24 +13,21 @@ protected constructor(
   protected readonly logger: LoggerService
 ){}
 
-static async create(options:{
+ static async create(options:{
     database: DatabaseService,
-    skipMigrations?:boolean,
+    skipMigrations?: boolean,
     logger: LoggerService
-}):Promise<DatabaseVeeStore>{
-    const { database, skipMigrations, logger } = options;
+  }): Promise<DatabaseVeeStore> {
+    const {database, skipMigrations,  logger} = options;
     const client = await database.getClient();
-
-    if(!database.migrations?.skip && !skipMigrations){
-        await client.migrate.latest({
-            directory: migrationsDir
-        })
-    }
-
+    
+    if (!database.migrations?.skip && !skipMigrations ) {
+      await client.migrate.latest({
+        directory: migrationsDir
+      })
+     }
     return new DatabaseVeeStore(client,logger)
-}
-
-// services
+  }
 
 
 
