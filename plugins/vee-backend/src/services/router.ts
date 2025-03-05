42f976aacb4c3aa487e956663861d662fd00081d
ValberJunior <valberjr1992@gmail.com>
2025-03-05 15:06:27 -0300

    feat: ✨ create new routes

--- plugins/vee-backend/src/services/router.ts
+++ plugins/vee-backend/src/services/router.ts
@@ -2,7 +2,7 @@ import { MiddlewareFactory } from '@backstage/backend-defaults/rootHttpRouter';
 import express, { RequestHandler } from 'express';
 import Router from 'express-promise-router';
 import { AssistantAIOptions } from '../utils/types';
-import { AnalyzerAIController, ScaffolderAIController } from '../controllers';
+import { AnalyzerAIController, FixedOptionsController, PluginsController, ScaffolderAIController, StackController } from '../controllers';
 import { createPermissionIntegrationRouter } from '@backstage/plugin-permission-node';
 import { GitController } from '../controllers/GitController';
 import compression from 'compression';
@@ -11,11 +11,14 @@ import { veePermissions } from '@veecode-platform/backstage-plugin-vee-common';
 export async function createRouter(
   options: AssistantAIOptions,
 ): Promise<express.Router> {
-  const { logger, config, httpAuth, permissions } = options;
+  const { logger, config, httpAuth, permissions, database } = options;
 
-  const analyzerAIController = new AnalyzerAIController(httpAuth, permissions,config,logger);
-  const scaffolderAIController = new ScaffolderAIController(httpAuth, permissions,config,logger);
-  const gitController = new GitController(httpAuth, permissions,config,logger);
+  const analyzerAIController = new AnalyzerAIController(httpAuth, permissions,config,logger,database);
+  const scaffolderAIController = new ScaffolderAIController(httpAuth, permissions,config,logger,database);
+  const gitController = new GitController(httpAuth, permissions,config,logger, database);
+  const fixedOptionsController = new FixedOptionsController(httpAuth, permissions,config,logger, database);
+  const stackController = new StackController(httpAuth, permissions,config,logger, database);
+  const pluginsController = new PluginsController(httpAuth, permissions,config,logger, database);
 
   const router = Router();
 
@@ -37,11 +40,32 @@ export async function createRouter(
     })
   );
 
+  /**
+   * Analyzer
+   */ 
   router.post("/submit-repo", analyzerAIController.createVectorStore as RequestHandler);
   router.post("/chat-analyze-repo", analyzerAIController.analyzeAndStartChat as RequestHandler);
   router.delete("/chat-analyze-repo", analyzerAIController.deleteChat as RequestHandler);
   router.post("/clone-repository", gitController.clone as RequestHandler);
-  router.post("/upload-template", scaffolderAIController.uploadTemplateFiles as RequestHandler);
+  /**
+   *  Create template with AI
+   */
+  router.get("/fixedOptions", fixedOptionsController.ListAllFixedOptions as RequestHandler);
+  router.get("/fixedOptions/:fixedOptionsId", fixedOptionsController.getFixedOptionsById as RequestHandler);
+  router.post("/fixedOptions", fixedOptionsController.createFixedOptions as RequestHandler);
+  router.patch("/fixedOptions/:fixedOptionsId", fixedOptionsController.editFixedOptions as RequestHandler);
+  router.delete("/fixedOptions/:fixedOptionsId", fixedOptionsController.deleteFixedOptions as RequestHandler);
+  router.get("/stacks", stackController.ListAllStacks as RequestHandler);
+  router.get("/stacks/:stackId", stackController.getStackById as RequestHandler);
+  router.post("/stacks", stackController.createStack as RequestHandler);
+  router.patch("/stacks/:stackId", stackController.editStack as RequestHandler);
+  router.delete("/stacks/:stackId", stackController.deleteStack as RequestHandler);
+  router.get("/plugins", pluginsController.ListAllPlugins as RequestHandler);
+  router.get("/plugins/:pluginId", pluginsController.getPluginById as RequestHandler);
+  router.post("/plugins", pluginsController.addPlugin as RequestHandler);
+  router.patch("/plugins/:pluginId", pluginsController.editPlugin as RequestHandler);
+  router.delete("/plugins/:pluginId", pluginsController.deleteStack as RequestHandler);
+  
   router.post("/chat-template", scaffolderAIController.startChat as RequestHandler);
   router.delete("/chat-template", scaffolderAIController.deleteChat as RequestHandler);
 
