eb8d890395f195623bd655d087aadd36b75d4aa3
ValberJunior <valberjr1992@gmail.com>
2025-03-05 15:07:18 -0300

    feat: ✨ add new exports

--- plugins/vee-backend/src/controllers/index.ts
+++ plugins/vee-backend/src/controllers/index.ts
@@ -1,2 +1,5 @@
 export { AnalyzerAIController } from "./AnalyzerAIController";
 export { ScaffolderAIController } from "./scaffolderAIController";
+export { FixedOptionsController } from "./FixedOptionsController";
+export { StackController } from "./StackController";
+export { PluginsController } from "./PluginsController";
