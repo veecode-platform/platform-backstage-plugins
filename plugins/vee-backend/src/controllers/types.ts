22c6044614c39887ad5d43eeb83b3e7fae4fa8f4
ValberJunior <valberjr1992@gmail.com>
2025-03-05 15:08:11 -0300

    feat: ✨ create new interfaces

--- plugins/vee-backend/src/controllers/types.ts
+++ plugins/vee-backend/src/controllers/types.ts
@@ -28,3 +28,39 @@ export interface IScaffolderAIControler{
 export interface IGitController {
     clone(req: Request, res: Response): Promise<void>,
 }
+
+/**
+ *  @public
+ *  FixedOptionsController
+ */
+export interface IFixedOptionsController {
+    ListAllFixedOptions: (req: Request, resp: Response) => Promise<void>;
+    getFixedOptionsById: (req: Request, resp: Response) => Promise<void>;
+    createFixedOptions: (req: Request, resp: Response) => Promise<void>;
+    editFixedOptions: (req: Request, resp: Response) => Promise<void>;
+    deleteFixedOptions: (req: Request, resp: Response) => Promise<void>
+}
+
+/**
+ *  @public
+ *  StackController
+ */
+export interface IStackController {
+    ListAllStacks: (req: Request, resp: Response) => Promise<void>;
+    getStackById: (req: Request, resp: Response) => Promise<void>;
+    createStack: (req: Request, resp: Response) => Promise<void>;
+    editStack: (req: Request, resp: Response) => Promise<void>;
+    deleteStack: (req: Request, resp: Response) => Promise<void>
+}
+
+/**
+ *  @public
+ *  PluginsController
+ */
+export interface IPluginsController {
+    ListAllPlugins: (req: Request, resp: Response) => Promise<void>;
+    getPluginById: (req: Request, resp: Response) => Promise<void>;
+    addPlugin: (req: Request, resp: Response) => Promise<void>;
+    editPlugin: (req: Request, resp: Response) => Promise<void>;
+    deleteStack: (req: Request, resp: Response) => Promise<void>
+}
