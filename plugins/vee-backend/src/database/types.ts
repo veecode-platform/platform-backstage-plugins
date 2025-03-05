74cebc9f2c6532520bbf91523cdb333e205a906d
ValberJunior <valberjr1992@gmail.com>
2025-03-05 15:08:38 -0300

    refactor: 📦 changes in VeeStore

--- plugins/vee-backend/src/database/types.ts
+++ plugins/vee-backend/src/database/types.ts
@@ -1,25 +1,34 @@
-import { IFixedOption, IPlugin, IStack } from "@veecode-platform/backstage-plugin-vee-common";
+import { IFixedOptions, IPlugin, IStack } from "@veecode-platform/backstage-plugin-vee-common";
 
-export interface IFixedOptionsStore {
-    listFixedOptions(): Promise<IFixedOption[] | null>;
-    getFixedOptionById(fixedOptionId: string): Promise<IFixedOption | null>;
-    createFixedOption(fixedOption: IFixedOption): Promise<IFixedOption|null>;
-    updateFixedOption(fixedOption: IFixedOption): Promise<IFixedOption | null>;
-    deleteFixedOption(fixedOptionId: string): Promise<boolean>
-}
-
-export interface IStackStore {
-    listStacks(): Promise<IStack[] | null>;
+export interface VeeStore {
+    listFixedOptions(): Promise<IFixedOptions[]>;
+    getFixedOptionsById(fixedOptionsId: string): Promise<IFixedOptions | null>;
+    createFixedOptions(fixedOptions: IFixedOptions): Promise<IFixedOptions | null>;
+    updateFixedOption({fixedOptionsId, fixedOptions} : UpdateFixedOptionsParams): Promise<IFixedOptions | null>;
+    deleteFixedOption(fixedOptionsId: string): Promise<boolean>;
+    listStacks(): Promise<IStack[]>;
     getStackById(stackId: string): Promise<IStack | null>;
     createStack(stack: IStack): Promise<IStack|null>;
-    updateStack(stack: IStack): Promise<IStack | null>;
+    updateStack({stackId, stack}: UpdateStackParams): Promise<IStack | null>;
     deleteStack(stackId: string): Promise<boolean>
-}
-
-export interface IPluginStore {
-    listPlugins():Promise<IPlugin[]|null>,
+    listPlugins():Promise<IPlugin[]>,
     getPluginById(pluginId:string):Promise<IPlugin[]|null>,
     createPlugin(plugin:IPlugin):Promise<IPlugin|null>,
-    updatePlugin(plugin:IPlugin):Promise<IPlugin|null>,
+    updatePlugin({ pluginId, plugin}: UpdatePluginParams):Promise<IPlugin|null>,
     deletePlugin(pluginId: string):Promise<boolean>
+}
+
+export type UpdateFixedOptionsParams = {
+    fixedOptionsId: string,
+    fixedOptions: Partial <IFixedOptions>
+}
+
+export type UpdateStackParams = {
+    stackId: string,
+    stack: Partial<IStack>
+}
+
+export type UpdatePluginParams = {
+    pluginId: string,
+    plugin: Partial<IPlugin>
 }
