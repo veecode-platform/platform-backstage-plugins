b3372fa9d985cd84418866f2b796f82ef5dfefd6
ValberJunior <valberjr1992@gmail.com>
2025-03-06 10:00:52 -0300

    refactor: 📦 delete exports

--- plugins/vee-backend/src/database/index.ts
+++ plugins/vee-backend/src/database/index.ts
@@ -1,4 +1,1 @@
 export { DatabaseVeeStore } from "./DatabaseVeeStore";
-export { FixedOptionsStore } from "./FixedOptionsStore";
-export { StackStore } from "./StackStore";
-export { PluginStore } from "./PluginStore";
