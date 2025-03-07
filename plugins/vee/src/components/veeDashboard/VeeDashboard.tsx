2495d4c3aab611b8076dd0f45dd1a55370a876d1
ValberJunior <valberjr1992@gmail.com>
2025-03-07 01:21:04 -0300

    refactor: 📦

--- plugins/vee/src/components/veeDashboard/VeeDashboard.tsx
+++ plugins/vee/src/components/veeDashboard/VeeDashboard.tsx
@@ -1,31 +1,21 @@
 import React from "react";
-import { Content } from "@backstage/core-components";
-import { ContentLayout } from "../shared";
-import { useVeeDashboardStyles } from "./styles";
+import { PageLayout } from "../shared";
 import { options } from "./options";
-import { MenuOptionCard } from "./menuOptionCard";
+import { MenuOptionCard } from "../shared";
 
-export const VeeDashboard = () => {
-
-    const { wrapper, content } = useVeeDashboardStyles();
-
-    return (
-        <Content className={wrapper}>
-            <ContentLayout 
-              title="Use AI to give development superpowers and faster delivery."
-              subtitle="Ensure a smoother and simpler workflow by selecting one of the options below"
-              styleCustom={content}
-              >
-              {options.map(option => (
-                <MenuOptionCard 
-                  key={option.id} 
-                  icon={option.icon} 
-                  title={option.title} 
-                  description={option.description}
-                  path={option.path}
-                  />
-              ))}
-            </ContentLayout>
-        </Content>
-    )
-}
+export const VeeDashboard = () => (
+  <PageLayout
+    title="Use AI to give development superpowers and faster delivery."
+    subtitle="Ensure a smoother and simpler workflow by selecting one of the options below"
+  >
+    {options.map(option => (
+      <MenuOptionCard
+        key={option.id}
+        icon={option.icon}
+        title={option.title}
+        description={option.description}
+        path={option.path}
+      />
+    ))}
+  </PageLayout>
+);
--- plugins/vee/src/components/veeDashboard/menuOptionCard/types.ts
+++
@@ -1,6 +0,0 @@
-export interface MenuOptionCardProps {
-    icon: React.JSX.Element,
-    title: string,
-    description: string,
-    path: string
-}
