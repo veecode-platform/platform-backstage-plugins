8eb7f67c586c325abf84dfd35a7d700e3d23cd52
ValberJunior <valberjr1992@gmail.com>
2025-03-07 01:18:53 -0300

    feat: ✨ new export

--- plugins/vee/src/components/index.ts
+++ plugins/vee/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./assistantAIContent";
 export { AssistantAIMenu } from "./assistantAIMenu";
 export { VeeDashboard } from "./veeDashboard";
+export { VeeHomepage } from "./veeHomepage"
