51f0fa9dd8906e2cd03f887b91c8cb2ba58857ad
ValberJunior <valberjr1992@gmail.com>
2025-03-06 22:29:47 -0300

    feat: ✨ add new subRoutes

--- plugins/vee/src/routes.ts
+++ plugins/vee/src/routes.ts
@@ -1,5 +1,17 @@
-import { createRouteRef } from '@backstage/core-plugin-api';
+import { createRouteRef, createSubRouteRef } from '@backstage/core-plugin-api';
 
 export const rootRouteRef = createRouteRef({
   id: 'vee'
 })
+
+export const settingsRouteRef = createSubRouteRef({
+  id: 'settings',
+  path: '/settings',
+  parent: rootRouteRef,
+});
+
+export const scaffolderAIRouteRef = createSubRouteRef({
+  id: 'scaffolder-ai',
+  path: '/scaffolder-ai',
+  parent: rootRouteRef,
+});
