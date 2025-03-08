993c6ab93dbab72e0c057e14ac6a67bcbc5b8c25
ValberJunior <valberjr1992@gmail.com>
2025-03-08 00:09:23 -0300

    feat: ✨ create SettingsPageRoot

---
+++ plugins/vee/src/components/settingsPage/settingsPageRoot.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { SettingsPage } from "./SettingsPage";
+import { ManageStacks } from "./stacks";
+import { ManagePlugins } from "./plugins";
+
+
+export const SettingsPageRoot = () => {
+    return (
+        <Routes>
+            <Route path="" element={<SettingsPage/>}/>
+            <Route path="manage-stacks" element={<ManageStacks/>}/>
+            <Route path="manage-plugins" element={<ManagePlugins/>}/>
+        </Routes>
+    )
+}
