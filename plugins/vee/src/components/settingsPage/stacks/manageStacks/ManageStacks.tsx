5314bf3791221d81956e8f6fa97516b9333f6569
ValberJunior <valberjr1992@gmail.com>
2025-03-08 00:10:17 -0300

    feat: ✨ Create manage stacks component

---
+++ plugins/vee/src/components/settingsPage/stacks/manageStacks/ManageStacks.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { IStack } from "@veecode-platform/backstage-plugin-vee-common";
+import { PageLayout, TableComponent } from "../../../shared";
+
+const rows : IStack[]= [
+    { id: "000", name: "Java", source: "http://xptop.com.br" },
+    { id: "001", name: "AWS EC2", source: "http://xptop.com.br" },
+    { id: "002", name: "Express", source: "http://xptop.com.br" },
+    { id: "003", name: "NextJs", source: "http://xptop.com.br" },
+  ];
+  
+
+export const ManageStacks = () => {
+    return (
+        <PageLayout
+         label="Manage Stacks"
+         title="List, create, edite or delete Stacks"
+         goBack
+         >
+          <TableComponent 
+             title="Stacks" 
+             data={rows as IStack[]}
+             noId
+             actions
+             onEdit={()=>{}}
+             onDelete={()=>{}}
+             />
+        </PageLayout>
+    )
+}
