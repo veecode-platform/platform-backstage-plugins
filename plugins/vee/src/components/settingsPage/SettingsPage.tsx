d460101ac657cb93cc22a9af78d2d87c3a67a688
ValberJunior <valberjr1992@gmail.com>
2025-03-08 00:08:50 -0300

    refactor: 📦

--- plugins/vee/src/components/settingsPage/SettingsPage.tsx
+++ plugins/vee/src/components/settingsPage/SettingsPage.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { MenuOptionCard, PageLayout } from "../shared";
 import { settingsOptions } from "./settings";
-import { useNavigate } from "react-router-dom";
 
 export const SettingsPage = () => {
-
-    const navigate = useNavigate();
-    const handleBack =  () => navigate(-1)
 
     return (
         <PageLayout 
           label="Settings" 
-          subtitle="configure your assistant according to the standard and needs of your squad."
-          handleBack={handleBack}
+          subtitle="Configure your assistant according to the standard and needs of your squad."
+          goBack
           >
             {settingsOptions.map( option => (
                 <MenuOptionCard
