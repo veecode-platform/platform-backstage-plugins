fea49257f4a1bfa55697523bfa9fa130e2f73e91
ValberJunior <valberjr1992@gmail.com>
2025-03-08 00:11:22 -0300

    refactor: 📦

--- plugins/vee/src/components/veeHomepage/VeeHomepage.tsx
+++ plugins/vee/src/components/veeHomepage/VeeHomepage.tsx
@@ -1,12 +1,12 @@
-import { SettingsPage } from "../settingsPage";
-import { VeeDashboard } from "../veeDashboard";
 import React from "react";
+import { VeeDashboard } from "../veeDashboard";
 import { Route, Routes } from "react-router-dom";
+import { SettingsPageRoot } from "../settingsPage";
 
 export const VeeHomepage = () => (
         <Routes>
             <Route path="" element={<VeeDashboard/>}/>
-            <Route path="/settings" element={<SettingsPage/>}/>
+            <Route path="/settings/*" element={<SettingsPageRoot/>}/>
             <Route path="/scaffolder-ai" element={<h1>Scaffolder-ai</h1>}/>
         </Routes>
     )
