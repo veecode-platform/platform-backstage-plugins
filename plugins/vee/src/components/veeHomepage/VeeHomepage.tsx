5e315cbb73d556bd1f7e00085d03c242471346d1
ValberJunior <valberjr1992@gmail.com>
2025-03-10 08:31:24 -0300

    feat: ✨ add VeeProvider

--- plugins/vee/src/components/veeHomepage/VeeHomepage.tsx
+++ plugins/vee/src/components/veeHomepage/VeeHomepage.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { VeeDashboard } from "../veeDashboard";
 import { Route, Routes } from "react-router-dom";
 import { SettingsPageRoot } from "../settingsPage";
+import { VeeProvider } from "../../context";
 
 export const VeeHomepage = () => (
-        <Routes>
-            <Route path="" element={<VeeDashboard/>}/>
-            <Route path="/settings/*" element={<SettingsPageRoot/>}/>
-            <Route path="/scaffolder-ai" element={<h1>Scaffolder-ai</h1>}/>
-        </Routes>
+            <VeeProvider>
+                <Routes>
+                    <Route path="" element={<VeeDashboard/>}/>
+                    <Route path="/settings/*" element={<SettingsPageRoot/>}/>
+                    <Route path="/scaffolder-ai" element={<h1>Scaffolder-ai</h1>}/>
+                </Routes>
+            </VeeProvider>
     )
