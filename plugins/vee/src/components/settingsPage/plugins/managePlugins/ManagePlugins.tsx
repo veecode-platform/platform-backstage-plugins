4e70cf4feeab5a09a72bfa175fdae7662b56387b
ValberJunior <valberjr1992@gmail.com>
2025-03-10 08:21:56 -0300

    refactor: 📦 update managePlugins

--- plugins/vee/src/components/settingsPage/plugins/managePlugins/ManagePlugins.tsx
+++ plugins/vee/src/components/settingsPage/plugins/managePlugins/ManagePlugins.tsx
@@ -1,34 +1,76 @@
 import React from "react";
-import { PageLayout, TableComponent } from "../../../shared";
-import { ManagePluginsRow } from "./types";
-
-const rows : ManagePluginsRow[]= [
-    { id: "000", name: "Github workflows", annotations: ['github-workflows/card'] },
-    { id: "001", name: "Gitlab Pipelines", annotations: ['gitlab-pipelines/card'] },
-    { id: "002", name: "Kubernetes", annotations: ['backstage/clusterName'] },
-    { id: "003", name: "Analyzer GPT", annotations: ['backstage/clusterName'] },
-    { id: "002", name: "Kubernetes", annotations: ['backstage/clusterName'] },
-    { id: "003", name: "Analyzer GPT", annotations: ['backstage/clusterName'] },
-    { id: "002", name: "Kubernetes", annotations: ['backstage/clusterName'] },
-    { id: "003", name: "Analyzer GPT", annotations: ['backstage/clusterName'] },
-  ];
-  
+import { PageLayout, TableComponent,ModalComponent } from "../../../shared";
+import { ManagePluginsRow, ModalVariantKey } from "./types";
+import { AddPlugin } from "./addPlugin/AddPlugin";
+import useAsync from "react-use/esm/useAsync";
+import { useVeeContext } from "../../../../context";
+import { UpdatePlugin } from "./updatePlugin";
 
 export const ManagePlugins = () => {
+
+    const [ showModal, setShowModal] = React.useState<boolean>(false);
+    const { listAllPlugins, getPluginById, addPluginSelected } = useVeeContext();
+    const [ modalVariant, setModalVariant ] = React.useState<ModalVariantKey>(null);
+
+    const { value: allPlugins, loading, error } = useAsync(listAllPlugins,[]);
+
+    const rows = React.useMemo(() => {
+        return allPlugins?.map(plugin => ({
+            id: plugin.id,
+            name: plugin.name,
+            annotations: [plugin.annotations[0]?.annotation as string]
+        })) || [];
+    }, [allPlugins]);
+
+    const modalTitle = () => {
+        if (modalVariant){
+           if (modalVariant === "create") return "Add a new plugin"
+           return "Edit Plugin"
+        }
+        return "Manage plugins"
+    }
+
+    const handleClose = () => setShowModal(!showModal);
+
+    const addNewPlugin = () => {
+        setShowModal(true);
+        setModalVariant("create")
+    }
+    const updatePlugin = async (id:string) => {
+        const plugin = await getPluginById(id);
+        // eslint-disable-next-line no-console
+        console.log("SALVEI ESSE PRUGGGG >>>", plugin)
+        if(plugin) addPluginSelected(plugin);
+        setShowModal(true);
+        setModalVariant("edit")
+    }
+
     return (
-        <PageLayout
-         label="Manage Plugins"
-         title="List, add, edite or delete plugins"
-         goBack
-         >
-          <TableComponent 
-             title="Plugins" 
-             data={rows as ManagePluginsRow[]}
-             noId
-             actions
-             onEdit={()=>{}}
-             onDelete={()=>{}}
-             />
-        </PageLayout>
+        <>
+         <PageLayout
+          label="Manage Plugins"
+          title="List, add, edite or delete plugins"
+          createAction={addNewPlugin}
+          goBack
+          >
+           <TableComponent 
+              title="Plugins" 
+              loading={loading}
+              error={error}
+              data={rows as ManagePluginsRow[]}
+              actions
+              onEdit={updatePlugin}
+              onDelete={()=>{}}
+              />
+         </PageLayout>
+         <ModalComponent 
+           title={modalTitle()}
+           open={showModal} 
+           handleClose={handleClose} 
+           >
+          { modalVariant === "create" && <AddPlugin/>}
+          { modalVariant === "edit" && <UpdatePlugin/>}
+         </ModalComponent>
+       </>
     )
 }
--- plugins/vee/src/components/settingsPage/plugins/managePlugins/types.ts
+++ plugins/vee/src/components/settingsPage/plugins/managePlugins/types.ts
@@ -1,3 +1,5 @@
 import { IPlugin } from "@veecode-platform/backstage-plugin-vee-common";
 
 export type ManagePluginsRow = Omit<IPlugin,"annotations"> & {annotations: string[]}
+
+export type ModalVariantKey = "edit" | "create" | null
