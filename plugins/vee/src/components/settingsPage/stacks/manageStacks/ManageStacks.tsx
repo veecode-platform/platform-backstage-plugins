479a525152104ff99f340c0320544998bcc895bb
ValberJunior <valberjr1992@gmail.com>
2025-03-11 10:43:41 -0300

    refactor: 📦

--- plugins/vee/src/components/settingsPage/stacks/manageStacks/ManageStacks.tsx
+++ plugins/vee/src/components/settingsPage/stacks/manageStacks/ManageStacks.tsx
@@ -1,29 +1,77 @@
 import React from "react";
-import { IStack } from "@veecode-platform/backstage-plugin-vee-common";
-import { PageLayout, TableComponent } from "../../../shared";
-
-const rows : IStack[]= [
-    { id: "000", name: "Java", source: "http://xptop.com.br" },
-    { id: "001", name: "AWS EC2", source: "http://xptop.com.br" },
-    { id: "002", name: "Express", source: "http://xptop.com.br" },
-    { id: "003", name: "NextJs", source: "http://xptop.com.br" },
-  ];
-  
+import { ModalComponent, PageLayout, TableComponent } from "../../../shared";
+import { AddStack } from "./addStack";
+import { UpdateStack } from "./updateStack";
+import useAsync from "react-use/esm/useAsync";
+import { ModalVariantKey } from "components/shared/modalComponent/types";
+import { useVeeContext } from "../../../../context";
+import { ManageStacksRow } from "./types";
 
 export const ManageStacks = () => {
+
+    const [ showModal, setShowModal] = React.useState<boolean>(false);
+    const { allStacksState, listAllStacks, getStackById, addStackSelected, removeStack } = useVeeContext();
+    const [ modalVariant, setModalVariant ] = React.useState<ModalVariantKey>(null);
+
+    const { loading, error } = useAsync(listAllStacks,[]);
+
+    const rows = React.useMemo(() => {
+        return allStacksState?.map(stack => ({
+            id: stack.id,
+            name: stack.name,
+            plugins: stack.plugins ? stack.plugins.flatMap(plugins => plugins.name) : []
+        })) || [];
+    }, [allStacksState]);
+
+    const modalTitle = () => {
+        if (modalVariant){
+           if (modalVariant === "create") return "Add a new stack"
+           return "Edit stack"
+        }
+        return "Manage stacks"
+    }
+
+    const handleClose = () => setShowModal(!showModal);
+
+    const addNewStack = () => {
+        setShowModal(true);
+        setModalVariant("create")
+    }
+    const handleUpdateStack = async (id:string) => {
+        const plugin = await getStackById(id);
+        if(plugin) addStackSelected(plugin);
+        setShowModal(true);
+        setModalVariant("edit")
+    }
+
+    const handleRemoveStack = async (id:string)=>{
+        await removeStack(id)
+    }
+
     return (
         <PageLayout
          label="Manage Stacks"
          title="List, create, edite or delete Stacks"
+         createAction={addNewStack}
          goBack
          >
           <TableComponent 
-             title="Stacks" 
-             data={rows as IStack[]}
-             actions
-             onEdit={()=>{}}
-             onDelete={()=>{}}
+              title="Stacks" 
+              loading={loading}
+              error={error}
+              data={rows as ManageStacksRow[]}
+              actions
+              onEdit={handleUpdateStack}
+              onDelete={handleRemoveStack}
              />
+             <ModalComponent 
+                title={modalTitle()}
+                open={showModal} 
+                handleClose={handleClose} 
+                >
+                { modalVariant === "create" && <AddStack onCloseModal={handleClose}/>}
+                { modalVariant === "edit" && <UpdateStack onCloseModal={handleClose}/>}
+             </ModalComponent>
         </PageLayout>
     )
 }
