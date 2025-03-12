f1c05c415a8105bacf12a5730a4c0b44d140c484
ValberJunior <valberjr1992@gmail.com>
2025-03-12 16:23:11 -0300

    refactor: 📦

--- plugins/vee/src/components/veeDashboard/VeeDashboard.tsx
+++ plugins/vee/src/components/veeDashboard/VeeDashboard.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { PageLayout } from "../shared";
 import { options } from "./options";
 import { MenuOptionCard } from "../shared";
+import  Grid2 from "@mui/material/Grid2";
 
 export const VeeDashboard = () => (
   <PageLayout
     title="Use AI to give development superpowers and faster delivery."
     subtitle="Ensure a smoother and simpler workflow by selecting one of the options below"
   >
+   <Grid2 container spacing={2}>
     {options.map(option => (
       <MenuOptionCard
         key={option.id}
@@ -17,5 +19,6 @@ export const VeeDashboard = () => (
         path={option.path}
       />
     ))}
+    </Grid2>
   </PageLayout>
 );
--- plugins/vee/src/components/veeHomepage/VeeHomepage.tsx
+++ plugins/vee/src/components/veeHomepage/VeeHomepage.tsx
@@ -3,13 +3,14 @@ import { VeeDashboard } from "../veeDashboard";
 import { Route, Routes } from "react-router-dom";
 import { SettingsPageRoot } from "../settingsPage";
 import { VeeProvider } from "../../context";
+import { ScaffolderAIPageRoot } from "../scaffolder-ai";
 
 export const VeeHomepage = () => (
             <VeeProvider>
               <Routes>
                   <Route path="" element={<VeeDashboard/>}/>
                   <Route path="/settings/*" element={<SettingsPageRoot/>}/>
-                  <Route path="/scaffolder-ai" element={<h1>Scaffolder-ai</h1>}/>
+                  <Route path="/scaffolder-ai/*" element={<ScaffolderAIPageRoot/>}/>
               </Routes>
             </VeeProvider>
     )
