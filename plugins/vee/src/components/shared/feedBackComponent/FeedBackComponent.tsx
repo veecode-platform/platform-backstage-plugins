9ab20bee8fd5df9ec68ec8de282ce9e40f362013
ValberJunior <valberjr1992@gmail.com>
2025-03-13 15:16:17 -0300

    refactor: 📦

--- plugins/vee/src/components/shared/feedBackComponent/FeedBackComponent.tsx
+++ plugins/vee/src/components/shared/feedBackComponent/FeedBackComponent.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core'
+import  Backdrop  from '@mui/material/Backdrop';
+import StarsAnimationData from '../../../assets/stars.json';
+import  Grid2  from '@mui/material/Grid2';
+import Box from "@mui/material/Box";
+import Typography from '@mui/material/Typography';
 import Lottie from 'react-lottie';
-import { FeedbackComponentProps } from './types';
-import { useFeedbackComponentStyles } from './styles';
+import { FeedbackComponentProps, FeedbackWrapperProps } from './types';
+import { useFeedbackStyles } from './styles';
 import SuccessAnimationData from '../../../assets/success.json';
 import ErrorAnimationData from '../../../assets/error.json';
+
+const lootieStyle = { 
+    position: 'absolute', 
+    top: '50%', 
+    left: '50%',
+    transform: 'translate(-50%,-50%)'
+   } as React.CSSProperties
 
 const SuccessAnimation = () => {
 
@@ -18,11 +29,12 @@ const SuccessAnimation = () => {
   };
 
   return (
-      <Lottie 
-	      options={defaultOptions}
-        height={150}
-        width={150}
-      />
+    <Lottie
+      options={defaultOptions}
+      height={150}
+      width={150}
+      style={lootieStyle}
+    />
   );
 }
 
@@ -38,36 +50,93 @@ const ErrorAnimation = () => {
   };
 
   return (
-      <Lottie 
-	      options={defaultOptions}
-        height={80}
-        width={90}
-      />
+    <Lottie
+      options={defaultOptions}
+      height={80}
+      width={90}
+      style={lootieStyle}
+    />
   );
 }
 
-export const FeedbackComponent : React.FC<FeedbackComponentProps>= (props) => {
+const LoadingAnimation = () => {
 
-  const { variant, message, actions } = props;
-  const { blur, root, content, animation, btnGroup } = useFeedbackComponentStyles();
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: StarsAnimationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
 
   return (
-   <div className={blur}>
-    <Box
+    <Lottie
+      options={defaultOptions}
+      height={120}
+      width={120}
+      style={lootieStyle}
+    />
+  );
+}
+
+
+export const FeedbackWrapper : React.FC<FeedbackWrapperProps>= (props) => {
+
+  const { open, onClose, message, actions, children } = props;
+  const {  root, content, animation, textContent , btnGroup } = useFeedbackStyles();
+
+  return (
+    <Backdrop
       className={root}
-     > 
-      <div className={content}>
-         <div className={animation}>
-           {variant === "success" ? <SuccessAnimation/> : <ErrorAnimation/>}
-          </div>
-          <Typography variant="h6" style={{marginLeft: '2rem'}}>{message}</Typography>
-      </div>
+      open={open}
+      onClick={onClose}
+    >
+      <Grid2 container className={content}>
+         <Grid2 size={4} className={animation}>
+           {children}
+          </Grid2>
+          <Grid2 size={8}>
+              <Typography variant="h5" component="div" className={textContent}>
+                {message}
+              </Typography>
+           </Grid2>
+      </Grid2>
       {!!actions && (
-         <div className={btnGroup}>
+         <Box className={btnGroup}>
          {actions}
-         </div>
+         </Box>
       )}
-    </Box>
-    </div>
+    </Backdrop>
+  )
+}
+
+
+export const FeedbackComponent : React.FC<FeedbackComponentProps>= (props) => {
+
+  const { variant, ...rest } = props;
+
+  if(variant === "success") return (
+     <FeedbackWrapper
+      {...rest}
+      >
+       <SuccessAnimation/>
+     </FeedbackWrapper>
+  )
+
+  if(variant === "error") return (
+    <FeedbackWrapper
+     {...rest}
+     >
+      <ErrorAnimation/>
+    </FeedbackWrapper>
+  )
+
+  return (
+    <FeedbackWrapper
+    {...rest}
+    >
+     <LoadingAnimation/>
+   </FeedbackWrapper>
   )
 }
