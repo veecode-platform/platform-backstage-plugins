ca89d4b924a603323a00dbe6d611102d8589d17b
ValberJunior <valberjr1992@gmail.com>
2025-03-13 15:13:41 -0300

    refactor: 📦

--- plugins/vee/src/components/generateTemplate/generateTemplate.tsx
+++ plugins/vee/src/components/generateTemplate/generateTemplate.tsx
@@ -7,13 +7,29 @@ import { useVeeContext } from '../../context';
 import useAsync from 'react-use/esm/useAsync';
 import { PluginList } from '../plugins';
 import type { PluginListProps } from "../plugins/pluginList/types"
-import { ModalComponent, PageLayout } from '../shared';
+import { FeedbackComponent, ModalComponent, PageLayout } from '../shared';
+import { GenerateTemplateWrapperProps } from './types';
+
+const GenerateTemplateWrapper : React.FC<GenerateTemplateWrapperProps> = (props) => {
+  const { children, createAction} = props;
+  return (
+    <PageLayout
+        title="Select plugins & generate a template with AI"
+        label="Generate a template"
+        goBack
+        createAction={createAction}
+      >
+        {children}
+      </PageLayout>
+  )
+}
 
 export const GenerateTemplate = () => {
 
   const { stackId } = useParams();
   const [ instructions, setInstructions ] = React.useState<InstructionsProps|null>(null);
   const [ showModal, setShowModal] = React.useState<boolean>(false);
+  const [ templateProcessing, setTemplateProcessing ] = React.useState<boolean>(false);
   const { getStackById } = useVeeContext();
 
   const { value: allPlugins, loading, error } = useAsync(async()=>{
@@ -37,9 +53,7 @@ export const GenerateTemplate = () => {
       },[allPlugins]);
 
   const handleClose = () => setShowModal(!showModal);
-  
   const handleSubmitInstructions = () => setShowModal(true);
-  
   const resetInstructionsState = () => setInstructions(null);
   
   React.useEffect(()=>{
@@ -49,22 +63,31 @@ export const GenerateTemplate = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [stackId])
 
+
+  if(loading) return (    // TODO 
+   <GenerateTemplateWrapper 
+     createAction={handleSubmitInstructions}>
+      <h1>Loading...</h1>
+   </GenerateTemplateWrapper>);
+
+if(error) return (   // TODO 
+  <GenerateTemplateWrapper 
+    createAction={handleSubmitInstructions}>
+     <h1>Error...</h1>
+  </GenerateTemplateWrapper>);
+
+
   return (
     <>
-      <PageLayout
-        title="Select plugins & generate a template with AI"
-        label="Generate a template"
-        goBack
-        createAction={handleSubmitInstructions}
-      >
-        {loading && <h1>Loading...</h1>}
-        {error && <h1>Error...</h1>}
+      <GenerateTemplateWrapper 
+       createAction={handleSubmitInstructions}>
         <PluginList
           data={plugins}
           instructions={instructions}
           onSaveInstructions={setInstructions}
         />
-      </PageLayout>
+      </GenerateTemplateWrapper>
+
       <ModalComponent
         title="Template information"
         open={showModal}
@@ -75,8 +98,15 @@ export const GenerateTemplate = () => {
           instructions={instructions}
           onSaveInstructions={setInstructions}
           resetInstructions={resetInstructionsState}
+          onTemplateProcessing={setTemplateProcessing}
         />
       </ModalComponent>
+
+     <FeedbackComponent 
+      open={templateProcessing} 
+      variant="loading" 
+      message="Generate a new template"
+      />        
     </>
   );
 };
