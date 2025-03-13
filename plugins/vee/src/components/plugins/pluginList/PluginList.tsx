28c9496abbcf3fe8de6eff80f2424766c378fe06
ValberJunior <valberjr1992@gmail.com>
2025-03-13 08:27:22 -0300

    refactor: 📦

--- plugins/vee/src/components/plugins/pluginList/PluginList.tsx
+++ plugins/vee/src/components/plugins/pluginList/PluginList.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { PageLayout } from "../../shared"
+import { ModalComponent, PageLayout } from "../../shared"
 import { useVeeContext } from "../../../context";
 import useAsync from "react-use/esm/useAsync";
 import { PluginListProps } from "./listComponent/types";
 import { ListComponent } from "./listComponent";
 import { useParams } from "react-router-dom";
+import { GenerateTemplate } from "../../generateTemplate";
+import { InstructionsProps } from "../../../utils/types";
 
 export const PluginList = () => {
 
     const { stackId } = useParams();
+    const [ instructions, setInstructions ] = React.useState<InstructionsProps|null>(null);
+    const [ showModal, setShowModal] = React.useState<boolean>(false);
     const { getStackById } = useVeeContext();
+
     const { value: allPlugins, loading, error } = useAsync(async()=>{
         if(stackId){
             const stack = await getStackById(stackId);
@@ -18,6 +23,7 @@ export const PluginList = () => {
         }
         return []
     },[]); // check error and loading
+
     const plugins : PluginListProps[] = React.useMemo(()=>{
        if(allPlugins){
         return allPlugins.map(plugin => ({
@@ -27,18 +33,52 @@ export const PluginList = () => {
         }))
        }
        return []
-    },[allPlugins])
+    },[allPlugins]);
+
+    const handleClose = () => setShowModal(!showModal);
+
+    const handleSubmitInstructions = () => {
+        setShowModal(true);
+    }
+
+    const resetInstructionsState = () => setInstructions(null);
+
+
+    React.useEffect(()=>{
+        if(stackId){
+            setInstructions({...instructions as InstructionsProps, stackId: stackId})
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [stackId])
 
     return (
+        <>
         <PageLayout
           title="Select plugins to add to the template"
           label="Plugins"
           goBack
-          createAction={()=>{}}
+          createAction={handleSubmitInstructions}
          >
-            {loading && <h1>Loading...</h1>}
-            {error && <h1>Error...</h1>}
-            <ListComponent data={plugins}/>
+          {loading && <h1>Loading...</h1>}
+          {error && <h1>Error...</h1>}
+        <ListComponent 
+         data={plugins}
+         instructions={instructions}
+         onSaveInstructions={setInstructions}
+         />
         </PageLayout>
+         <ModalComponent 
+           title="Template information"
+           open={showModal} 
+           handleClose={handleClose} 
+           >
+          <GenerateTemplate
+           onCloseModal={handleClose}
+           instructions={instructions}
+           onSaveInstructions={setInstructions}
+           resetInstructions={resetInstructionsState}
+          />
+         </ModalComponent>        
+        </>
     )
 }
