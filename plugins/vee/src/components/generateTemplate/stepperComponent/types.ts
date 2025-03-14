046e55d9ec6d0decefbaecd7dd6fa7adcb22e472
ValberJunior <valberjr1992@gmail.com>
2025-03-14 11:33:55 -0300

    refactor: 📦

--- plugins/vee/src/components/generateTemplate/stepperComponent/types.ts
+++ plugins/vee/src/components/generateTemplate/stepperComponent/types.ts
@@ -1,9 +1,4 @@
-import { InstructionsProps } from "../../../utils/types";
-
 export interface StepperComponentProps {
     onCloseModal: () => void;
-    instructions: InstructionsProps | null;
-    resetInstructions: () => void;
-    onSaveInstructions: React.Dispatch<React.SetStateAction<InstructionsProps | null>>;
     onTemplateProcessing: React.Dispatch<React.SetStateAction<boolean>>
 }
