c2d1933b49644fa5938151fb27f1797b15e7a807
ValberJunior <valberjr1992@gmail.com>
2025-03-17 15:34:02 -0300

    feat: ✨ add getContentBySource

--- plugins/vee/src/api/git/providers/github/type.ts
+++ plugins/vee/src/api/git/providers/github/type.ts
@@ -1,5 +1,6 @@
 import { FileContent, PullRequestResponse } from "@veecode-platform/backstage-plugin-vee-common";
 
 export interface IGithubManager {
-    createPullRequest(files: FileContent[], location: string, title: string, message: string): Promise<PullRequestResponse>
+    createPullRequest(files: FileContent[], location: string, title: string, message: string): Promise<PullRequestResponse>;
+    getContentBySource(source: string): Promise<string>
 }
