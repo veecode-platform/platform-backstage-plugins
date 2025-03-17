4bb540734844ae9bc4b54608efee299a56d5e80e
ValberJunior <valberjr1992@gmail.com>
2025-03-17 15:37:47 -0300

    feat: ✨ replace output placeholder with TemplateOutput component

--- plugins/vee/src/components/scaffolder-ai/ScaffolderAIPageRoot.tsx
+++ plugins/vee/src/components/scaffolder-ai/ScaffolderAIPageRoot.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { StackList } from "../stacks";
 import { GenerateTemplate } from "../generateTemplate";
+import { TemplateOutput } from "../templateOutput";
 
 
 export const ScaffolderAIPageRoot = () => {
@@ -9,7 +10,7 @@ export const ScaffolderAIPageRoot = () => {
         <Routes>
             <Route path="" element={<StackList/>}/>
             <Route path="/:stackId" element={<GenerateTemplate/>}/>
-            <Route path="output" element={<h1>Output</h1>}/>
+            <Route path="output" element={<TemplateOutput/>}/>
         </Routes>
     )
 }
