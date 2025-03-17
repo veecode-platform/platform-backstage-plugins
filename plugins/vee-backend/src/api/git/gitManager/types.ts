3551360e5c9f352f0febb512691ced06a7088ae4
ValberJunior <valberjr1992@gmail.com>
2025-03-17 15:46:04 -0300

    refactor: 📦 update returnRepoInfo to accept structured parameters and add partialClone method for enhanced repository management

--- plugins/vee-backend/src/api/git/gitManager/types.ts
+++ plugins/vee-backend/src/api/git/gitManager/types.ts
@@ -1,10 +1,17 @@
 import { IRepository } from "@veecode-platform/backstage-plugin-vee-common";
 
 export interface IGitManager {
-    returnRepoInfo(location: string): Promise<{
+    returnRepoInfo({location, partial}:ReturnRepoInfoParams): Promise<{
         localPath: string;
         repoUrl: string;
         branch: string;
+        folderPath: string | null
     }>,
     cloneRepo(token: string, localPath: string, repoUrl: string, branch: string): Promise<IRepository>;
+    partialClone(token: string, localPath: string, repoUrl: string, branch: string, folderPath: string | null): Promise<IRepository>
+}
+
+export type ReturnRepoInfoParams = {
+    location: string,
+    partial?:boolean
 }
