9664ed8f40475a73e58580ef2c963e4931bba150
ValberJunior <valberjr1992@gmail.com>
2025-03-17 15:45:00 -0300

    feat: ✨ refactor IVeeClient methods to use structured parameters for improved clarity and type safety

--- plugins/vee-backend/src/api/types.ts
+++ plugins/vee-backend/src/api/types.ts
@@ -1,12 +1,52 @@
 import { DefaultResponse, FileContent, IChat } from "@veecode-platform/backstage-plugin-vee-common";
 
 export interface IVeeClient{
-    submitDataToVectorStore(engine:string, repoName: string, files: FileContent[]): Promise<{
+    submitDataToVectorStore({
+        engine,
+        repoName, 
+        files
+       }: SubmitDataToVectorStoreParams
+       ): Promise<{
         vectorStoreId: string;
         status: string;
         message: string;
     }>,
-    chat(engine:string, vectorStoreId: string, prompt: string, repoName:string, repoStructure:string): Promise<IChat>,
-    clearHistory(engine:string,vectorStoreId: string, assistantId: string, threadId: string): Promise<DefaultResponse>
+    chat({
+       engine,
+       vectorStoreId,
+       prompt,
+       repoName,
+       repoStructure
+     }:ChatParams
+    ): Promise<IChat>,
+    clearHistory({
+        engine,
+        vectorStoreId, 
+        assistantId,
+        threadId
+    }: ClearHistoryParams
+    ): Promise<DefaultResponse>
 };
 
+export type SubmitDataToVectorStoreParams = {
+    engine: string,
+    repoName: string,
+    files: FileContent[]
+}
+
+export type ChatParams = {
+    engine:string, 
+    vectorStoreId: string, 
+    prompt: string, 
+    repoName:string, 
+    repoStructure:string
+    isTemplate?:boolean,
+    useDataset?:boolean
+}
+
+export type ClearHistoryParams = {
+    engine:string,
+    vectorStoreId: string, 
+    assistantId: string, 
+    threadId: string
+}
