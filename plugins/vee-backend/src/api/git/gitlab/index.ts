a3966dfc2c88964042acf768afdc85b9004eb004
ValberJunior <valberjr1992@gmail.com>
2025-03-17 15:45:30 -0300

    refactor: 📦 update returnRepoInfo method to accept structured parameters and include folderPath in the response

--- plugins/vee-backend/src/api/git/gitlab/index.ts
+++ plugins/vee-backend/src/api/git/gitlab/index.ts
@@ -1,17 +1,19 @@
 import { extractGitLabInfo } from "../../../utils/helpers/extractGitlabInfo";
-import { IGitlabManager } from "./types";
+import { IGitlabManager, ReturnGitlabRepoInfo } from "./types";
 
 export class GitlabManager implements IGitlabManager {
 
-    async returnRepoInfo (url:string) {
+    async returnRepoInfo ({url, partial}:ReturnGitlabRepoInfo) {
         const { host, group, repo, branch } = extractGitLabInfo(url);
         const repoUrl = `https://${host}/${group}/${repo}.git`;
         const localPath = `./temp-${repo}`;
+        const folderPath =  partial ? '' : null // TODO 
     
         return {
           localPath,
           repoUrl,
-          branch
+          branch,
+          folderPath
         }
       }
 }
