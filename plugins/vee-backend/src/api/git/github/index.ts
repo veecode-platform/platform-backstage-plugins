8c1338a977258a10d19fb27fbce9ba2b1edfcf0d
ValberJunior <valberjr1992@gmail.com>
2025-03-17 15:45:09 -0300

    feat: ✨ update returnRepoInfo method to accept structured parameters and include folderPath in the response

--- plugins/vee-backend/src/api/git/github/index.ts
+++ plugins/vee-backend/src/api/git/github/index.ts
@@ -1,17 +1,19 @@
 import { extractGitHubInfo } from "../../../utils/helpers/extractGithubInfo";
-import { IGithubManager } from "./types";
+import { IGithubManager, ReturnGithubRepoInfo } from "./types";
 
 export class GithubManager implements IGithubManager {
 
-  async returnRepoInfo(url: string) {
-    const { host, owner, repo, branch } = extractGitHubInfo(url);
+  async returnRepoInfo({url, partial}:ReturnGithubRepoInfo) {
+    const { host, owner, repo, branch,folderPath } = extractGitHubInfo(url,partial);
     const repoUrl = `https://${host}/${owner}/${repo}.git`;
     const localPath = `./temp-${repo}`;
+    const folderPathResult = folderPath ? folderPath : null
 
     return {
       localPath,
       repoUrl,
       branch,
+      folderPath: folderPathResult
     };
   }
 }
