852eaf7f7454989d9321cddc88849f5d5e2f5244
ValberJunior <valberjr1992@gmail.com>
2025-03-17 15:47:10 -0300

    feat: ✨ enhance extractGitHubInfo function to support partial path extraction

--- plugins/vee-backend/src/utils/helpers/extractGithubInfo.ts
+++ plugins/vee-backend/src/utils/helpers/extractGithubInfo.ts
@@ -1,18 +1,33 @@
-export function extractGitHubInfo(url: string) {
+export function extractGitHubInfo(url:string, partial?:boolean) {
   const regex =
-    /^https:\/\/([^\/]+)\/([^\/]+)\/([^\/]+)\/tree\/([^\/]+)(?:\/.*)?$/;
+    /https:\/\/([^/]+)\/([^/]+)\/([^/]+)\/(blob|tree)\/([^/]+)(?:\/([^?]+))?/;
   const match = url.match(regex);
 
   if (!match) {
-    throw new Error("Invalid GitHub URL.");
+    throw new Error('URL inválida');
   }
 
-  const [_, host, owner, repo, branch] = match;
+  const [_, host, owner, repo, type, branch, path] = match;
+
+  if (partial && path) {
+    const folderPath = path.split('/').slice(0, -1).join('/');
+    return {
+      host,
+      owner,
+      repo,
+      type,
+      branch,
+      path,
+      folderPath,
+    };
+  }
 
   return {
     host,
     owner,
     repo,
+    type,
     branch,
+    path: path || '',
   };
-}
+};
