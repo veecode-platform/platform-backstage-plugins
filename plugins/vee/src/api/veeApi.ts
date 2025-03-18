5f0afc9c4067d6526a78908d51b353d36d863aa8
ValberJunior <valberjr1992@gmail.com>
2025-03-18 17:06:58 -0300

    feat: ✨ update addPlugin method to include docs parameter for enhanced plugin creation

--- plugins/vee/src/api/veeApi.ts
+++ plugins/vee/src/api/veeApi.ts
@@ -23,7 +23,7 @@ export interface VeeApi {
     removeStack(stackId: string): Promise<DeleteServiceResponse>;
     listPlugins(): Promise<IPlugin[]>;
     getPluginById(pluginId: string): Promise<IPlugin>;
-    addPlugin({ name, annotations }: CreatePluginParams): Promise<VeeResponse<IPlugin>>;
+    addPlugin({ name, docs, annotations }: CreatePluginParams): Promise<VeeResponse<IPlugin>>;
     editPlugin({ id, ...data }: ParamsWithRequiredId<IPlugin>): Promise<VeeResponse<IPlugin>>;
     removePlugin(pluginId: string): Promise<DeleteServiceResponse>
 }
