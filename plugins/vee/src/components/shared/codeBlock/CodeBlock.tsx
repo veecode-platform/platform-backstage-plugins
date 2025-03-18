6b06cbabebd7377f89e8d7db4de58034f278f09d
ValberJunior <valberjr1992@gmail.com>
2025-03-18 13:13:42 -0300

    feat: ✨ implement syntax highlighting in CodeBlockComponent using shiki for improved code presentation

--- plugins/vee/src/components/shared/codeBlock/CodeBlock.tsx
+++ plugins/vee/src/components/shared/codeBlock/CodeBlock.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import { CodeBlockProps } from './types';
-import { CodeBlock, dracula } from "react-code-blocks";
+import { useCodeBlockStyles } from './style';
+import { BundledLanguage, createHighlighter, SpecialLanguage } from 'shiki';
+import { allLanguages } from '../../../utils/constants/codeblock-languages';
 
-export const CodeBlockComponent : React.FC<CodeBlockProps> = ({ code, language }) => {
+const highlighter = await createHighlighter({
+  themes: ['github-dark-default'],
+  langs: allLanguages,
+})
+
+export const CodeBlockComponent: React.FC<CodeBlockProps> = (props) => {
+  const { root } = useCodeBlockStyles();
+  const { code, language } = props;
+  const [codeState, setCodeState] = React.useState<string>('');
+
+  React.useEffect(() => {
+    const fetchCode = () => {
+      try {
+        const codeString = highlighter.codeToHtml(code, {
+          lang: language as BundledLanguage | SpecialLanguage,
+          theme: 'github-dark-default',
+        });
+       setCodeState(codeString)
+      } catch (error) {
+       // console.error('Error fetching tokens:', error);
+       setCodeState("Error");
+      }
+    };
+
+    fetchCode(); // Chame a função assíncrona dentro do useEffect
+  }, [code, language]);
+
+  if (codeState === '') {
+    return <div>Loading...</div>; // TODO
+  }
+
   return (
-      <CodeBlock
-        text={code}
-        language={language}
-        theme={dracula} 
-        showLineNumbers={false}
-      />
+    <div className={root}>
+      <div dangerouslySetInnerHTML={{ __html: codeState }} />
+    </div>
   );
-}
+};
