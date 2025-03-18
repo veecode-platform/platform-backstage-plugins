0480abcf94504d3dd22c872be76024d516ed52bc
ValberJunior <valberjr1992@gmail.com>
2025-03-18 19:34:07 -0300

    feat: ✨ add route for ManageFixedOptions in SettingsPageRoot

--- plugins/vee/src/components/settingsPage/SettingsPageRoot.tsx
+++ plugins/vee/src/components/settingsPage/SettingsPageRoot.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { SettingsPage } from "./SettingsPage";
 import { ManageStacks } from "../stacks";
 import { ManagePlugins } from "../plugins";
+import { ManageFixedOptions } from "../fixedOptions";
 
 
 export const SettingsPageRoot = () => {
@@ -11,6 +12,7 @@ export const SettingsPageRoot = () => {
             <Route path="" element={<SettingsPage/>}/>
             <Route path="manage-stacks" element={<ManageStacks/>}/>
             <Route path="manage-plugins" element={<ManagePlugins/>}/>
+            <Route path="manage-fixed-options" element={<ManageFixedOptions/>}/>
         </Routes>
     )
 }
