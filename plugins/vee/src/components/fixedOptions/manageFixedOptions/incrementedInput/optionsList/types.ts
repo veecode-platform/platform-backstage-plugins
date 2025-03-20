3c77c982c1b394446b321e24c11ff1e07dbe873c
ValberJunior <valberjr1992@gmail.com>
2025-03-20 12:28:22 -0300

    feat: ✨ add OptionListProps interface for managing options in OptionsList component

---
+++ plugins/vee/src/components/fixedOptions/manageFixedOptions/incrementedInput/optionsList/types.ts
@@ -0,0 +1,8 @@
+import { IOption } from "@veecode-platform/backstage-plugin-vee-common";
+
+export interface OptionListProps {
+    data: IOption[],
+    onRemoveOption: (id: string) => void,
+    onEditOptionFromList: (optionData: IOption) => void,
+    onSaveOptions: () => void
+}
