de7577072d1b2328b94fbe2a6448c10a242dd20e
ValberJunior <valberjr1992@gmail.com>
2025-03-20 12:26:57 -0300

    refactor: 🗑️ remove OptionList component and related files

--- plugins/vee/src/components/fixedOptions/manageFixedOptions/incrementedInput/optionList/types.ts
+++
@@ -1,7 +0,0 @@
-import { IOption } from "@veecode-platform/backstage-plugin-vee-common";
-
-export interface OptionListProps {
-    data: IOption[],
-    onRemoveOption: (id: string) => void,
-    onEditOptionFromList: (optionData: IOption) => void
-}
