332e7b1543d8ab7dff28683c41751e30200b68e5
ValberJunior <valberjr1992@gmail.com>
2025-03-20 12:28:50 -0300

    fix: 🐛 enforce options property as required in FixedOptionStateType interface

--- plugins/vee/src/components/fixedOptions/manageFixedOptions/state/types.ts
+++ plugins/vee/src/components/fixedOptions/manageFixedOptions/state/types.ts
@@ -3,14 +3,5 @@ import { IOption } from "@veecode-platform/backstage-plugin-vee-common";
 export interface FixedOptionStateType {
     FixedOptionId?: string,
     type: string,
-    options?: IOption[]
+    options: IOption[]
   }
-
-  // export interface IOption {
-  //    id?:string,
-  //    fixed_option_id?: string,
-  //    label: string,
-  //    prompt: string,
-  //    created_at?: Date,
-  //    updated_at?: Date,
-  // }
