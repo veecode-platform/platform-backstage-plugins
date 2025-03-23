1145c808856b49567015fe0326d1dd2b061cf81a
ValberJunior <valberjr1992@gmail.com>
2025-03-23 20:42:47 -0300

    feat: Simplify returnRepoInfo method by removing partial parameter

--- plugins/vee-backend/src/api/git/github/index.ts
+++ plugins/vee-backend/src/api/git/github/index.ts
@@ -1,10 +1,10 @@
 import { extractGitHubInfo } from "../../../utils/helpers/extractGithubInfo";
-import { IGithubManager, ReturnGithubRepoInfo } from "./types";
+import { IGithubManager } from "./types";
 
 export class GithubManager implements IGithubManager {
 
-  async returnRepoInfo({url, partial}:ReturnGithubRepoInfo) {
-    const { host, owner, repo, branch,folderPath } = extractGitHubInfo(url,partial);
+  async returnRepoInfo(url : string) {
+    const { host, owner, repo, branch,folderPath } = extractGitHubInfo(url);
     const repoUrl = `https://${host}/${owner}/${repo}.git`;
     const localPath = `./temp-${repo}`;
     const folderPathResult = folderPath ? folderPath : null
