40e5a1f2d90635fb8ffd1d3038c6625d7b80f761
ValberJunior <valberjr1992@gmail.com>
2025-03-24 16:19:15 -0300

    refactor: Update StackReducer to handle new stack actions for improved state management

--- plugins/vee/src/components/stacks/manageStacks/state/reducer.ts
+++ plugins/vee/src/components/stacks/manageStacks/state/reducer.ts
@@ -1,3 +1,4 @@
+import { IPlugin } from "@veecode-platform/backstage-plugin-vee-common";
 import { StackActionsType } from "./action";
 import { StackStateType } from "./types";
 
@@ -5,8 +6,14 @@ export const initialStackState : StackStateType | null = null;
 
 export const StackReducer = (state: StackStateType | null, action: StackActionsType ) : StackStateType | null  => {
   switch (action.type) {
-    case 'ADD_STACK_STATE':
-        return action.payload;
+    case 'SET_STACK_DATA':
+        return action.payload as StackStateType;
+    case 'SET_STACK_NAME':
+        return {...state, name: action.payload};
+    case 'SET_STACK_SOURCE':
+        return {...state, source: action.payload};
+    case 'SET_STACK_PLUGINS':
+        return {...state, plugins: action.payload as IPlugin[]}
     case 'CLEAR_STACK_STATE':
         return null;
     default:
