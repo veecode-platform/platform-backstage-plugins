55aefdaf945a83661fdc7babe456215c4eb5a3ff
ValberJunior <valberjr1992@gmail.com>
2025-03-24 16:19:00 -0300

    refactor: Update stack state actions for improved clarity and functionality

--- plugins/vee/src/components/stacks/manageStacks/state/action.ts
+++ plugins/vee/src/components/stacks/manageStacks/state/action.ts
@@ -1,13 +1,37 @@
-import { StackStateType } from "./types";
+import type { IPlugin } from "@veecode-platform/backstage-plugin-vee-common";
+import type { StackStateType } from "./types";
 
 export const addStackState = (stack:StackStateType) => ({
-  type: 'ADD_STACK_STATE',
+  type: 'SET_STACK_DATA',
   payload: stack
 } as const);
 
-export const clearStackState = () => ({
-  type: 'CLEAR_STACK_STATE',
+export const setStackName = (stackName:string) => ({
+  type: 'SET_STACK_NAME',
+  payload: stackName
+});
+
+export const setStackSource = (source:string) => ({
+  type: 'SET_STACK_SOURCE',
+  payload: source
+});
+
+export const setStackPlugins = (plugins:IPlugin[]) => ({
+  type: 'SET_STACK_PLUGINS',
+  payload: plugins
+} as const)
+
+
+export const resetStackState = () => ({
+  type: 'RESET_STACK_STATE',
   payload: null
 } as const);
 
-export type StackActionsType = ReturnType<typeof addStackState | typeof clearStackState>
+export type StackActionsType = 
+ReturnType<
+typeof addStackState | 
+typeof setStackName |
+typeof setStackSource |
+typeof setStackPlugins |
+typeof resetStackState
+>
