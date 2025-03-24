6bc48a695b82988a7a8fc341a0baa2fe315d52d9
ValberJunior <valberjr1992@gmail.com>
2025-03-24 13:14:21 -0300

    refactor: Add baseFilePath parameter to transform functions for improved path handling

--- plugins/vee-backend/src/utils/helpers/transformStringToFileContent.ts
+++ plugins/vee-backend/src/utils/helpers/transformStringToFileContent.ts
@@ -1,7 +1,7 @@
 import { FileContent } from "@veecode-platform/backstage-plugin-vee-common";
 import { getLanguageFromFilename } from "./getLanguageFromFilename";
 
-function transformToObjectWithFileContent(obj: Record<string, any>): FileContent[] {
+function transformToObjectWithFileContent(obj: Record<string, any>,baseFilePath: string = ""): FileContent[] {
   const result: FileContent[] = [];
 
   function traverse(currentObject: Record<string, any>, currentPath: string = ""): void {
@@ -11,7 +11,7 @@ function transformToObjectWithFileContent(obj: Record<string, any>): FileContent
         if (currentObject[key] === null) {
           result.push({
             name: key,
-            relativePath: newPath.replace(/^\//, ''),
+            relativePath: `${baseFilePath}/${newPath.replace(/^\//, '')}`,
             content: `Content of ${key} here`,
             type: getLanguageFromFilename(key),
           });
@@ -26,12 +26,12 @@ function transformToObjectWithFileContent(obj: Record<string, any>): FileContent
   return result;
 }
 
-export function transformStringToObject(jsonString: string): FileContent[] {
+export function transformStringToObject(jsonString: string, baseFilePath: string = ""): FileContent[] {
   const newJsonString = jsonString.replace(
     /([{,]\s*)(\.\w+|[a-zA-Z0-9_\-.]+)\s*:/g,
     '$1"$2":'
   );
   const objTransformed: Record<string, any> = JSON.parse(newJsonString); 
-  const fileContentsData = transformToObjectWithFileContent(objTransformed);
+  const fileContentsData = transformToObjectWithFileContent(objTransformed,baseFilePath);
   return fileContentsData;
 }
