886b1acb95e1173309e3dcff1914b56514721e72
ValberJunior <valberjr1992@gmail.com>
2025-03-26 00:03:19 -0300

    feat: Implement permission checks for routes in ScaffolderAIPageRoot

--- plugins/vee/src/components/scaffolder-ai/ScaffolderAIPageRoot.tsx
+++ plugins/vee/src/components/scaffolder-ai/ScaffolderAIPageRoot.tsx
@@ -3,14 +3,34 @@ import { Route, Routes } from "react-router-dom";
 import { StackList } from "../stacks";
 import { GenerateTemplate } from "../generateTemplate";
 import { TemplateOutput } from "../templateOutput";
+import { RequirePermission } from "@backstage/plugin-permission-react";
+import { 
+    veeGenerateTemplatePermission, 
+    veeManageStacksPermission 
+} from "@veecode-platform/backstage-plugin-vee-common";
 
 
 export const ScaffolderAIPageRoot = () => {
     return (
         <Routes>
-            <Route path="" element={<StackList/>}/>
-            <Route path="/:stackId" element={<GenerateTemplate/>}/>
-            <Route path="output" element={<TemplateOutput/>}/>
+            <Route path="" element={
+                <RequirePermission 
+                  permission={veeManageStacksPermission}>
+                    <StackList/>
+                </RequirePermission>
+            }/>
+            <Route path="/:stackId" element={
+                <RequirePermission
+                  permission={veeGenerateTemplatePermission}>
+                    <GenerateTemplate/>
+                </RequirePermission>
+            }/>
+            <Route path="output" element={
+                <RequirePermission
+                permission={veeGenerateTemplatePermission}>
+                  <TemplateOutput/>
+              </RequirePermission>
+            }/>
         </Routes>
     )
 }
