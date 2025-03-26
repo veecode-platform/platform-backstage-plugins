61b176221ff0aa463b4b35af0c5f6d291f4f7aa5
ValberJunior <valberjr1992@gmail.com>
2025-03-26 00:04:09 -0300

    feat: Implement permission checks for VeeDashboard, SettingsPageRoot, and ScaffolderAIPageRoot

--- plugins/vee/src/components/veeHomepage/VeeHomepage.tsx
+++ plugins/vee/src/components/veeHomepage/VeeHomepage.tsx
@@ -4,13 +4,34 @@ import { Route, Routes } from "react-router-dom";
 import { SettingsPageRoot } from "../settingsPage";
 import { VeeProvider } from "../../context";
 import { ScaffolderAIPageRoot } from "../scaffolder-ai";
+import { RequirePermission } from "@backstage/plugin-permission-react";
+import { 
+  veeAccessSettingsPanelPermission, 
+  veeGenerateTemplatePermission, 
+  veeReadPermission } from "@veecode-platform/backstage-plugin-vee-common";
 
 export const VeeHomepage = () => (
             <VeeProvider>
               <Routes>
-                  <Route path="" element={<VeeDashboard/>}/>
-                  <Route path="/settings/*" element={<SettingsPageRoot/>}/>
-                  <Route path="/scaffolder-ai/*" element={<ScaffolderAIPageRoot/>}/>
+                  <Route path="" element={
+                     <RequirePermission
+                      permission={veeReadPermission}
+                      >
+                       <VeeDashboard/>
+                     </RequirePermission>
+                  }/>
+                  <Route path="/settings/*" element={
+                    <RequirePermission 
+                      permission={veeAccessSettingsPanelPermission}>
+                       <SettingsPageRoot/>
+                    </RequirePermission>
+                  }/>
+                  <Route path="/scaffolder-ai/*" element={
+                    <RequirePermission 
+                     permission={veeGenerateTemplatePermission}>
+                      <ScaffolderAIPageRoot/>
+                    </RequirePermission>
+                  }/>
               </Routes>
             </VeeProvider>
     )
