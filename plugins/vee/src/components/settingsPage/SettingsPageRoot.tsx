631092670efdc370352b5efeee1969751ecfa6fb
ValberJunior <valberjr1992@gmail.com>
2025-03-26 00:03:55 -0300

    feat: Add permission checks for accessing settings and managing stacks, plugins, and fixed options in SettingsPageRoot

--- plugins/vee/src/components/settingsPage/SettingsPageRoot.tsx
+++ plugins/vee/src/components/settingsPage/SettingsPageRoot.tsx
@@ -4,15 +4,38 @@ import { SettingsPage } from "./SettingsPage";
 import { ManageStacks } from "../stacks";
 import { ManagePlugins } from "../plugins";
 import { ManageFixedOptions } from "../fixedOptions";
+import { RequirePermission } from "@backstage/plugin-permission-react";
+import { veeAccessSettingsPanelPermission, veeManageFixedOptionsPermission, veeManagePluginsPermission, veeManageStacksPermission } from "@veecode-platform/backstage-plugin-vee-common";
 
 
 export const SettingsPageRoot = () => {
     return (
         <Routes>
-            <Route path="" element={<SettingsPage/>}/>
-            <Route path="manage-stacks" element={<ManageStacks/>}/>
-            <Route path="manage-plugins" element={<ManagePlugins/>}/>
-            <Route path="manage-fixed-options" element={<ManageFixedOptions/>}/>
+            <Route path="" element={
+                <RequirePermission 
+                   permission={veeAccessSettingsPanelPermission}>
+                    <SettingsPage/>
+                </RequirePermission>
+            }/>
+            <Route path="manage-stacks" element={
+                <RequirePermission 
+                  permission={veeManageStacksPermission}>
+                    <ManageStacks/>
+                </RequirePermission>
+            }/>
+            <Route path="manage-plugins" element={
+                <RequirePermission
+                 permission={veeManagePluginsPermission}
+                 >
+                    <ManagePlugins/>
+                </RequirePermission>
+            }/>
+            <Route path="manage-fixed-options" element={
+                <RequirePermission 
+                  permission={veeManageFixedOptionsPermission}>
+                    <ManageFixedOptions/>
+                </RequirePermission>
+            }/>
         </Routes>
     )
 }
