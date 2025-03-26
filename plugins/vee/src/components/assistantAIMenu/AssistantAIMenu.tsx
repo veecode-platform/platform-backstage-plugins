b9b916ab58f3ec793fb8a18896988ce2abbd8253
ValberJunior <valberjr1992@gmail.com>
2025-03-26 00:03:11 -0300

    feat: Integrate permission check in AssistantAIMenu for conditional rendering

--- plugins/vee/src/components/assistantAIMenu/AssistantAIMenu.tsx
+++ plugins/vee/src/components/assistantAIMenu/AssistantAIMenu.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 import { SidebarItem } from "@backstage/core-components";
 import { AssistantAIMenuIcon } from "../../assets/assistant-ai-menu-icon";
+import { usePermission } from "@backstage/plugin-permission-react";
+import { veeReadPermission } from "@veecode-platform/backstage-plugin-vee-common";
 
-export const AssistantAIMenu = () => (<SidebarItem icon={AssistantAIMenuIcon} to="vee" text="Assistant AI" />)
+export const AssistantAIMenu = () => {
+
+    const {loading: loadingPermission } = usePermission({
+        permission: veeReadPermission
+    });
+
+    return (
+       <>
+        {!loadingPermission && (
+            <SidebarItem 
+              icon={AssistantAIMenuIcon} 
+              to="vee" 
+              text="Vee AI" />
+        )}
+       </>
+    )
+}
