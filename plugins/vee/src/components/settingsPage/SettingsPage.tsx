65ed05d20d564b98b7733f00534d4dea7b0644f1
ValberJunior <valberjr1992@gmail.com>
2025-03-26 00:03:35 -0300

    feat: Add permission checks for managing stacks, plugins, and fixed options in SettingsPage

--- plugins/vee/src/components/settingsPage/SettingsPage.tsx
+++ plugins/vee/src/components/settingsPage/SettingsPage.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { MenuOptionCard, PageLayout } from "../shared";
-import { settingsOptions } from "./settings";
 import  Grid2 from "@mui/material/Grid2";
+import { StackIcon } from "../../assets/stack-icon";
+import { PluginIcon } from "../../assets/plugin-icon";
+import { FixedOptionsIcon } from "../../assets/fixed-options-icon";
+import { usePermission } from "@backstage/plugin-permission-react";
+import { veeManageFixedOptionsPermission, veeManagePluginsPermission, veeManageStacksPermission } from "@veecode-platform/backstage-plugin-vee-common";
 
 export const SettingsPage = () => {
+
+  const { loading: loadingStacksPermission } = usePermission({
+     permission: veeManageStacksPermission
+   });
+  const { loading: loadingPluginsPermission } = usePermission({
+    permission: veeManagePluginsPermission
+  });
+  const { loading: loadingFixedOptionsPermission } = usePermission({
+    permission: veeManageFixedOptionsPermission
+  })
 
     return (
         <PageLayout 
@@ -12,15 +26,28 @@ export const SettingsPage = () => {
           goBack
           >
            <Grid2 container spacing={2}> 
-              {settingsOptions.map( option => (
-                  <MenuOptionCard
-                    key={option.id}
-                    icon={option.icon}
-                    title={option.title}
-                    description={option.tooltip}
-                    path={option.path}
-                  />
-              ))}
+              {!loadingStacksPermission && (
+                <MenuOptionCard
+                  icon={StackIcon}
+                  title="Manage Stacks"
+                  description="List, create, delete or edit a stack in this menu."
+                  path="manage-stacks"/>
+                )}
+              {!loadingPluginsPermission && (
+                <MenuOptionCard
+                  icon={PluginIcon}
+                  title="Manage Plugins"
+                  description="List, add, edit or remove the plugins that will be available for your templates"
+                  path="manage-plugins"/>
+               )}
+              {!loadingFixedOptionsPermission &&(
+                <MenuOptionCard
+                icon={FixedOptionsIcon}
+                title="Manage Fixed Options"
+                description="List, create, delete or edit fixed options that will appear in your AI assistant according to your needs."
+                path="manage-fixed-options"
+              />
+              )}
             </Grid2>
         </PageLayout>
     )
