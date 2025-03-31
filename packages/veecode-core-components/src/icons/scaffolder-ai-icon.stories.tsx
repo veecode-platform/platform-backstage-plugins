46f3fbc9859001af2aa379eb88145a3f87c593c6
ValberJunior <valberjr1992@gmail.com>
2025-03-31 06:59:12 -0300

    Add stories for ScaffolderAIIcon component in Storybook

---
+++ packages/veecode-core-components/src/icons/scaffolder-ai-icon.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { ScaffolderAIIcon } from "./scaffolder-ai-icon";
+
+export default {
+    title: 'Icons/ScaffolderAIIcon',
+    component: ScaffolderAIIcon
+} as Meta;
+
+export const Default : StoryObj = {}
