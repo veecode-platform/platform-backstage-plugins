b2db300c8954bf70c2606ce83841c7d0584bf65a
ValberJunior <valberjr1992@gmail.com>
2025-03-31 06:57:57 -0300

    Add ButtonSecondary component with styles and support for icon and label

---
+++ packages/veecode-core-components/src/components/Button/ButtonSecondary.tsx
@@ -0,0 +1,30 @@
+import { makeStyles } from "@mui/styles"
+import React from "react"
+import { themeVariables } from "../../utils/constants/themeVariables"
+import { ButtonRoot } from "./ButtonRoot";
+
+export interface ButtonSecondaryProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+ label: string,
+ icon: React.ElementType
+}
+
+const useButtonSecondaryStyles = makeStyles({
+    secondary:{
+        backgroundColor: themeVariables.colors.grey,
+        color: themeVariables.colors.black
+    }
+});
+
+export const ButtonSecondary = React.forwardRef<HTMLButtonElement, ButtonSecondaryProps>(
+    ({ label, icon: Icon, ...rest}, ref) => {
+        const { secondary } = useButtonSecondaryStyles();
+        return (
+            <ButtonRoot 
+              ref={ref} 
+              styles={secondary} 
+              {...rest}>
+               {Icon && <Icon/>} {label}
+            </ButtonRoot>
+        )
+    }
+)
