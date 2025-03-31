886b8d744dc663f260e3ef7e289d243dabdcd620
ValberJunior <valberjr1992@gmail.com>
2025-03-31 07:00:03 -0300

    Add stories for VeeLogo component in Storybook

---
+++ packages/veecode-core-components/src/icons/VeeLogo.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { VeeLogo } from "./VeeLogo";
+
+export default {
+    title: 'Icons/VeeLogo',
+    component: VeeLogo
+} as Meta;
+
+export const Default : StoryObj = {}
