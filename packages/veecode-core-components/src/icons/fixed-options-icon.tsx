4c1c2097c538361f6652e6192cb1c1993b732c5c
ValberJunior <valberjr1992@gmail.com>
2025-03-31 06:58:54 -0300

    Add FixedOptionsIcon component for options functionality

---
+++ packages/veecode-core-components/src/icons/fixed-options-icon.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const FixedOptionsIcon = () => (
+  <svg 
+   version="1.1" 
+   id="Icons" 
+   xmlns="http://www.w3.org/2000/svg" 
+   xmlnsXlink="http://www.w3.org/1999/xlink" 
+   viewBox="0 0 32 32" 
+   xmlSpace="preserve" 
+   fill="#00F6BD" 
+   stroke="#00F6BD"
+   width="40" 
+   height="40" 
+   >
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"> </g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"> </g>
+    <g id="SVGRepo_iconCarrier"> 
+      <line style={{fill: "none",stroke:"#00F6BD",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round",strokeMiterlimit:"10"}} x1="17" y1="5" x2="29" y2="5"> </line> 
+      <line style={{fill: "none",stroke:"#00F6BD",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round",strokeMiterlimit:"10"}} x1="17" y1="9" x2="24" y2="9"> </line> 
+      <line style={{fill: "none",stroke:"#00F6BD",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round",strokeMiterlimit:"10"}} x1="17" y1="21" x2="29" y2="21"> </line> 
+      <line style={{fill: "none",stroke:"#00F6BD",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round",strokeMiterlimit:"10"}} x1="17" y1="25" x2="24" y2="25"> </line>
+       <circle style={{fill: "none",stroke:"#00F6BD",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round",strokeMiterlimit:"10"}} cx="8" cy="8" r="5"> </circle> 
+       <circle style={{fill: "none",stroke:"#00F6BD",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round",strokeMiterlimit:"10"}} cx="8" cy="24" r="5"> </circle> 
+       <circle style={{fill: "none",stroke:"#00F6BD",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round",strokeMiterlimit:"10"}} cx="8" cy="8" r="1"> </circle> 
+      </g>
+  </svg>
+);
+
+
