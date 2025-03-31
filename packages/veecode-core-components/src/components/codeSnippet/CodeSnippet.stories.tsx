d7ef7c383cfda3ee33b9cfa2f36cfc88e8602ff4
ValberJunior <valberjr1992@gmail.com>
2025-03-31 06:58:03 -0300

    Add CodeSnippet component stories for demonstration and testing

---
+++ packages/veecode-core-components/src/components/codeSnippet/CodeSnippet.stories.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import type { Meta } from "@storybook/react";
+import { CodeSnippet,CodeSnippetProps } from "./CodeSnippet";
+import { Box } from "@mui/material";
+
+
+export default {
+    title: 'Components/CodeSnippet',
+    component: CodeSnippet,
+    args:{
+        code: 'console.log("hello world!)',
+        language: 'javascript'
+    },
+    argTypes:{
+        code: { control: 'text' },
+        language: { control: 'text'},
+        className: { control: "text" }
+    },
+    decorators: [
+      (Story) => {
+        return (<Box sx={{ maxWidth: '80vw'}}>{Story()}</Box>)
+      }
+    ]
+} as Meta<CodeSnippetProps>;
+
+
+export const Default = (args: CodeSnippetProps) => (
+    <CodeSnippet
+     code={args.code}
+     language={args.language}
+     />
+)
