fffba0f0e97eebf39e731695f3a8137d4a200efe
ValberJunior <valberjr1992@gmail.com>
2025-03-31 06:59:45 -0300

    Add StackIcon component for icon functionality

---
+++ packages/veecode-core-components/src/icons/stack-icon.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export const StackIcon = () => (
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    viewBox="0 12.875 50 41.635"
+    enableBackground="new 0 12.875 50 41.635"
+    xmlSpace="preserve"
+    fill="#00F6BD"
+    width={40}
+    height={40}
+  >
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"> </g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    > </g>
+    <g id="SVGRepo_iconCarrier">
+      {' '}
+      <g id="Layer_3"> </g>{' '}
+      <g id="Layer_2">
+        {' '}
+        <g>
+          {' '}
+          <polygon
+            fill="none"
+            stroke="#00F6BD"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            points="25.331,36.012 2.571,25.035 25.209,14.178 47.97,25.156 "
+          > </polygon>{' '}
+        </g>{' '}
+        <g>
+          {' '}
+          <path
+            fill="none"
+            stroke="#00F6BD"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M40.883,30.362l7.087,3.419L25.331,44.637 L2.571,33.66c0,0,3.909-1.873,7.012-3.361"
+          > </path>{' '}
+        </g>{' '}
+        <g>
+          {' '}
+          <polyline
+            fill="none"
+            stroke="#00F6BD"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            points="41.229,39.155 47.97,42.406 25.331,53.262 2.571,42.285 9.081,39.164 "
+          > </polyline>{' '}
+        </g>{' '}
+      </g>{' '}
+    </g>
+  </svg>
+);
