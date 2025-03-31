958d9468005acf23ba8961e6d18f58a27b95b2ca
ValberJunior <valberjr1992@gmail.com>
2025-03-31 06:59:51 -0300

    Add stories for VeeSettingsIcon component in Storybook

---
+++ packages/veecode-core-components/src/icons/vee-settings-icon.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { VeeSettingsIcon } from "./vee-settings-icon";
+
+export default {
+    title: 'Icons/VeeSettingsIcon',
+    component: VeeSettingsIcon
+} as Meta;
+
+export const Default : StoryObj = {}
