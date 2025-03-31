ec911c791e5cb32874e695b63e39831109205191
ValberJunior <valberjr1992@gmail.com>
2025-03-31 07:03:57 -0300

    feat: ✨ create const themeVariables

---
+++ packages/veecode-core-components/src/utils/constants/themeVariables.ts
@@ -0,0 +1,34 @@
+export const themeVariables = {
+    layout: {
+        maxWidth: '1200px'
+    },
+    colors:{
+        main: "#00F6BD",
+        grey: "#B6B6B6",
+        black: "#151515",
+        dark: "#00000067",
+        white: "#FAFAFA",
+        red: "#D50000",
+        darkGrey: "#202020",
+        success: "#4CAF50"
+    },
+    blur:{
+        dark: '#00000070'
+    },
+    background:{
+        main: '#151515',
+        secondary: '#424242',
+        dark: '#0F0F0F',
+        danger: '#FF000025'
+    },
+    codeBlock:{
+        dark: '#0D1117'
+    },
+    border:{
+        main: '#B5B5B530'
+    },
+    list:{
+        background: "#0F0F0F",
+        hover: "#00000087"
+    }
+}
