55cb1dda58b48fcf6b1c88a9829ea33e8cb0a562
ValberJunior <valberjr1992@gmail.com>
2025-04-01 16:41:08 -0300

    feat: ✨ add new exports

--- packages/veecode-core-components/src/animations/index.ts
+++ packages/veecode-core-components/src/animations/index.ts
@@ -1,3 +1,5 @@
 export { SuccessAnimation } from "./success/Success";
 export { ErrorAnimation } from "./error/Error";
 export * from "./loading"
+export { InfoAnimation } from "./info/Info";
+export { WarningAnimation } from "./warning/Warning"
