89f4ea420666f92faccc49a9e73e33745a2906b2
ValberJunior <valberjr1992@gmail.com>
2025-04-06 00:47:07 -0300

    feat: ✨ add PluginNotConfiguredIcon

---
+++ packages/veecode-core-components/src/icons/plugin-not-configured-icon.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { PluginNotConfiguredIcon } from "./plugin-not-configured-icon";
+
+
+export default {
+    title: 'Icons/PluginNotConfiguredIcon',
+    component: PluginNotConfiguredIcon
+} as Meta;
+
+export const Default : StoryObj = {}
