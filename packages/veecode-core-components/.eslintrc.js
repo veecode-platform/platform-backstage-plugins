c800a1330921d95c4f824bc160e222ed1b5ddf05
ValberJunior <ValberJunior@users.noreply.github.com>
2025-04-07 13:00:17 +0000

    deploy: d132e1358e34a0846d85f5336c2a5aef743a558c

--- packages/veecode-core-components/.eslintrc.js
+++
@@ -1,2 +0,0 @@
-module.exports = require('@backstage/cli/config/eslint-factory')(__dirname);
-export const extends = ['plugin:storybook/recommended'];
--- packages/veecode-core-components/.storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/manager-api';
-import VeeCodeTheme from './VeeCodeTheme';
- 
-addons.setConfig({
-  theme: VeeCodeTheme
-});
