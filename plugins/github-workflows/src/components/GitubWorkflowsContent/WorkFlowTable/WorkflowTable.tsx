e480c87cfdcd4d0764b6c48a72e2143435d3b4cd
ValberJunior <valberjr1992@gmail.com>
2025-06-26 11:46:37 -0300

    refactor: replace react-icons with Material-UI icons and improve code formatting in WorkflowTable component

--- plugins/github-workflows/src/components/GitubWorkflowsContent/WorkFlowTable/WorkflowTable.tsx
+++ plugins/github-workflows/src/components/GitubWorkflowsContent/WorkFlowTable/WorkflowTable.tsx
@@ -19,62 +19,75 @@ import Timer from '@material-ui/icons/Timer';
 import { useWorkflowTableStyles } from './styles';
 import { WorkflowTableProps } from './types';
 import SelectBranch from '../../SelectBranch/SelectBranch';
-import { IoOpenOutline } from "react-icons/io5";
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import { WorkFlowStatus } from '../WorkFlowStatus';
 
-
-const WorkflowTable : React.FC<WorkflowTableProps> = (props) => {
-  
-  const [ showModal, setShowModal ] = React.useState<boolean>(false);
-  const [parametersState, setParametersState] = React.useState<WorkflowDispatchParameters[]|null>(null)
-  const [ loading, setLoading] = React.useState<boolean>(false);
+const WorkflowTable: React.FC<WorkflowTableProps> = props => {
+  const [showModal, setShowModal] = React.useState<boolean>(false);
+  const [parametersState, setParametersState] = React.useState<
+    WorkflowDispatchParameters[] | null
+  >(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
   const { entity } = useEntity();
   const { projectName, hostname } = useEntityAnnotations(entity as Entity);
   const navigate = useNavigate();
-  const {action,source,clickable,title,options,name} = useWorkflowTableStyles();
-  const { items, updateData} = props;
+  const { action, source, clickable, title, options, name } =
+    useWorkflowTableStyles();
+  const { items, updateData } = props;
 
-  const refresh = async ()=> {
-    setLoading(true)
+  const refresh = async () => {
+    setLoading(true);
     await updateData();
-    setTimeout(()=> setLoading(false), 800)
-  }
+    setTimeout(() => setLoading(false), 800);
+  };
 
   const handleShowModal = () => {
-    setShowModal(!showModal)
-  }
+    setShowModal(!showModal);
+  };
 
-  const handleCICDLogs = (id: string) => {   
-    navigate(`/catalog/${entity.metadata.namespace}/${entity.kind.toLowerCase()}/${entity.metadata.name}/ci-cd/${id}`) 
-  }
+  const handleCICDLogs = (id: string) => {
+    navigate(
+      `/catalog/${entity.metadata.namespace}/${entity.kind.toLowerCase()}/${
+        entity.metadata.name
+      }/ci-cd/${id}`,
+    );
+  };
 
   const columns: TableColumn[] = [
-    { title: 'Name', field: 'name',  width:'1fr', align:'center'},
-    { title: 'Status', field: 'status', width:'1fr', align:'center' },
-    { title: 'Action', field: 'action', width:'1fr', align:'center' },
-    { title: 'Source', field: 'source', width:'1fr', align:'center'},
-    { title: 'Logs', field: 'logs', width:'auto', align:'center'}
+    { title: 'Name', field: 'name', width: '1fr', align: 'center' },
+    { title: 'Status', field: 'status', width: '1fr', align: 'center' },
+    { title: 'Action', field: 'action', width: '1fr', align: 'center' },
+    { title: 'Source', field: 'source', width: '1fr', align: 'center' },
+    { title: 'Logs', field: 'logs', width: 'auto', align: 'center' },
   ];
-
 
   const data = items.map(item => {
     return {
       name: (
         <div className={name}>
-          {item.name} 
-           <Tooltip
-                title={
-                  item.lastRunId ? 'Open the actions panel on github...' : 'First, run the workflow!'
-                }
-                placement="bottom"
-              >
-                <Link to={item.lastRunId ? `https://${hostname}/${projectName}/actions/runs/${item.lastRunId}` : '#'} target='_blank'>
-                <IoOpenOutline
-                  className={clickable}
-                  color={item.lastRunId ? 'primary' : 'disabled'}
-                />
-                </Link>
-              </Tooltip>
+          {item.name}
+          <Tooltip
+            title={
+              item.lastRunId
+                ? 'Open the actions panel on github...'
+                : 'First, run the workflow!'
+            }
+            placement="bottom"
+          >
+            <Link
+              to={
+                item.lastRunId
+                  ? `https://${hostname}/${projectName}/actions/runs/${item.lastRunId}`
+                  : '#'
+              }
+              target="_blank"
+            >
+              <OpenInNewIcon
+                className={clickable}
+                color={item.lastRunId ? 'primary' : 'disabled'}
+              />
+            </Link>
+          </Tooltip>
         </div>
       ),
       status: (
@@ -124,82 +137,78 @@ const WorkflowTable : React.FC<WorkflowTableProps> = (props) => {
       ),
       logs: (
         <>
-          {
-            item.status === StatusWorkflowEnum.inProgress ||
-            item.status === StatusWorkflowEnum.queued ? (
-              <Tooltip title="Please wait the workflow run" placement="bottom">
-                <Timer
-                  className={clickable}
-                  color="disabled"
-                  style={{ cursor: 'wait' }}
-                />
-              </Tooltip>
-            ) : (
-              <Tooltip
-                title={
-                  item.lastRunId ? 'Last run Logs...' : 'First, run the workflow!'
-                }
-                placement="bottom"
-              >
-                <DescriptionIcon
-                  className={clickable}
-                  onClick={() => {
-                    if (!item.lastRunId) return;
-                    handleCICDLogs(item.lastRunId!.toString());
-                  }}
-                  color={item.lastRunId ? 'primary' : 'disabled'}
-                />
-              </Tooltip>
-            )
-          }
+          {item.status === StatusWorkflowEnum.inProgress ||
+          item.status === StatusWorkflowEnum.queued ? (
+            <Tooltip title="Please wait the workflow run" placement="bottom">
+              <Timer
+                className={clickable}
+                color="disabled"
+                style={{ cursor: 'wait' }}
+              />
+            </Tooltip>
+          ) : (
+            <Tooltip
+              title={
+                item.lastRunId ? 'Last run Logs...' : 'First, run the workflow!'
+              }
+              placement="bottom"
+            >
+              <DescriptionIcon
+                className={clickable}
+                onClick={() => {
+                  if (!item.lastRunId) return;
+                  handleCICDLogs(item.lastRunId!.toString());
+                }}
+                color={item.lastRunId ? 'primary' : 'disabled'}
+              />
+            </Tooltip>
+          )}
         </>
       ),
     };
   });
 
   const TitleBar = (
-      <>
-        <Typography className={title}>
-          <GithubIcon/>
-          All Workflows
-        </Typography>
-        <Box role="combobox" className={options}>
-            <SelectBranch/>
-        </Box>
-      </>
-  )
+    <>
+      <Typography className={title}>
+        <GithubIcon />
+        All Workflows
+      </Typography>
+      <Box role="combobox" className={options}>
+        <SelectBranch />
+      </Box>
+    </>
+  );
 
   return (
-   <>
-    <Table
-      style={{width: '100%', padding: '1rem'}}
-      title={TitleBar}
-      options={{ search: false, paging: true }}
-      columns={columns}
-      data={data}
-      isLoading={loading}
-      actions={[
-        {
-          icon: () => <SyncIcon />,
-          tooltip: 'Reload workflow runs',
-          isFreeAction: true,
-          onClick: () => refresh(),
-        },
-      ]}
-    />
-     <>
-        {
-          showModal && (
-            <ModalComponent 
-              open={showModal} 
-              handleModal={handleShowModal}
-              parameters={parametersState ?? []}
-              />
-          )
-        }
-        </>
-  </>
+    <>
+      <Table
+        style={{ width: '100%', padding: '1rem' }}
+        title={TitleBar}
+        options={{ search: false, paging: true }}
+        columns={columns}
+        data={data}
+        isLoading={loading}
+        actions={[
+          {
+            icon: () => <SyncIcon />,
+            tooltip: 'Reload workflow runs',
+            isFreeAction: true,
+            onClick: () => refresh(),
+          },
+        ]}
+      />
+      <>
+        {showModal && (
+          <ModalComponent
+            open={showModal}
+            handleModal={handleShowModal}
+            parameters={parametersState ?? []}
+          />
+        )}
+      </>
+    </>
   );
 };
 
-export default React.memo(WorkflowTable)
+export default React.memo(WorkflowTable);
