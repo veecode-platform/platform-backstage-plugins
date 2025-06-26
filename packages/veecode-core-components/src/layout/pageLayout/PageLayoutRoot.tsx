01b7ad226d2085790272ce1583bda19ee03baa51
ValberJunior <valberjr1992@gmail.com>
2025-06-26 11:22:39 -0300

    style: format code and improve consistency in PageLayoutRoot component

--- packages/veecode-core-components/src/layout/pageLayout/PageLayoutRoot.tsx
+++ packages/veecode-core-components/src/layout/pageLayout/PageLayoutRoot.tsx
@@ -1,21 +1,20 @@
-
-import React from "react";
-import { Content } from "@backstage/core-components/index";
-import { Theme } from "@mui/material/styles";
-import { makeStyles } from "@mui/styles";
+import React from 'react';
+import { Content } from '@backstage/core-components';
+import { Theme } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
 
 export interface PageLayoutRootProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 const usePageLayoutRootStyles = makeStyles((theme: Theme) => ({
   root: {
     backgroundColor: theme.palette.grey[900],
     minHeight: '100vh',
-  }
+  },
 }));
 
-export const PageLayoutRoot : React.FC<PageLayoutRootProps> = ({children}) => {
+export const PageLayoutRoot: React.FC<PageLayoutRootProps> = ({ children }) => {
   const { root } = usePageLayoutRootStyles();
-  return <Content className={root}> {children} </Content>
-}
+  return <Content className={root}> {children} </Content>;
+};
