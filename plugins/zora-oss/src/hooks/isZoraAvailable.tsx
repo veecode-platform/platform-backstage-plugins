60758a5b679b4935c044942646006b0928c99dc1
ValberJunior <valberjr1992@gmail.com>
2025-06-27 14:24:45 -0300

    fix: ensure isZoraAvailable returns a boolean value

--- plugins/zora-oss/src/hooks/isZoraAvailable.tsx
+++ plugins/zora-oss/src/hooks/isZoraAvailable.tsx
@@ -1,5 +1,5 @@
 import { Entity } from '@backstage/catalog-model';
 import { CLUSTER_NAME } from '../utils/constants/annotations';
 
-export const isZoraAvailable = (entity: Entity) =>
-  entity.metadata.annotations?.[CLUSTER_NAME];
+export const isZoraAvailable = (entity: Entity): boolean =>
+  entity.metadata.annotations?.[CLUSTER_NAME] !== undefined;
