9b1e20016b72732e76f9a808e640def8f4bbffc9
ValberJunior <valberjr1992@gmail.com>
2025-06-26 21:35:54 -0300

    :tada: release : add alpha export

--- plugins/gitlab-pipelines/dev/index.tsx
+++ plugins/gitlab-pipelines/dev/index.tsx
@@ -1,13 +1,14 @@
+// eslint-disable-next-line no-restricted-syntax
 import React from 'react';
 import { createDevApp } from '@backstage/dev-utils';
-import { gitlabPipelinesPlugin, } from '../src/plugin';
+import { gitlabPipelinesPlugin } from '../src/plugin';
 import { GitlabPipelinesList } from '../src/components/GitlabPipelinesList';
 
 createDevApp()
   .registerPlugin(gitlabPipelinesPlugin)
   .addPage({
-    element: <GitlabPipelinesList/>,
+    element: <GitlabPipelinesList />,
     title: 'Root Page',
-    path: '/gitlab-pipelines'
+    path: '/gitlab-pipelines',
   })
   .render();
--- plugins/gitlab-pipelines/src/index.ts
+++ plugins/gitlab-pipelines/src/index.ts
@@ -1,2 +1,9 @@
-export * from './plugin';
+export {
+  gitlabPipelinesPlugin,
+  gitlabPipelinesPlugin as plugin,
+  GitlabJobs,
+  GitlabPipelineList,
+} from './plugin';
 export * from './hooks';
+export * from './components/GitlabPipelinesList';
+export * from './components/GitlabJobs';
